7645b52c8d70640bbc7318bcef23bfa1b3f64417
Jaideep Das <jaideep.jdof@gmail.com>
2013-12-19 23:23:57 +0530

    user model description updated. fitness model skeleton

---
+++ api/models/Fitness.js
@@ -0,0 +1,19 @@
+/**
+ * Fitness
+ *
+ * @module      :: Model
+ * @description :: User fitness information model. One to many relation between user and fitness.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};
--- api/models/User.js
+++ api/models/User.js
@@ -2,12 +2,16 @@
  * User
  *
  * @module      :: Model
- * @description :: User attribute and profile model.
+ * @description :: User and profile attribute model. One to many relation between organization and user.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
 
 module.exports = {
     attributes: {
+        organization: {
+            type: 'uuid',
+            required: true
+        },
         uid: {
             type: 'alphanumeric', 
             required: true
