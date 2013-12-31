2a717315730cc76da11026c245064196bcf0ce65
Jaideep Das <jaideep.jdof@gmail.com>
2013-12-31 18:20:41 +0530

    user valid param tests

--- test/user-api-test.js
+++ test/user-api-test.js
@@ -9,4 +9,8 @@ suite.discuss('User API')
     .setHeader('Content-Type', 'application/json')
     .get('/user/')
     .expect(200, {code: 29045, message: 'access token missing'})
+    .undiscuss().unpath()
+    .discuss('GET request with correct parameters')
+    .get('/user/', {access_token: '52c2b79dc91759c810d9a5ca'})
+    .expect(200, {id: '52c2b8be350f040212646809'})
     .export(module);
