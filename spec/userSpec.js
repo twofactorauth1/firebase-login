3ea76f30ffe6d90c2a6f6f70cfd3808f5e945e19
Jaideep Das <jaideep.jdof@gmail.com>
2014-01-07 23:14:23 +0530

    web driver trial test

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,11 +1,13 @@
-var request = require('request');
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-describe("User API", function () {
-    it("without authentication should respond with 200", function (done) {
-        request('http://localhost:1337/user', function (error, response, body) {
-            expect(response.statusCode).toEqual(200);
-            expect(response.request.path).toEqual('/auth/login/');
+describe('User API', function () {
+    it('Without authentication', function (done) {
+        driver.get('http://localhost:1337/user/');
+        driver.getTitle().then(function (title) {
+            expect(title).toBe('Sails');
             done();
+            driver.close();
         });
-    });
+    }, 10000);
 });
