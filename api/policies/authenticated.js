b2f5cc9266d00616b537c3a9f9d819284a554a6a
Jaideep Das <jaideep.jdof@gmail.com>
2014-01-07 14:03:28 +0530

    auth policy

---
+++ api/policies/authenticated.js
@@ -0,0 +1,11 @@
+/**
+ * Allow any authenticated user.
+ */
+module.exports = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    else {
+        return res.redirect('/auth/login/');
+    }
+};
