e376aedc0bd142f49ad7c87afff3e27a8e08b7ba
Jaideep Das <jaideep.jdof@gmail.com>
2014-01-10 11:35:23 +0530

    attempted selenium calls for APIs

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -2,12 +2,17 @@ var webdriver = require('selenium-webdriver');
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
 describe('User API', function () {
-    it('Without authentication', function (done) {
-        driver.get('http://localhost:1337/user/');
-        driver.getTitle().then(function (title) {
-            expect(title).toBe('Sails');
-            done();
-            driver.close();
-        });
-    }, 10000);
+    it('authenticate admin user', function () {
+        driver.get('http://localhost:1337/auth/login/');
+        driver.findElement(webdriver.By.name('username')).sendKeys('admin');
+        driver.findElement(webdriver.By.name('password')).sendKeys('admin');
+        driver.findElement(webdriver.By.xpath('//input[@type="submit"]')).click();
+    }, 20000);
+    
+    it('GET request without parameters', function () {
+        driver.get('http://localhost:1337/user');
+        console.log(webdriver.By.tagName('body').text);
+        driver.quit();
+    }, 20000);
+    
 });
