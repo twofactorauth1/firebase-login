d76708365fe3f92c3b91ca17e8eb4ec19f8a2611
Jaideep Das <jaideep.jdof@gmail.com>
2014-01-14 17:29:53 +0530

    product model

---
+++ api/models/Product.js
@@ -0,0 +1,20 @@
+/**
+ * Product
+ *
+ * @module      :: Model
+ * @description :: DB representation of the subsription product.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+var slug = require('slugg');
+
+module.exports = {
+    attributes: {
+        name: {type: 'string', required: true},
+        slug: {type: 'string', required: true, unique: true},
+        description: {type: 'text', required: true}
+    },
+    beforeCreate: function (product, callback) {
+        product.slug = slug(product.name);
+        callback(null, product);
+    }
+};
