91745bc6bf2dcabf6549429d4c96d5fe37b1f91e
Jaideep Das <jaideep.jdof@gmail.com>
2014-01-24 11:08:37 +0530

    prod settigns

--- config/adapters.js
+++ config/adapters.js
@@ -12,16 +12,28 @@
  * http://sailsjs.org/#documentation
  */
 
+var mongo_settings = {};
+if (process.env.NODE_ENV === 'production') {
+    mongo_settings = {module: 'sails-mongo',
+                      host: 'novus.modulusmongo.net',
+                      port: 27017,
+                      user: 'indigenous',
+                      password: 'oxf25ufo',
+                      database: 'H2inesux'};
+}
+else {
+    mongo_settings = {module: 'sails-mongo',
+                      host: 'localhost',
+                      port: 27017,
+                      user: '',
+                      password: '',
+                      database: 'bioindigenous'};
+}
+
 module.exports.adapters = {
 
     // refer to https://github.com/balderdashy/sails-mongo for advance config
     'default': 'mongo',
-    mongo: {
-        module   : 'sails-mongo',
-        host     : 'localhost',
-        port     : 27017,
-        user     : '',
-        password : '',
-        database : 'bioindigenous',
-    }
+    // indigenous:oxf25ufo@novus.modulusmongo.net:27017/H2inesux
+    mongo: mongo_settings
 };
