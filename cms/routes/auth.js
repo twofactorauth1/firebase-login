71492c9d6baf9d8419e4007827086720db3de666
Jaideep Das <jaideep.jdof@gmail.com>
2014-02-05 17:28:07 +0530

    restored auth route

---
+++ cms/routes/auth.js
@@ -0,0 +1,33 @@
+var passport = require('passport');
+
+exports.login = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.json({status: true, message: 'Login Successful.'});
+    }
+    passport.authenticate('local', function (err, user) {
+        if (err) {
+            return res.json({status: false, message: err.message});
+        }
+        else {
+            if (user) {
+                req.logIn(user, function (err) {
+                    if (err) {
+                        return res.json({status: false, message: err.message});
+                    }
+                    else {
+                        return res.json({status: true, message: 'Login Successful.'});
+                    }
+                });
+            }
+            else {
+                return res.json({status: false, message: 'Invalid Email / Password.'});
+            }
+        }
+    })(req, res, next);
+};
+
+exports.logout = function (req, res) {
+    req.logout();
+    return res.json({status: true, message: 'Logout Successful.'});
+};
+
