9adbb5a4e791506c98091bdca67a4658b108d2f2
Jaideep Das <jaideep.jdof@gmail.com>
2014-02-06 11:24:35 +0530

    beautified the code

--- frontend/middlewares/subdomainAuthorize.js
+++ frontend/middlewares/subdomainAuthorize.js
@@ -7,69 +7,64 @@ module.exports = function () {
     return function (req, res, next) {
         if (req.isAuthenticated()) {
             var userType = req.user.role;
-            if (userType===1) {
-                Customer.findOne({user: req.user._id}, function (err, customer) {
+            if (userType === 1) {
+                Customer.findOne({
+                    user: req.user._id
+                }, function (err, customer) {
                     next();
                 });
-            }
-            else if (userType===2) {
+            } else if (userType === 2) {
                 if (req.session.subDomains) {
-                    if (req.session.subDomains.indexOf(req.subdomains.join('.'))===-1) {
+                    if (req.session.subDomains.indexOf(req.subdomains.join('.')) === -1) {
                         return res.send(404, 'View does not exist');
-                    }
-                    else {
+                    } else {
                         next();
                     }
-                }
-                else {
-                    Client.findOne({user: req.user._id}, function (err, client) {
+                } else {
+                    Client.findOne({
+                        user: req.user._id
+                    }, function (err, client) {
                         if (err) {
                             return res.send(404, 'View does not exist');
-                        }
-                        else {
+                        } else {
                             if (client) {
-                                Site.find({client: client._id}, function (err, sites) {
+                                Site.find({
+                                    client: client._id
+                                }, function (err, sites) {
                                     if (err) {
                                         return res.send(404, 'View does not exist');
-                                    }
-                                    else {
+                                    } else {
                                         if (sites) {
                                             var subDomains = [];
                                             sites.forEach(function (element, index, array) {
                                                 subDomains.push(element.subDomain);
                                             });
                                             req.session.subDomains = subDomains;
-                                            if (subDomains.indexOf(req.subdomains.join('.'))===-1) {
+                                            if (subDomains.indexOf(req.subdomains.join('.')) === -1) {
                                                 return res.send(404, 'View does not exist');
-                                            }
-                                            else {
+                                            } else {
                                                 next();
                                             }
-                                        }
-                                        else {
+                                        } else {
                                             return res.send(404, 'View does not exist');
                                         }
                                     }
-                                });    
-                            }
-                            else {
+                                });
+                            } else {
                                 return res.send(404, 'View does not exist');
                             }
                         }
                     });
                 }
-            }
-            else {
+            } else {
                 next();
             }
-        }
-        else {
+        } else {
             var allowedPaths = ['/login', '/login/'];
             if (allowedPaths.indexOf(req.path)) {
                 next();
-            }
-            else {
-                return res.send(404, 'View does not exist.');    
+            } else {
+                return res.send(404, 'View does not exist.');
             }
         }
     }
