b6b8a2201760f4d1756612fee2981caac3efe2f6
Christopher Mina <christopher.mina@gmail.com>
2014-02-12 05:19:33 -0700

    Removing routes from app.js and creating organized hierarchy for managing them in the long term

---
+++ routers/login.router.js
@@ -0,0 +1,22 @@
+var passport = require('passport');
+var authRoutes = require('../routes/auth');
+
+var router = function() {
+};
+
+_.extend(router.prototype, {
+
+    initialize: function() {
+        var app = global.app;
+
+        app.post('/login/', authRoutes.login);
+        app.get('/logout/', authRoutes.logout);
+        app.get('/login/facebook/', passport.authenticate('facebook', {scope: ['email']}));
+        app.get('/login/facebook/callback/', passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/login/facebook'}));
+
+        return this;
+    }
+});
+
+module.exports.router = new router().initialize();
+
---
+++ routers/routerloader.js
@@ -0,0 +1,7 @@
+var loginRouter = require('../routers/login.router');
+var indexRouter = require('../routers/index.router');
+
+module.exports = {
+    loginRouter: loginRouter,
+    indexRouter: indexRouter
+};
