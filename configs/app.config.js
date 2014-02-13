40d20e7e9cbef99076f632999d1df81b2b48690f
Christopher Mina <christopher.mina@gmail.com>
2014-02-12 17:45:03 -0700

    Using dynamic port for modulus

--- configs/app.config.js
+++ configs/app.config.js
@@ -9,9 +9,15 @@ var environments = {
 //---------------------------------------------------------
 
 process.env.NODE_ENV = environments.PRODUCTION;
-process.env.PORT = 3000; //remember to change serverURL as well, when changing port!
 
-var serverUrl = "http://localhost:3000";
+var serverUrl = "";
+//We don't want to specifcy a port if we're on MODULUS.IO - needs to be dynamic
+if (process.env.NODE_ENV != environments.PRODUCTION) {
+    process.env.PORT = 3000; //remember to change serverURL as well, when changing port!
+    serverUrl = "http://localhost:" + process.env.PORT;
+} else {
+    serverUrl = "http://indigenous-10744.onmodulus.net/";
+}
 
 //---------------------------------------------------------
 // EXPORTS
--- configs/log4js.config.js
+++ configs/log4js.config.js
@@ -14,7 +14,7 @@ var props = {
 
 switch(process.env.NODE_ENV) {
     case 'production':
-        props.logLevel = log4js.levels.ERROR;
+        props.logLevel = log4js.levels.DEBUG; //TODO: Change back to "ERROR"
         break;
     case 'staging':
         props.logLevel = log4js.levels.DEBUG;
