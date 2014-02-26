1b07a6744099bdcfae750a54809ad88735e5a803
Christopher Mina <christopher.mina@gmail.com>
2014-02-26 07:08:25 -0700

    Merge from origin

---
+++ authentication/passport.facebook.js
@@ -0,0 +1,40 @@
+var passport = require('passport');
+var facebookStrategy = require('passport-facebook').Strategy;
+var facebookConfig = require('../configs/facebook.config.js');
+var UserDao = require('../dao/user.dao.js');
+
+
+passport.use(new facebookStrategy({
+    clientID: facebookConfig.FACEBOOK_CLIENT_ID,
+    clientSecret: facebookConfig.FACEBOOK_CLIENT_SECRET,
+    callbackURL: facebookConfig.FACEBOOK_CALLBACK_URL
+    },
+   function (accessToken, refreshToken, profile, done) {
+       var email = profile.emails[0].value;
+       UserDao.getUserByEmail(email, function (err, user) {
+	   if (err) {
+	       return done(null, false, {message: 'An error occurred searching for user by email ID.'});
+	   }
+	   else {
+	       if (user){
+		   return done(null, user);
+	       }
+	       else {
+		   UserDao.createUserFromFacebookProfile(accessToken, profile, function (err, user) {
+		       if (err) {
+			   return done(null, false, {message: 'An error occurred trying to create the user.'});
+		       }
+		       else {
+			   if (user) {
+			       return done(null, user);
+			   }
+			   else {
+			       return done(null, false, {message: 'User not created.'}); 
+			   }
+		       }
+		   });
+	       }
+	   }
+       });
+   }
+));
