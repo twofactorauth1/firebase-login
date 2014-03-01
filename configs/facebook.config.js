951f59111e93daad5feb95936a4c261623335710
Christopher Mina <christopher.mina@gmail.com>
2014-03-01 04:47:31 -0700

    Using environment variables for FB creds

--- configs/facebook.config.js
+++ configs/facebook.config.js
@@ -1,8 +1,11 @@
 var appConfig =  require('./app.config');
 
+var facebookClientId = process.env.FACEBOOK_CLIENT_ID || '256401934537921';
+var facebookClientSecret = process.env.FACEBOOK_CLIENT_SECRET || 'baf33c08a8c2cef97d7db1ad5c7451a8';
+
 module.exports = {
-    FACEBOOK_CLIENT_ID: '269704773200651',
-    FACEBOOK_CLIENT_SECRET: 'baf33c08a8c2cef97d7db1ad5c7451a8',
+    FACEBOOK_CLIENT_ID: facebookClientId
+    FACEBOOK_CLIENT_SECRET: facebookClientSecret,
     FACEBOOK_CALLBACK_URL_SIGNUP: appConfig.server_url + "/signup/facebook/callback",
     FACEBOOK_CALLBACK_URL_LOGIN: appConfig.server_url + "/login/facebook/callback",
 
