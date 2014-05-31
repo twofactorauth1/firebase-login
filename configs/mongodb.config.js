0f31a2bc3d2feb3a73004d4d8703990d8c77d5b5
Brad Risse <bradly.risse@gmail.com>
2014-05-30 17:07:23 -0700

    Updated MongoHQ config

--- configs/mongodb.config.js
+++ configs/mongodb.config.js
@@ -7,16 +7,15 @@
 
 var config = require('./app.config');
 
-var connectionString = "mongodb://localhost/bioindigenous";
+//var connectionString = "mongodb://localhost/bioindigenous";
 //var connectionString = "mongodb://indigenous:sLRxKmiG42sq@novus.modulusmongo.net:27017/H2inesux";
-//var connectionString = "mongodb://indigenous:33e90274a9d44740f26e3c70341aebce@kahana.mongohq.com:10077/indigenous”;
+var connectionString = "mongodb://indiweb:anime1@kahana.mongohq.com:10077/indigenous";
 
-if (process.env.MONGO_CONNECT != null) {
-    connectionString = process.env.MONGO_CONNECT;
-}
+
+// if (process.env.MONGO_CONNECT != null) {
+//     connectionString = process.env.MONGO_CONNECT;
+// }
 
 module.exports = {
     MONGODB_CONNECT: connectionString
 };
-
-
