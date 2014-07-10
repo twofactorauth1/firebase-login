50a568c3d92f780a4b6a62297a0f0bdd93229798
Kyle Miller <millkyl@gmail.com>
2014-07-10 10:35:11 -0400

    Fix Stripe Oauth delegation.

---
+++ authentication/oauth/index.js
@@ -0,0 +1,15 @@
+/**
+ * Module dependencies.
+ */
+var Strategy = require('./stripe_strategy');
+
+
+/**
+ * Framework version.
+ */
+//require('pkginfo')(module, 'version');
+
+/**
+ * Expose constructors.
+ */
+exports.Strategy = Strategy;
