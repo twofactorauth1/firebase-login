45dfa19f81986ecabe3e4dec1d75628a34446905
zeeshanhanif <zeeshanhanif@gmail.com>
2014-07-10 17:03:58 +0500

    #21 Front end form submission for potential customer. Submission to mailchimp removed

--- routers/home.server.router.js
+++ routers/home.server.router.js
@@ -148,26 +148,20 @@ _.extend(router.prototype, BaseRouter.prototype, {
 
     signUpNews: function(req, resp) {
         var self = this, contact, accountToken, deferred;
-
+        console.log(req.body);
         var email = req.body.email;
         console.log('Email: '+JSON.stringify(email));
 
         var accountToken = cookies.getAccountToken(req);
         console.log('Account Token: '+accountToken);
 
-        contactDao.createContactFromEmail(email, accountToken, function (err, value) {
+        contactDao.createContactFromData(req.body, accountToken, function (err, value) {
             if (!err) {
-                req.login(value, function (err) {
-                    if (err) {
-                        return resp.redirect("/");
-                    } else {
-                        req.flash("info", "Account created successfully");
-                        return resp.redirect("/");
-                    }
-                });
+                req.flash("info", "Account created successfully");
+                return resp.redirect("/");
             } else {
                 req.flash("error", value.toString());
-                return resp.redirect("/signup");
+                return resp.redirect("/");
             }
         });
     }
