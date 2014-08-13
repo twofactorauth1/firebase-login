53b0e2e45dbed894b9e59f0ddfeb38ecc95f2e28
Kyle Miller <millkyl@gmail.com>
2014-08-13 14:32:03 -0500

    Disable api tests because of necessary hosts file configuration.

--- gruntfile.js
+++ gruntfile.js
@@ -185,8 +185,7 @@ module.exports = function(grunt) {
     grunt.registerTask('compiletemplates', ['compilehbs', 'handlebars','clean:hbs']);
     grunt.registerTask('production',['clean:prebuild','less','requirejs','clean:postbuild']);
 
-    grunt.registerTask('tests', ['nodeunit:api', 'nodeunit:biometricsPlatform', 'nodeunit:contacts', 'nodeunit:twonet',
-        'nodeunit:utils']);
+    grunt.registerTask('tests', ['nodeunit:biometricsPlatform', 'nodeunit:contacts', 'nodeunit:twonet', 'nodeunit:utils']);
     grunt.registerTask('testContextio', ['nodeunit:contextio']);
     grunt.registerTask('testBiometricsPlatform', ['nodeunit:biometricsPlatform']);
     grunt.registerTask('testTwonetclient', ['nodeunit:twonetclient']);
