6b1018d8fe65c51229da251138786a248c252da1
Kyle Miller <millkyl@gmail.com>
2014-08-18 14:36:30 -0500

    Add product skeleton

---
+++ products/model/product.js
@@ -0,0 +1,6 @@
+/**
+ * COPYRIGHT INDIGENOUS SOFTWARE, INC., LLC 2014
+ *
+ * All use or reproduction of any or all of this content must be approved.
+ * Please contact info@indigenous.io for approval or questions.
+ */
