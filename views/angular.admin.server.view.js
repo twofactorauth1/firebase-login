e13f0b31e32485303f5e22126b8a83d1579cf91d
Jaideep Das <jaideep.jdof@gmail.com>
2014-08-22 15:30:51 +0530

    setting up a view for angular admin

---
+++ views/angular.admin.server.view.js
@@ -0,0 +1,44 @@
+/**
+ * COPYRIGHT INDIGENOUS SOFTWARE, INC., LLC 2014
+ *
+ * All use or reproduction of any or all of this content must be approved.
+ * Please contact info@indigenous.io for approval or questions.
+ */
+
+var BaseView = require('./base.server.view');
+
+var view = function(req,resp,options) {
+    this.init.apply(this, arguments);
+};
+
+_.extend(view.prototype, BaseView.prototype, {
+
+    show: function(root) {
+        var data = {
+            router:"account/admin",
+            root:root || "admin",
+            location:"admin",
+            includeHeader:true,
+            includeFooter:true
+        };
+
+        var self = this;
+        this.getAccount(function(err, value) {
+            if (!err && value != null) {
+                data.account = value.toJSON();
+            }
+
+            data.showPreloader = true;
+            data.includeJs = false;
+            data = self.baseData(data);
+
+            self.resp.render('admin', data);
+            self.cleanUp();
+            data = self = null;
+        });
+    }
+});
+
+$$.v.AdminView = view;
+
+module.exports = view;
