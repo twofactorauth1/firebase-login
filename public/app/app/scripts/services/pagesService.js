ea885c70f552df142a66471339c61a56a863c823
Kashish Gupta <kashishgupta1990@yahoo.com>
2014-08-23 14:59:50 +0530

    filled data with temp page service data

--- public/app/app/scripts/services/pagesService.js
+++ public/app/app/scripts/services/pagesService.js
@@ -29,67 +29,67 @@ mainApp.factory('pagesService', function () {
                         "_id": "23ad6ee5-66ba-4a65-a82b-a187242599f3",
                         "anchor": null,
                         "type": "freeform",
-                        "label": "",
+                        "label": "Free Form Label",
                         "description": "",
-                        "value": ""
+                        "value": "Free Form Value"
                     },
                     {
                         "_id": "b9d284c4-48f0-4718-a12b-210b8c73df60",
                         "anchor": null,
                         "type": "contact-us",
-                        "label": "",
-                        "description": "",
+                        "label": "Contact US",
+                        "description": "you can contact us any time on number give below",
                         "hours": [ ],
                         "location": {
-                            "address": "",
-                            "address2": "",
-                            "city": "",
-                            "state": "",
-                            "zip": "",
-                            "lat": "",
-                            "lon": "",
+                            "address": "B-4 82 Paschim Vihar New Delhi",
+                            "address2": "B-1 90 Rajori Garden, Patalia",
+                            "city": "City",
+                            "state": "State",
+                            "zip": "110063",
+                            "lat": "112121212",
+                            "lon": "332131312",
                             "showMap": false,
-                            "addressDisplayLabel": ""
+                            "addressDisplayLabel": "display label"
                         },
                         "contact": {
-                            "email": "",
-                            "phone": ""
+                            "email": "kashish.gupta@intelligrape.com",
+                            "phone": "9999749722"
                         }
                     },
                     {
                         "_id": "1e2b6078-c1d2-4550-8747-35eb74c8d763",
                         "anchor": null,
                         "type": "feature-blocks",
-                        "label": "",
-                        "description": "",
+                        "label": "Feature Block",
+                        "description": "Feature Block Description",
                         "features": [ ]
                     },
-                    {
+                    /*{
                         "_id": "09bf4453-e567-4263-9042-cb2e9eb1adf2",
                         "anchor": null,
                         "type": "feature-list",
-                        "label": "",
-                        "description": "",
+                        "label": "feature-list-label",
+                        "description": "feature-list-description",
                         "features": [ ]
-                    },
-                    {
+                    },*/
+                    /*{
                         "_id": "86663e44-c1d2-4743-b1b1-3a42d2f6ad5e",
                         "anchor": null,
                         "type": "image-gallery",
-                        "label": "",
-                        "description": "",
+                        "label": "image-gallery-label",
+                        "description": "image-gallery-description",
                         "imageSize": "medium",
                         "source": null,
                         "images": [ ]
-                    },
-                    {
+                    },*/
+                    /*{
                         "_id": "fadf8b54-6c03-4d43-9a3e-6338ddc147d4",
                         "anchor": null,
                         "type": "image-slider",
-                        "label": "",
-                        "description": "",
+                        "label": "image-slider-label",
+                        "description": "image-slider-description",
                         "images": [ ]
-                    }
+                    }*/
                 ],
                 "created": {
                     "date": 1408599403942,
