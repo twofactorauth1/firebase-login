cf7edbb3a9fd896b6f3051d040ff232decdb6f55
Brad Risse <bradly.risse@gmail.com>
2014-08-25 21:57:38 -0700

    Account UI
    Added Account UI into new Angular section #185

--- public/angular_admin/controllers/account.js
+++ public/angular_admin/controllers/account.js
@@ -2,6 +2,7 @@ define(['app', 'apiService'], function(app) {
     app.controller('AccountCtrl', ['$scope', 'ApiService', function ($scope, ApiService) {
     	ApiService.getUser(function (user) {
     		$scope.user = user;
+    		$scope.activeTab = 'account';
     	});
     }]);
 });
