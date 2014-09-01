ab020143ec72eda7f8f9597809474efe53395f76
Kashish Gupta <kashishgupta1990@yahoo.com>
2014-09-01 11:26:39 +0530

    #62 AngularApp MongoDb Integration

--- public/app/app/scripts/filters/CreateUrlFilter.js
+++ public/app/app/scripts/filters/CreateUrlFilter.js
@@ -1,7 +1,4 @@
-mainApp.filter('CreateUrlFilter', ['accountService', function (accountService) {
-    console.log('i m filter');
-    var componentURL;
-
+mainApp.filter('createUrlFilter', ['accountService', function (accountService) {
     return function (obj) {
         if (obj) {
             accountService(function (err, data) {
@@ -9,15 +6,12 @@ mainApp.filter('CreateUrlFilter', ['accountService', function (accountService) {
                     console.log('Error ' + err);
                     return err;
                 } else {
-                    console.log(data, "==========data=======");
-                    obj.forEach(function (component) {
-                        component['url'] = 'components/' + component.type + '/'+ component.type + '_'+ data.website.themeId + '.html';
-                        console.log(component.url);
+                    obj.forEach(function (cp) {
+                        cp['url'] = 'components/' + cp.type + '/'+ cp.type + '_'+ data.website.themeId + '.html';
                     });
-                    console.log("@#@##############@@@@",obj);
-                    return obj;
                 }
             });
         }
+        return obj;
     }
 }]);
