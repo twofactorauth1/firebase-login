256fdb3c0306b78c702f0cfaa17f87e582b09116
Kashish Gupta <kashishgupta1990@yahoo.com>
2014-09-08 16:19:08 +0530

    #62 refactor theme layout

--- public/app/scripts/directives/parallax.js
+++ public/app/scripts/directives/parallax.js
@@ -4,8 +4,8 @@
 
 'use strict';
 
-angular.module('mainApp', ['duParallax']).
-  controller('MyCtrl', function($scope, parallaxHelper){
+mainApp.controller('MyCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
+
     $scope.background = parallaxHelper.createAnimator(-0.3, 150, -150);
-  }
-);
+
+}]);
