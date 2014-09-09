83b33d025c23ca8851fe342581befe68ba685d4b
Dhiraj Sharma <dhiraj.sharma@intelligrape.com>
2014-09-09 14:57:26 +0530

    #62 pageService and meet-team_v1 component refactored

--- public/app/scripts/services/pagesService.js
+++ public/app/scripts/services/pagesService.js
@@ -30,55 +30,56 @@ mainApp.factory('pagesService', ['websiteService', '$http', function (websiteSer
                     },
                     "components": [
                         {
-                         "_id": "f43b987f-9164-4b08-8a27-57471e789016",
-                         "anchor": null,
-                         "type": "masthead",
-                         "label": "Masthead",
-                         "description": "This is the masthead description",
-                         "title": "Introducing <b>INDIGENOUS!</b>",
-                         "subtitle": "Indigenous is a fully integrated business management platform&nbsp;<br>for independent service professionals and small businesses<br>",
-                         "bg": {
-                         "img": {
-                         "url": "/assets/images/indimain/home-top.jpg",
-                         "width": 2716,
-                         "height": 1810,
-                         "parallax": true,
-                         "blur": false
-                         },
-                         "color": ""
-                         },
-                         "btn": {
-                         "text": "Learn More",
-                         "url": "#features",
-                         "icon": "fa fa-email"
-                         }
-                         },
-                         {
-                         "_id": "s32d253h-8675-2x06-2v68-65431s970864",
-                         "anchor": "customers",
-                         "type": "feature-block",
-                         "label": "",
-                         "description": "",
-                         "value": "",
-                         "title": "<span class='fa fa-users'></span>",
-                         "subtitle": "We’re launching our first customers now! Check back here and on <a href='/page/blog'>our blog</a> to learn more about how Indigenous has helped their businesses.",
-                         "text": "",
-                         "bg": {
-                         "img": {
-                         "url": "",
-                         "width": 800,
-                         "height": 200,
-                         "parallax": false,
-                         "blur": false
-                         },
-                         "color": "#4bb0cb"
-                         },
-                         "btn": {
-                         "text": "Btn Text",
-                         "url": "http://google.com",
-                         "icon": "fa fa-rocket"
-                         }
-                         },
+                            "_id": "f43b987f-9164-4b08-8a27-57471e789016",
+                            "anchor": null,
+                            "type": "masthead",
+                            "label": "Masthead",
+                            "description": "This is the masthead description",
+                            "title": "Introducing <b>INDIGENOUS!</b>",
+                            "subtitle": "Indigenous is a fully integrated business management platform&nbsp;<br>for independent service professionals and small businesses<br>",
+                            "bg": {
+                                "img": {
+                                    "url": "/assets/images/indimain/home-top.jpg",
+                                    "width": 2716,
+                                    "height": 1810,
+                                    "parallax": true,
+                                    "blur": false
+                                },
+                                "color": ""
+                            },
+                            "btn": {
+                                "text": "Learn More",
+                                "url": "#features",
+                                "icon": "fa fa-email"
+                            }, "version": 1
+                        },
+                        {
+                            "_id": "s32d253h-8675-2x06-2v68-65431s970864",
+                            "anchor": "customers",
+                            "type": "feature-block",
+                            "label": "",
+                            "description": "",
+                            "value": "",
+                            "title": "<span class='fa fa-users'></span>",
+                            "subtitle": "We’re launching our first customers now! Check back here and on <a href='/page/blog'>our blog</a> to learn more about how Indigenous has helped their businesses.",
+                            "text": "",
+                            "bg": {
+                                "img": {
+                                    "url": "",
+                                    "width": 800,
+                                    "height": 200,
+                                    "parallax": false,
+                                    "blur": false
+                                },
+                                "color": "#4bb0cb"
+                            },
+                            "btn": {
+                                "text": "Btn Text",
+                                "url": "http://google.com",
+                                "icon": "fa fa-rocket"
+                            },
+                            "version": 1
+                        },
                         {
                             "_id": "a21b456q-5564-1g87-6j43-22123j887656",
                             "anchor": "features",
@@ -122,20 +123,127 @@ mainApp.factory('pagesService', ['websiteService', '$http', function (websiteSer
                                 "text": "Learn More",
                                 "url": "#features",
                                 "icon": "fa fa-email"
-                            }
+                            },
+                            "version": 1
                         },
                         {
-                            "_id": "j45k442s-0034-2f09-4h65-33423h8894778",
+                            "_id": "00000000-0000-0000-0000-000000000000",
                             "anchor": "blog",
                             "type": "blog-teaser",
                             "title": "Check out our blog",
-                            "subtitle": "",
+                            "subtitle": "This is the greatest blog!",
+
+                            //posts is array of Id according to id we fetch post from post Model
+                            //For testing purpose we contain whole post in posts
+
                             "posts": [
-                                "820067b3-5a90-4bcc-94d9-81019027c900"
+                                {
+                                    "_id": "00000000-0000-0000-0000-000000000001",
+                                    "accountId": "6",
+                                    websiteId: "4s4r2sx2-3g54-3f5h-44f3-erer3d22ss3s",
+                                    post_author: "Noel Norcross",
+                                    post_title: "What's an Indigenous?",
+                                    post_content: "<p>The problem with giving your company an adjective for a name is that it imputes the essence</p>",
+                                    post_excerpt: "The problem with giving your company an adjective",
+                                    post_status: "Publish",
+                                    post_url: "welcome-to-indigenous",
+                                    post_tags: [
+                                        "Social Media",
+                                        "Customer Service",
+                                        "Marketing",
+                                        "Pricing"
+                                    ],
+                                    post_category: "Marketing",
+                                    comment_status: "on",
+                                    comment_count: "2",
+                                    created: {
+                                        date: 1397761951291,
+                                        by: null
+                                    },
+                                    modified: {
+                                        date: "",
+                                        by: null
+                                    }
+                                }
                             ],
+                            "bg": {
+                                "img": {
+                                    "url": "http://www.site.com/images/bgimage.jpg",
+                                    "width": 1400,
+                                    "height": 836,
+                                    "parallax": true,
+                                    "blur": true
+                                },
+                                "color": "#000000"
+                            },
+                            "btn": {
+                                "text": "Btn Text",
+                                "url": "http://google.com",
+                                "icon": "fa fa-rocket"
+                            },
+                            "version": 1
+                        },
+                        {
+                            "_id": "d12b442s-0034-2f09-4h65-33423h8894778",
+                            "anchor": null,
+                            "type": "signup-form",
+                            "formName": "",
+                            "contactType": "",
+                            "fields": [],
+                            "title": "Sign Up For News Updates",
+                            "btntext": "I'M INTERESTED",
                             "label": "",
                             "description": "",
-                            "value": ""
+                            "version": 1
+                        },
+                        {
+                            "_id": "c12b442s-0034-2f09-4h65-33423h674832",
+                            "anchor": "team",
+                            "type": "meet-team",
+                            "title": " Team",
+                            "teamMembers": [
+                                {
+                                    "name": "Mike Trevino",
+                                    "position": "Chief Executive Officer",
+                                    "profilepic": "/assets/images/indimain/team/mike.jpg",
+                                    "bio": "Mike is a senior business leader with global experience developing high growth companies and a focus on delivering advanced software.",
+                                    "social": {
+                                        "linkedin": "http://www.linkedin.com/pub/mike-trevino/0/6b2/856"
+                                    }
+                                },
+                                {
+                                    "name": "",
+                                    "position": "",
+                                    "profilepic": "/assets/images/indimain/team/become.png",
+                                    "bio": "The bio.",
+                                    "become-member": "true"
+                                },
+                                {
+                                    "_id": "g98d442s-0034-2f09-4h65-33423h8894778",
+                                    "anchor": null,
+                                    "type": "social",
+                                    "networks": {
+                                        "facebook": "https://www.facebook.com/indigenous.io",
+                                        "twitter": "https://twitter.com/indigenous_io",
+                                        "google-plus": "https://plus.google.com/u/0/105022706688618584400/posts/p/pub",
+                                        "pinterest": "http://www.pinterest.com/indigenousdotio/"
+                                    }
+                                }
+                            ],
+                            "value": "",
+                            "version": 1
+                        },
+                        {
+                            "_id": "g98d442s-0034-2f09-4h65-33423h8894778",
+                            "anchor": null,
+                            "type": "social",
+                            "networks": {
+                                "facebook": "https://www.facebook.com/indigenous.io",
+                                "twitter": "https://twitter.com/indigenous_io",
+                                "google-plus": "https://plus.google.com/u/0/105022706688618584400/posts/p/pub",
+                                "pinterest": "http://www.pinterest.com/indigenousdotio/"
+                            },
+                            "version": 1
                         }
 
                     ],
