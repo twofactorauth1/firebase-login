2882348f32521baeb930c7b42535bb51fbed7f96
Nikolay Shikov <nik.shikov@gmail.com>
2014-09-11 19:37:16 +0300

    IN PROGRESS: Porting VAR functionality(new design for landing page)

--- public/pipeshift/js/modules/video/directive/coursePreview.js
+++ public/pipeshift/js/modules/video/directive/coursePreview.js
@@ -7,11 +7,29 @@ angular.module('app.directives').directive('coursePreview', function () {
         scope: {
             editMode: "=",
             course: "=",
-            minHeight: "="
+            minHeight: "=",
+            isLoggedInAndSubscribed: "=",
+            standalone: "="
         },
-        controller: function ($scope, $http) {
+        controller: function ($scope, $http, $location, $modal) {
             var course = $scope.course;
             $scope.modal = {};
+            var carouselData = [];
+            var courseVideos = $scope.course.videos;
+            if (courseVideos.length > 0) {
+                $scope.selectedVideo = courseVideos[0];
+                for (var i = 0; i < Math.ceil(courseVideos.length / 3); i++) {
+                    var pageData = [];
+                    for (var j = 0; j < 3; j++) {
+                        var video = courseVideos[i * 3 + j];
+                        if (video != null) {
+                            pageData.push(video);
+                        }
+                    }
+                    carouselData.push(pageData);
+                }
+            }
+            $scope.carouselData = carouselData;
             $scope.subscribe = function () {
                 if (course.subdomain == null || course.subdomain.trim() == "") {
                     alert("Please define course subdomain first.");
@@ -21,15 +39,54 @@ angular.module('app.directives').directive('coursePreview', function () {
                         if (course.videos.length == 0) {
                             alert("Error: empty course");
                         } else {
-                            $http.post('/api/1.0/campaignmanager/pipeshift/courses/' + course._id + '/subscribe/', {email: $scope.modal.email, course: course, timezoneOffset: getTimezoneOffset()}).success(function (data) {
-                                alert("Course has been scheduled for " + $scope.modal.email);
-                            }).error(function (data) {
-                                alert("Some error happened.");
-                            });
+                            subscribeToCourse($scope.modal.email, course);
                         }
                     }
                 }
             };
+            function subscribeToCourse(email, course) {
+                $http.post('/api/1.0/campaignmanager/pipeshift/courses/' + course._id + '/subscribe/', {email: $scope.modal.email, course: course, timezoneOffset: getTimezoneOffset()}).success(function (data) {
+                    if (data.success) {
+                        alert("Course has been scheduled for " + email);
+                    } else {
+                        alert(data.error);
+                    }
+                }).error(function (data) {
+                    alert("Some error happened.");
+                });
+            }
+
+            $scope.selectVideo = function (video) {
+                $http.get("/api/1.0/campaignmanager/pipeshift/courses/" + course._id + "/subscribers/video/" + video._id).success(function (response) {
+                    if (response.success) {
+                        $scope.selectedVideo = response.result;
+                        var $videoPlayerContainer = $("#videoPlayerContainer");
+                        $videoPlayerContainer.empty();
+                        $videoPlayerContainer.html("<div video-player video='selectedVideo' courseDetails='course'></div>");
+                        $compile($videoPlayerContainer)($scope);
+                    }
+                })
+            }
+            $scope.videoId = $location.search().videoId;
+            if ($scope.videoId != null) {
+                $scope.selectVideo({_id: $scope.videoId});
+            }
+            if ($scope.standalone && !$scope.isLoggedInAndSubscribed && course.showExitIntentModal) {
+                ouibounce($('#exit_intent_modal')[0], {aggressive: true});
+            }
+            $scope.share = function (shareUrl) {
+                window.open(shareUrl + $location.absUrl(), '_blank', 'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0');
+            }
+            $scope.showCourseSubscribeModal = function () {
+                var modalInstance = $modal.open({
+                    templateUrl: '/views/modal/subscribeModal.html',
+                    controller: 'CourseSubscribeModalController',
+                });
+                modalInstance.result.then(function (email) {
+                    subscribeToCourse(email, course);
+                }, function () {
+                });
+            }
         },
         replace: true,
         restrict: 'E',
