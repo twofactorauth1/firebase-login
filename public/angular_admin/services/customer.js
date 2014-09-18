31d97b770f2026046f423a72c98d9cac53f3936e
Jaideep Das <jaideepdas@localhost.localdomain>
2014-09-19 00:46:26 +0530

    customer section customer listing and search bar filter

--- public/angular_admin/services/customer.js
+++ public/angular_admin/services/customer.js
@@ -1,5 +1,12 @@
-define(['app', 'stripe'], function (app) {
+define(['app'], function (app) {
     app.register.service('CustomerService', function ($http) {
-        
+        var baseUrl = '/api/1.0/';
+        this.getCustomers = function (fn) {
+            var apiUrl = baseUrl + ['contact'].join('/');
+            $http.get(apiUrl)
+            .success(function (data, status, headers, config) {
+                fn(data);
+            });
+        };
     });
 });
