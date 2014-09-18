af1fde4578a6f1ef0778351741b0870359b1a330
Nikolay Shikov <nik.shikov@gmail.com>
2014-09-18 18:43:49 +0300

    IN PROGRESS: Multiple App.js Files #281(redirect to /admin1)

--- public/js/routers/account/admin.router.js
+++ public/js/routers/account/admin.router.js
@@ -8,14 +8,13 @@
 define([
     'routers/account/contact.router',
     'routers/account/account.router',
-    'routers/account/pipeshift.router',
     'routers/account/commerce.router',
     'routers/account/social_dashboard.router',
     'routers/account/cms.router',
     'views/account/admin.view',
     'views/account/dashboard.view',
     'views/account/contact.view'
-], function (contactRouter, accountRouter, pipeshiftRouter, commerceRouter, cmsRouter, social_dashboardRouter, AdminView, DashboardView, ContactView) {
+], function (contactRouter, accountRouter, commerceRouter, cmsRouter, social_dashboardRouter, AdminView, DashboardView, ContactView) {
 
     var router = Backbone.Router.extend({
 
