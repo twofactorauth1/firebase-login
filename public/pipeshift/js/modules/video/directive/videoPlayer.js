017475bf0b94c42666dc2d55e21cf6184feae87e
Nikolay Shikov <nik.shikov@gmail.com>
2014-09-19 19:32:13 +0300

    Bugfixes for VAR functionality

--- public/pipeshift/js/modules/video/directive/videoPlayer.js
+++ public/pipeshift/js/modules/video/directive/videoPlayer.js
@@ -4,9 +4,11 @@ angular.module('var.directives').directive('videoPlayer', function () {
             video: "="
         },
         controller: function ($scope, $sce) {
-            $scope.video.embedurl = "http://www.youtube.com/embed/" + $scope.video.videoId + "?autoplay=0&theme=light&color=white&iv_load_policy=3";
-            $scope.trustSrc = function (src) {
-                return $sce.trustAsResourceUrl(src);
+            if ($scope.video) {
+                $scope.video.embedurl = "http://www.youtube.com/embed/" + $scope.video.videoId + "?autoplay=0&theme=light&color=white&iv_load_policy=3";
+                $scope.trustSrc = function (src) {
+                    return $sce.trustAsResourceUrl(src);
+                }
             }
         },
         replace: true,
