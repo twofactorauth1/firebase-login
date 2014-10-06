d207258de882134be0c1e2191fc03bf0c3a41842
Jaideep Das <jaideep.jdof@gmail.com>
2014-10-06 19:21:09 +0530

    dashboard serialization WIP

--- public/angular_admin/controllers/dashboard.js
+++ public/angular_admin/controllers/dashboard.js
@@ -6,18 +6,25 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
             self.next().toggleClass('open');
             self.find('.arrow').toggleClass('open closed');
     	});
-    	
+
     	$('.module_sidebar').draggable({
-    		helper: 'clone', 
-    		revert: true
+    		helper: 'clone',
+    		revert: true,
+            stop: function (event, ui) {
+                console.log(chartGrid.serialize());
+            }
     	});
-    	
+
     	var chartGrid = $(".gridster ul").gridster({
     		widget_margins: [20, 20],
         	widget_base_dimensions: [400, 200],
-            shift_larger_widgets_down: false
+            shift_larger_widgets_down: false,
+            serialize_params: function ($w, wgd) {
+                return {col: wgd.col, row: wgd.row, size_x: wgd.size_x, size_y: wgd.size_y,
+                        class: $w.attr('class'), create: (!$w.hasClass('preview-holder'))};
+            }
     	}).data('gridster');
-    	
+
     	var charts = {};
     	charts.ST_plan_creations = function (boxId) {
     		PaymentService.getListPlans(function (plans) {
@@ -33,12 +40,12 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
     					chartData[entryDate] = 1;
     				}
     			});
-    			
+
     			$.each(chartData, function (k, v) {
     				xAxis.push(k);
     				xValue.push(v);
     			});
-    			
+
 	    		c3.generate({
 					bindto: '.' + boxId,
 					data: {
@@ -59,7 +66,7 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 				});
     		});
     	};
-    	
+
     	charts.ST_plan_subscriptions = function (boxId) {
     		var planData = {};
     		PaymentService.getListPlans(function (plans) { //TODO: add promise lib here
@@ -97,7 +104,7 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
     			});
     		});
     	};
-    	
+
         charts.TN_prolific_bio = function (boxId) {
     		TwoNetService.getBodyMeasurement(824, function (bios) {
     			var xAxis = ['x'];
@@ -109,7 +116,7 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
                     xAxis.push(entryDate);
                     xValue.push(parseFloat(value.values[0].value));
     			});
-    			
+
 	    		c3.generate({
 					bindto: '.' + boxId,
 					data: {
@@ -143,7 +150,7 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
                     xAxis.push(entryDate);
                     xValue.push(parseFloat(value.values[0].value));
     			});
-    			
+
 	    		c3.generate({
 					bindto: '.' + boxId,
 					data: {
@@ -165,7 +172,7 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 				});
     		});
     	};
-    	
+
 		charts.FB_overview = function(boxId) {
 		    c3.generate({
 		        bindto : '.' + boxId,
@@ -184,34 +191,34 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		        }
 		    });
 		};
-				
+
 		charts.FB_likesPerDay = function(boxId) {
 		    c3.generate({
 		        bindto : '.' + boxId,
 		        data : {
 		            json:[{
-		                date: '2014-06-20', 
-		                likes: '2', 
+		                date: '2014-06-20',
+		                likes: '2',
 		                unlikes: '0'
 		            }
 		            , {
-		                date: '2014-06-21', 
-		                likes: '4', 
+		                date: '2014-06-21',
+		                likes: '4',
 		                unlikes: '1'
 		            }
 		            , {
-		                date: '2014-06-22', 
-		                likes: '7', 
+		                date: '2014-06-22',
+		                likes: '7',
 		                unlikes: '2'
 		            }
 		            , {
-		                date: '2014-06-24', 
-		                likes: '5', 
+		                date: '2014-06-24',
+		                likes: '5',
 		                unlikes: '2'
 		            }
 		            , {
-		                date: '2014-06-25', 
-		                likes: '6', 
+		                date: '2014-06-25',
+		                likes: '6',
 		                unlikes: '1'
 		            }],
 		            keys: {
@@ -228,46 +235,46 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		            }
 		        }
 		    });
-		};  
-				
+		};
+
 		charts.FB_postTimeline = function(boxId) {
 		    c3.generate({
 		        bindto : '.' + boxId,
 		        data : {
 		        	type : 'pie',
 		            json:[ {
-		                date: '2014-06-21', 
-		                likes: '2', 
-		                shares: '2', 
-		                comments: '1', 
+		                date: '2014-06-21',
+		                likes: '2',
+		                shares: '2',
+		                comments: '1',
 		                title: 'Post title'
 		            }
 		            , {
-		                date: '2014-06-22', 
-		                likes: '4', 
-		                shares: '1', 
-		                comments: '1', 
+		                date: '2014-06-22',
+		                likes: '4',
+		                shares: '1',
+		                comments: '1',
 		                title: 'Post title'
 		            }
 		            , {
-		                date: '2014-06-23', 
-		                likes: '7', 
-		                shares: '2', 
-		                comments: '1', 
+		                date: '2014-06-23',
+		                likes: '7',
+		                shares: '2',
+		                comments: '1',
 		                title: 'Post title'
 		            }
 		            , {
-		                date: '2014-06-24', 
-		                likes: '5', 
-		                shares: '3', 
-		                comments: '1', 
+		                date: '2014-06-24',
+		                likes: '5',
+		                shares: '3',
+		                comments: '1',
 		                title: 'Post title'
 		            }
 		            , {
-		                date: '2014-06-25', 
-		                likes: '1', 
-		                shares: '1', 
-		                comments: '1', 
+		                date: '2014-06-25',
+		                likes: '1',
+		                shares: '1',
+		                comments: '1',
 		                title: 'Post title'
 		            }],
 		            keys: {
@@ -276,87 +283,87 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		        }
 		    });
 		};
-				
+
 		charts.FB_postInteractionsPerDay = function(boxId) {
 		    c3.generate({
 		        bindto : '.' + boxId,
 		        data : {
 		            json:[{
-		                date: '2014-06-21', 
-		                likes: 2, 
-		                shares: 2, 
+		                date: '2014-06-21',
+		                likes: 2,
+		                shares: 2,
 		                comments: 1
 		            }
 		            , {
-		                date: '2014-06-22', 
-		                likes: 4, 
-		                shares: 1, 
+		                date: '2014-06-22',
+		                likes: 4,
+		                shares: 1,
 		                comments: 2
 		            }
 		            , {
-		                date: '2014-06-23', 
-		                likes: 7, 
-		                shares: 2, 
+		                date: '2014-06-23',
+		                likes: 7,
+		                shares: 2,
 		                comments: 3
 		            }
 		            , {
-		                date: '2014-06-24', 
-		                likes: 5, 
-		                shares: 3, 
+		                date: '2014-06-24',
+		                likes: 5,
+		                shares: 3,
 		                comments: 1
 		            }
 		            , {
-		                date: '2014-06-25', 
-		                likes: 3, 
-		                shares: 4, 
+		                date: '2014-06-25',
+		                likes: 3,
+		                shares: 4,
 		                comments: 2
 		            }
 		            , {
-		                date: '2014-06-26', 
-		                likes: 2, 
-		                shares: 5, 
+		                date: '2014-06-26',
+		                likes: 2,
+		                shares: 5,
 		                comments: 2
 		            }
 		            , {
-		                date: '2014-06-27', 
-		                likes: 5, 
-		                shares: 6, 
+		                date: '2014-06-27',
+		                likes: 5,
+		                shares: 6,
 		                comments: 4
 		            }
 		            , {
-		                date: '2014-06-28', 
-		                likes: 8, 
-		                shares: 5, 
+		                date: '2014-06-28',
+		                likes: 8,
+		                shares: 5,
 		                comments: 3
 		            }
 		            , {
-		                date: '2014-06-29', 
-		                likes: 4, 
-		                shares: 5, 
+		                date: '2014-06-29',
+		                likes: 4,
+		                shares: 5,
 		                comments: 4
 		            }
 		            , {
-		                date: '2014-06-10', 
-		                likes: 5, 
-		                shares: 6, 
+		                date: '2014-06-10',
+		                likes: 5,
+		                shares: 6,
 		                comments: 5
 		            }
 		            , {
-		                date: '2014-06-11', 
-		                likes: 6, 
-		                shares: 7, 
+		                date: '2014-06-11',
+		                likes: 6,
+		                shares: 7,
 		                comments: 6
 		            }
 		            , {
-		                date: '2014-06-12', 
-		                likes: 7, 
-		                shares: 8, 
+		                date: '2014-06-12',
+		                likes: 7,
+		                shares: 8,
 		                comments: 7
 		            }
 		            , {
-		                date: '2014-06-13', 
-		                likes: 8, 
-		                shares: 9, 
+		                date: '2014-06-13',
+		                likes: 8,
+		                shares: 9,
 		                comments: 8
 		            }],
 		            keys: {
@@ -374,64 +381,64 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		        }
 		    });
 		};
-				
+
 		charts.FB_reachPerDay = function(boxId) {
 		    c3.generate({
 		        bindto : '.' + boxId,
 		        data : {
 		        	type: 'bar',
 		            json:[{
-		                date: '2014-06-21', 
-		                paid: 5000, 
-		                organic: 2000, 
+		                date: '2014-06-21',
+		                paid: 5000,
+		                organic: 2000,
 		                viral: 1000
 		            }
 		            , {
-		                date: '2014-06-22', 
-		                paid: 8000, 
-		                organic: 1000, 
+		                date: '2014-06-22',
+		                paid: 8000,
+		                organic: 1000,
 		                viral: 200
 		            }
 		            , {
-		                date: '2014-06-23', 
-		                paid: 4000, 
-		                organic: 2000, 
+		                date: '2014-06-23',
+		                paid: 4000,
+		                organic: 2000,
 		                viral: 300
 		            }
 		            , {
-		                date: '2014-06-24', 
-		                paid: 5000, 
-		                organic: 3000, 
+		                date: '2014-06-24',
+		                paid: 5000,
+		                organic: 3000,
 		                viral: 100
 		            }
 		            , {
-		                date: '2014-06-25', 
-		                paid: 3000, 
-		                organic: 4000, 
+		                date: '2014-06-25',
+		                paid: 3000,
+		                organic: 4000,
 		                viral: 200
 		            }
 		            , {
-		                date: '2014-06-26', 
-		                paid: 2000, 
-		                organic: 5000, 
+		                date: '2014-06-26',
+		                paid: 2000,
+		                organic: 5000,
 		                viral: 200
 		            }
 		            , {
-		                date: '2014-06-27', 
-		                paid: 8000, 
-		                organic: 6000, 
+		                date: '2014-06-27',
+		                paid: 8000,
+		                organic: 6000,
 		                viral: 400
 		            }
 		            , {
-		                date: '2014-06-28', 
-		                paid: 4000, 
-		                organic: 5000, 
+		                date: '2014-06-28',
+		                paid: 4000,
+		                organic: 5000,
 		                viral: 700
 		            }
 		            , {
-		                date: '2014-06-29', 
-		                paid: 4000, 
-		                organic: 7000, 
+		                date: '2014-06-29',
+		                paid: 4000,
+		                organic: 7000,
 		                viral: 6000
 		            }
 		            ],
@@ -450,81 +457,81 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		        }
 		    });
 		};
-				
-				
+
+
 		charts.FB_topTenPosts = function(boxId) {
 		    var chart = c3.generate({
 		        bindto : '.' + boxId,
 		        data: {
 		            json: [
 		            {
-		                date: '2014-06-20', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-20',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 40087
 		            }
 		            , {
 		                date: '2014-06-29',
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 30087
 		            }
 		            , {
-		                date: '2014-06-28', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-28',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 20087
 		            }
 		            , {
-		                date: '2014-06-27', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-27',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 9087
 		            }
 		            , {
-		                date: '2014-06-26', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-26',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 887
 		            }
 		            , {
-		                date: '2014-06-25', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-25',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 787
 		            }
 		            , {
-		                date: '2014-06-24', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-24',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 687
 		            }
 		            , {
-		                date: '2014-06-23', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-23',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 587
 		            }
 		            , {
 		                date: '2014-06-22',
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 487
 		            }
 		            , {
-		                date: '2014-06-21', 
-		                message: 'Hello World', 
-		                reach: 71213, 
-		                engaged: 1347, 
+		                date: '2014-06-21',
+		                message: 'Hello World',
+		                reach: 71213,
+		                engaged: 1347,
 		                talking: 387
 		            }
 		            ],
@@ -542,8 +549,8 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		            }
 		        }
 		    });
-		
-		}; 
+
+		};
 		charts.FB_engagedDemographics = function(boxId) {
 		    var chart = c3.generate({
 		        bindto : '.' + boxId,
@@ -551,28 +558,28 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		            json: [
 		            {
 		                male: 12,
-		                female: 14, 
+		                female: 14,
 		                range: '25-34'
 		            }
 		            , {
 		                male: 15,
-		                female: 24, 
+		                female: 24,
 		                range: '35-44'
 		            }
 		            , {
 		                male: 19,
-		                female: 7, 
+		                female: 7,
 		                range: '45-54'
 		            }
 		            , {
 		                male: 8,
-		                female: 2, 
+		                female: 2,
 		                range: '55-64'
-		            }		           
+		            }
 		            , {
-		                range: '65+', 
+		                range: '65+',
 		                male: 1,
-		                female: 0, 
+		                female: 0,
 		            },
 		            ],
 		            keys: {
@@ -586,23 +593,23 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		            }
 		        }
 		    });
-			
-		}; 
-					
+
+		};
+
 		charts.FB_topFans = function(boxId) {
 		    var chart = c3.generate({
 		        bindto : '.' + boxId,
 		        data: {
 		            json: [
 		            {
-		                
-		                "name": "Fabiane Bergmann",		                
+
+		                "name": "Fabiane Bergmann",
 		                "likes": 21,
 		                "comments": 7
 		            }
-		            , {		               
+		            , {
 		                "name": "Ricardo Tomasi",
-		                "likes": 12,		                
+		                "likes": 12,
 		                "comments": 5
 		            }
 		            , {
@@ -615,7 +622,7 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		                "likes": 5,
 		                "comments": 5
 		            }
-		            , {		                 
+		            , {
 		                "name": "André Tomasi",
 		                "likes": 4,
 		                "comments": 2
@@ -632,10 +639,10 @@ define(['app', 'c3', 'jqueryGridster', 'jqueryUI', 'paymentService', 'twoNetServ
 		            }
 		        }
 		    });
-			
-		}; 
 
-		
+		};
+
+
     	$('.gridster').droppable({
     		drop: function (event, ui) {
     			var boxId = ui.draggable.attr('data-name') + '-' + Math.floor((Math.random() * 100) + 1);
