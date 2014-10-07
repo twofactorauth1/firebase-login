8ba856c470437481ebd964727bd56c9fcc1235a9
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2014-10-07 17:32:07 +0530

    Working on task #388

---
+++ public/angular_admin/services/import_contacts.js
@@ -0,0 +1,47 @@
+define(['app', 'constants'], function(app) {
+	app.register.service('ImportContactService', function($http) {
+		var baseUrl = '/api/1.0/';
+		this.importContacts = function(socialType, fn) {
+			var api = "social/";
+			switch(socialType) {
+				case $$.constants.social.types.FACEBOOK:
+					api += "facebook/";
+					break;
+				case $$.constants.social.types.GOOGLE:
+					api += "google/";
+					break;
+				case $$.constants.social.types.LINKEDIN:
+					api += "linkedin/";
+					break;
+			}
+			var apiUrl = baseUrl + api + ['checkaccess'].join('/');
+			$http.get(apiUrl)
+			.success(function(data, status, headers, config) {
+				var path = "";
+				api = "social/";
+				switch(socialType) {
+					case $$.constants.social.types.FACEBOOK:
+						api += "facebook/";
+						path = "friends/import/"
+						break;
+					case $$.constants.social.types.GOOGLE:
+						api += "google";
+						path = "contacts/import/";
+						break;
+					case $$.constants.social.types.LINKEDIN:
+						api += "linkedin/";
+						path = "connections/import/";
+						break;
+				}
+				apiUrl = baseUrl + api + path;
+				$http.get(apiUrl).success(function(data, status, headers, config) {
+					fn(data);
+				}).error(function(data, status, headers, config) {
+					fn(data);
+				});
+			}).error(function(data, status, headers, config) {
+					fn(data);
+				});
+		}
+	});
+});
