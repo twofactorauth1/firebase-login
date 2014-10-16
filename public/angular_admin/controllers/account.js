9f34f49d65bc681518545a30aeede3c0d0776e88
Jaideep Das <jaideep.jdof@gmail.com>
2014-10-16 11:42:15 +0530

    added state change directive

--- public/angular_admin/controllers/account.js
+++ public/angular_admin/controllers/account.js
@@ -1,4 +1,4 @@
-define(['app', 'userService', 'paymentService', 'skeuocardDirective', 'ngProgress','mediaDirective'], function(app) {
+define(['app', 'userService', 'paymentService', 'skeuocardDirective', 'ngProgress','mediaDirective', 'stateNavDirective'], function(app) {
     app.register.controller('AccountCtrl', ['$scope', 'UserService', 'PaymentService', 'ngProgress', function ($scope, UserService, PaymentService, ngProgress) {
         ngProgress.start();
 
