fc1512f6141a830df8e60e84c9e9012f3e96f392
Kyle Miller <millkyl@gmail.com>
2014-10-23 21:32:01 -0500

    Add security to authentication API

--- api/1.0/authentication.api.js
+++ api/1.0/authentication.api.js
@@ -34,18 +34,24 @@ _.extend(api.prototype, baseApi.prototype, {
         if (accountId == null) {
             return this.wrapError(resp, 400, "Invalid parameter", "Invalid parameter provided for accountId");
         }
-        //TODO: add security - VIEW_ACCOUNT
-        if (accountId > 0 && !this.sm.canReadAccount(req, accountId)) {
-            return this.wrapError(resp, 401, "Unauthorized access to this account", "Unauthorized access to account [" + accountId + "]");
-        }
-
-        authenticationDao.getAuthenticatedUrlForAccount(accountId, this.userId(req), req.query.path, null, function(err, value) {
-            if (err) {
-                return self.wrapError(resp, 500, "An error occurred", err, value);
+        
+        self.checkPermissionForAccount(req, self.sc.privs.VIEW_ACCOUNT, accountId, function(err, isAllowed){
+            if(isAllowed !== true) {
+                return self.send403(req);
+            }
+            if (accountId > 0 && !this.sm.canReadAccount(req, accountId)) {
+                return this.wrapError(resp, 401, "Unauthorized access to this account", "Unauthorized access to account [" + accountId + "]");
             }
 
-            resp.send({url:value});
+            authenticationDao.getAuthenticatedUrlForAccount(accountId, this.userId(req), req.query.path, null, function(err, value) {
+                if (err) {
+                    return self.wrapError(resp, 500, "An error occurred", err, value);
+                }
+
+                resp.send({url:value});
+            });
         });
+
     }
 });
 
