401a9c6fa8f184281f8be0a206aae0a77e3fc866
Kyle Miller <millkyl@gmail.com>
2014-11-03 20:39:24 -0600

    Fix JSON parse of mandrill webhooks.

--- api/1.0/analytics.api.js
+++ api/1.0/analytics.api.js
@@ -43,13 +43,25 @@ _.extend(api.prototype, baseApi.prototype, {
         self.log.debug('>> sendToKeen');
         //var request = require('request');
         var request = require('superagent');
+
+        var msg = {};
+        if(req.body.mandrill_events) {
+            try {
+                msg = JSON.parse(req.body.mandrill_events);
+            } catch(err) {
+                self.log.debug('error parsing events: ' + err);
+                msg = req.body;
+            }
+
+        } else {
+            msg = req.body;
+        }
         var url = 'https://api.keen.io/3.0/projects/54528c1380a7bd6a92e17d29/events/mandrill_events?api_key=c36124b0ccbbfd0a5e50e6d8c7e80a870472af9bf6e74bd11685d30323096486a19961ebf98d57ee642d4b83e33bd3929c77540fa479f46e68a0cdd0ab57747a96bff23c4d558b3424ea58019066869fd98d04b2df4c8de473d0eb66cc6164f03530f8ab7459be65d3bf2e8e8a21c34a';
         var newrequest = request.post(url)
             //.set('cookie', cookie)
-            .send(JSON.parse(req.body))
+            .send(msg)
             .end(function(result){
-                self.log.debug('result:');
-                console.dir(result);
+                self.log.debug('<< sendToKeen');
             });
         //TODO: Verify message from mandirll
         //TODO: parameterize url
