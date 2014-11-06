6fcbee11fbe91ce45e77b076870d5afea8a125c3
zeeshanhanif <zeeshanhanif@gmail.com>
2014-11-06 19:17:12 +0500

    #554 Single/Multiple Image Selection(done)

--- public/angular_admin/directives/mediadirective.js
+++ public/angular_admin/directives/mediadirective.js
@@ -57,114 +57,151 @@ define(['angularAMD', 'angularFileUpload', 'assetsService', 'moment', 'timeAgoFi
                 $scope.lastSelect = null;
                 $scope.isSingleSelect = true;
                 $scope.showType = "all";
-                $scope.select_all = null;
+                $scope.select_all = false;
                 $scope.batch = [];
                 $scope.m = {};
 
-                /*
-                 $scope.m.deleteAsset = function (assetId) {
-                 AssetsService.deleteAssetById(function (resp, status){
-                 if (status === 1 ) {
-                 $scope.assets.forEach(function(v, i){
-                 if ( v._id === assetId ) {
-                 $scope.assets.splice(i, 1);
-                 }
-                 })
-                 }
-                 }, assetId );
-                 };
-                 $scope.m.batchDeleteAsset = function () {
-                 $scope.assets.forEach(function (v, i){
-                 if(v.checked)
-                 $scope.m.deleteAsset(v._id);
-                 });
-                 };
-                 $scope.m.selectAll = function() {
-                 $scope.assets.forEach(function (v, i) {
-                 if ($scope.showType === 'all' || v.mimeType.match($scope.showType) ) {
-                 v.checked = $scope.select_all;
-                 }
-                 });
-                 };
-                 $scope.m.selectStatus = function() {
-                 var allTrue = true;
-                 $scope.assets.forEach(function (v, i) {
-                 if ( v.checked !== true ) {
-                 allTrue = false;
-                 }
-                 });
-                 $scope.select_all = allTrue === true;
-                 };
-                 $scope.m.showType = function (type) {
-                 $scope.showType = type;
-                 };
-                 $scope.m.resetUploader = function() {
-                 $scope.uploadComplete = false;
-                 };
-                 $scope.m.getSingleSelect=function() {
-                 $scope.batch=[];
+/*
+                $scope.m.deleteAsset = function (assetId) {
+                    AssetsService.deleteAssetById(function (resp, status) {
+                        if (status === 1) {
+                            $scope.assets.forEach(function (v, i) {
+                                if (v._id === assetId) {
+                                    $scope.assets.splice(i, 1);
+                                }
+                            })
+                        }
+                    }, assetId);
+                };
+                $scope.m.batchDeleteAsset = function () {
+                    $scope.assets.forEach(function (v, i) {
+                        if (v.checked)
+                            $scope.m.deleteAsset(v._id);
+                    });
+                };
+                $scope.m.selectAll = function () {
+                    $scope.assets.forEach(function (v, i) {
+                        if ($scope.showType === 'all' || v.mimeType.match($scope.showType)) {
+                            v.checked = $scope.select_all;
+                        }
+                    });
+                };
+                $scope.m.selectStatus = function () {
+                    var allTrue = true;
+                    $scope.assets.forEach(function (v, i) {
+                        if (v.checked !== true) {
+                            allTrue = false;
+                        }
+                    });
+                    $scope.select_all = allTrue === true;
+                };
+                $scope.m.showType = function (type) {
+                    $scope.showType = type;
+                };
+                $scope.m.resetUploader = function () {
+                    $scope.uploadComplete = false;
+                };
+                $scope.m.getSingleSelect = function () {
+                    $scope.batch = [];
 
-
-                 $scope.assets.forEach(function (v, i){
-                 if(v.checked)
-                 $scope.batch.push(v);
-                 });
-                 };
-                 $scope.m.onInsertMedia = function() {
-                 $scope.m.getSingleSelect();
-                 if ( $scope.batch.length > 0 ) {
-                 $scope.onInsertMediacb($scope.batch[$scope.batch.length-1]);
-                 }
-                 };
-                 $scope.m.singleSelect = function (event) {
-                 if ( $scope.lastSelect !== null  && $scope.lastSelect.id !== event.target.id && $scope.lastSelect.checked === true) {
-                 $scope.lastSelect.checked = false;
-                 }
-                 $scope.lastSelect = event.target;
-                 $scope.m.getSingleSelect();
-                 };
-                 */
-
-                $scope.m.selectAll = function() {
 
                     $scope.assets.forEach(function (v, i) {
+                        if (v.checked)
+                            $scope.batch.push(v);
+                    });
+                };
+                $scope.m.onInsertMedia = function () {
+                    $scope.m.getSingleSelect();
+                    if ($scope.batch.length > 0) {
+                        $scope.onInsertMediacb($scope.batch[$scope.batch.length - 1]);
+                    }
+                };
+                $scope.m.singleSelect = function (event) {
+                    if ($scope.lastSelect !== null && $scope.lastSelect.id !== event.target.id && $scope.lastSelect.checked === true) {
+                        $scope.lastSelect.checked = false;
+                    }
+                    $scope.lastSelect = event.target;
+                    $scope.m.getSingleSelect();
+                };
+*/
+                $scope.m.selectAll = function() {
+                    $scope.batch = [];
+                    $scope.assets.forEach(function (v) {
                         if ( $scope.select_all === false ) {
-                            //v.checked = false;
-                            $scope.assets[i].checked = false;
+                            v.checked = false;
                         }
                         else if ( $scope.showType === 'all' || v.mimeType.match($scope.showType) ) {
-                            //v.checked = true;
-                            $scope.assets[i].checked = true;
+                            v.checked = true;
+                            $scope.batch.push(v);
                         }
                     });
 
                     $scope.lastSelect = null;
+                    $scope.m.selectAllStatus();
                 };
-
+/*
                 $scope.$watch("select_all", function () {
                     $scope.m.selectAll();
                 });
-
+*/
                 $scope.m.singleSelect = function (asset) {
-                    $scope.select_all = false;
-                    $timeout(function (){
-                        if( !$scope.isSingleSelect){
-                            $scope.batch.push(asset)
+                    $timeout(function () {
+                        if( !$scope.isSingleSelect) {
+                            //$scope.batch.push(asset);
+                            var hasAsset = false;
+                            $scope.batch.forEach(function (v, i) {
+                                if ( asset._id === v._id ) {
+                                    $scope.batch.splice(i, 1);
+                                    hasAsset = true;
+                                }
+                            });
+                            if ( !hasAsset ) {
+                                $scope.batch.push(asset)
+                            }
 
+                            $scope.m.selectAllStatus();
+                        } else if ( $scope.isSingleSelect ) {
+                            $scope.batch.forEach(function (v) {
+                                if ( asset._id === v._id ) {
+                                } else {
+                                    v.checked = false;
+                                }
+                            });
+                            $scope.batch = [];
+                            $scope.batch.push(asset);
+
+                            $scope.m.selectAllStatus();
                         }
-                        else if ( $scope.lastSelect !== null && $scope.lastSelect._id !== asset._id && $scope.lastSelect.checked === true) {
-                            $scope.lastSelect.checked = false;
-                        }
-                        $scope.lastSelect = asset;
                     }, 0)
                 };
-
-                $scope.m.multiSelect = function () {
-
+                $scope.m.toggleShiftKey = function (event){
+                    $scope.isSingleSelect = !$scope.isSingleSelect;
                 };
-                $scope.m.checkKey = function (event){
-                    console.log(event);
-                    $scope.isSingleSelect = false;
+
+                $scope.m.selectAllStatus = function () {
+                    var allTrue = true;
+                    $scope.assets.forEach(function (v, i) {
+                        if (v.checked !== true) {
+                            allTrue = false;
+                        }
+                    });
+                    $scope.select_all = allTrue === true;
+                };
+                $scope.m.deleteAsset = function () {
+                    AssetsService.deleteAssetById($scope.batch, function (assetId, resp, status) {
+                        if ( status === 1 ) {
+                            $scope.assets.forEach(function (v, i) {
+                                if (v._id === assetId) {
+                                    $scope.assets.splice(i, 1);
+                                }
+                            });
+                            $scope.batch.forEach(function (v, i) {
+                                if (v._id === assetId) {
+                                    $scope.batch.splice(i, 1);
+                                }
+                            });
+                        }
+                    });
                 };
             },
 
@@ -177,57 +214,27 @@ define(['angularAMD', 'angularFileUpload', 'assetsService', 'moment', 'timeAgoFi
                 element.attr("data-target", "#media-manager-modal");
             }
         };
-    }]).directive('ngShift', function () {
-        return function (scope, element, attrs) {
-            element.bind("keydown", function (event) {
-                if(event.which === 65) {
-                    scope.$apply(function (){
-                        scope.$eval(attrs.ngShift);
-                    });
-
-                    event.preventDefault();
-                }
-            });
-        };
-    }).directive('captureShift', function(){
+    }]).directive('captureShift', function(){
             return {
                 restrict: 'A',
                 scope: {
                     onPresskey: "="
-
                 },
                 link: function(scope, elem, attrs) {
-                    /*
-                    elem[0].onkeydown = function(e){
-                        if(e.shiftKey) {
-                            scope.onPresskey();
-                            e.preventDefault();
-                        console.log("shift");
-                        }
-                    }
-                    */
                     function onShift(e){
                         if ( e.shiftKey ) {
                             scope.onPresskey();
-                            e.preventDefault();
-                            console.log(e)
-                            console.log("shift down");
-
                             elem[0].onkeydown = null;
                             elem[0].onkeyup = offShift;
                         }
                     }
                     function offShift(e) {
-                        console.log(e);
                         if ( e.keyIdentifier==='Shift' ) {
-                            console.log("shift up");
-
+                            scope.onPresskey();
                             elem[0].onkeyup = null;
                             elem[0].onkeydown = onShift;
-
                         }
                     }
-                  //  elem[0].onkeyup = offShift;
                     elem[0].onkeydown = onShift;
                 }
             }
