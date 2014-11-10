daa7f6d0653cd6a2118570078b7a020d80ea12a6
Jaideep Das <jaideep.jdof@gmail.com>
2014-11-10 21:19:14 +0530

    fix for #479

--- public/angular_admin/controllers/customer_edit.js
+++ public/angular_admin/controllers/customer_edit.js
@@ -176,18 +176,44 @@ define(['app',
       };
 
       $scope.savePreferencesFn = function() {
-        UserService.updateUserPreferences($scope.userPreferences, function(preferences) {
-          $scope.userPreferences = preferences;
-        });
+        UserService.updateUserPreferences($scope.userPreferences, function(preferences) {});
+        if ($scope.customerId) {
+          if ($scope.customer.type === undefined) {
+            $scope.customer.type = $scope.userPreferences.default_customer_type;
+          }
+          if ($scope.customer.details[0].addresses.length === 0) {
+            $scope.customer.details[0].addresses.push({});
+            $scope.customer.details[0].addresses[0].city = $scope.userPreferences.default_customer_city;
+            $scope.customer.details[0].addresses[0].state = $scope.userPreferences.default_customer_state;
+            $scope.customer.details[0].addresses[0].country = $scope.userPreferences.default_customer_country;
+            $scope.customer.details[0].addresses[0].zip = $scope.userPreferences.default_customer_zip;
+          }
+        } else {
+          $scope.customer.type = $scope.userPreferences.default_customer_type;
+          $scope.customer.details[0].addresses.push({});
+          $scope.customer.details[0].addresses[0].city = $scope.userPreferences.default_customer_city;
+          $scope.customer.details[0].addresses[0].state = $scope.userPreferences.default_customer_state;
+          $scope.customer.details[0].addresses[0].country = $scope.userPreferences.default_customer_country;
+          $scope.customer.details[0].addresses[0].zip = $scope.userPreferences.default_customer_zip;
+        }
       };
-
-      UserService.getUserPreferences(function(preferences) {
-        $scope.userPreferences = preferences;
-      });
 
       if ($scope.customerId) {
         CustomerService.getCustomer($scope.customerId, function(customer) {
           $scope.customer = customer;
+          UserService.getUserPreferences(function(preferences) {
+            $scope.userPreferences = preferences;
+            if ($scope.customer.type === undefined) {
+              $scope.customer.type = $scope.userPreferences.default_customer_type;
+            }
+            if ($scope.customer.details[0].addresses.length === 0) {
+              $scope.customer.details[0].addresses.push({});
+              $scope.customer.details[0].addresses[0].city = $scope.userPreferences.default_customer_city;
+              $scope.customer.details[0].addresses[0].state = $scope.userPreferences.default_customer_state;
+              $scope.customer.details[0].addresses[0].country = $scope.userPreferences.default_customer_country;
+              $scope.customer.details[0].addresses[0].zip = $scope.userPreferences.default_customer_zip;
+            }
+          });
           ngProgress.complete();
           $scope.fullName = [$scope.customer.first, $scope.customer.middle, $scope.customer.last].join(' ');
           if ($scope.customer.details[0].addresses.length) {
@@ -202,6 +228,15 @@ define(['app',
       } else {
         ngProgress.complete();
         $scope.customerAddressWatchFn(0);
+        UserService.getUserPreferences(function(preferences) {
+          $scope.userPreferences = preferences;
+          $scope.customer.type = $scope.userPreferences.default_customer_type;
+          $scope.customer.details[0].addresses.push({});
+          $scope.customer.details[0].addresses[0].city = $scope.userPreferences.default_customer_city;
+          $scope.customer.details[0].addresses[0].state = $scope.userPreferences.default_customer_state;
+          $scope.customer.details[0].addresses[0].country = $scope.userPreferences.default_customer_country;
+          $scope.customer.details[0].addresses[0].zip = $scope.userPreferences.default_customer_zip;
+        });
       }
 
       $scope.$watch('fullName', function(newValue, oldValue) {
@@ -227,9 +262,9 @@ define(['app',
         }
       });
 
-    $scope.insertPhoto=function(asset){
-        $scope.customer.photo=asset.url;
-    };
+      $scope.insertPhoto = function(asset) {
+        $scope.customer.photo = asset.url;
+      };
 
     }
   ]);
