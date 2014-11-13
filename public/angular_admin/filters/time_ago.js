0c8de303f22e37e2e3a0e0d98c408cc5da750da0
zeeshanhanif <zeeshanhanif@gmail.com>
2014-11-13 16:53:31 +0500

    fixed moment.js error

--- public/angular_admin/filters/time_ago.js
+++ public/angular_admin/filters/time_ago.js
@@ -1,4 +1,4 @@
-define(['app'], function(app) {
+define(['app', 'moment'], function(app) {
   app.register.filter('timeAgoFilter', function() {
     return function(date) {
       return moment(date).fromNow();
