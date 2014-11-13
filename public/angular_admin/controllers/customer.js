f03a6934f1afe6a4a6064767a00f1b9a0a10cdc3
Jaideep Das <jaideep.jdof@gmail.com>
2014-11-13 20:06:33 +0530

    fixed formatting

--- public/angular_admin/controllers/customer.js
+++ public/angular_admin/controllers/customer.js
@@ -61,11 +61,11 @@ define(['app', 'customerService', 'stateNavDirective', 'truncateDirective', 'ngP
 
     $scope.orderByFn = function() {
       var customers = $scope.fetchedCustomers || [];
-      var present = customers.filter(function(elem) { if(elem[$scope.customerOrder]) {return elem[$scope.customerOrder] != ''; }});
+      var present = customers.filter(function(elem) { if(elem[$scope.customerOrder]) {return elem[$scope.customerOrder] !== ''; }});
       var sortedAlphabetically = present.sort(function(a, b) {
-            return $scope.sortCustomers(a.first,b.first) || $scope.sortCustomers(a.last,b.last)
+            return $scope.sortCustomers(a.first,b.first) || $scope.sortCustomers(a.last,b.last);
       });
-      var empty = customers.filter(function(elem) { return elem.first == ""; });
+      var empty = customers.filter(function(elem) { return elem.first === ""; });
       var concatCustomers = sortedAlphabetically.concat(empty);
       // if ($scope.customerSortReverse) {
       //   concatCustomers = concatCustomers.reverse();
@@ -185,7 +185,7 @@ define(['app', 'customerService', 'stateNavDirective', 'truncateDirective', 'ngP
       $scope.$watch('sortOrder', function(newValue, oldValue) {
         console.log('sort order ', newValue);
         newValue = parseInt(newValue);
-        if (newValue == '') {
+        if (newValue === '') {
           $scope.customerOrder = 'first';
           $scope.customerSortReverse = false;
         } else if (newValue == 1) {
@@ -288,13 +288,14 @@ define(['app', 'customerService', 'stateNavDirective', 'truncateDirective', 'ngP
         // Save user preferences
         $scope.userPreferences.customerSettings.gridViewDisplay = value;
         $scope.savePreferencesFn();
-      }
+      };
+
       $scope.setImportantContact = function(customer) {
         customer.starred = true;
         CustomerService.saveCustomer(customer, function(customers) {
           ToasterService.show('success', "Contact updated succesfully.");
-        })
-      }
+        });
+      };
 
       UserService.getUserPreferences(function(preferences) {
         $scope.userPreferences = preferences;
@@ -309,12 +310,12 @@ define(['app', 'customerService', 'stateNavDirective', 'truncateDirective', 'ngP
             customerSortReverse: false,
             customerDisplayFormat: 'first',
             gridViewDisplay: "true"
-          }
+          };
         }
 
       });
       $scope.savePreferencesFn = function() {
-        UserService.updateUserPreferences($scope.userPreferences, function() {})
+        UserService.updateUserPreferences($scope.userPreferences, function() {});
       };
 
     });
