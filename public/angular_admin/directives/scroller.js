d9286444ae46bdd9934088e7eeb8fcde0280e506
Jaideep Das <jaideep.jdof@gmail.com>
2014-11-14 22:14:25 +0530

    added fix for the isolated scope issue in scroller

--- public/angular_admin/directives/scroller.js
+++ public/angular_admin/directives/scroller.js
@@ -2,15 +2,12 @@ define(['angularAMD'], function (angularAMD) {
     angularAMD.directive('indiscroller', function() {
         return {
             restrict: 'A',
-            scope: {
-              updateFn: '=indiscrollerUpdate'
-            },
             link: function (scope, elem, attrs) {
                 rawElement = elem[0];
                 elem.bind('scroll', function () {
                     if((rawElement.scrollTop + rawElement.offsetHeight + 1000 ) >= rawElement.scrollHeight){
-                        scope.updateFn(rawElement.scrollTop);
                         scope.$apply('customerScrollFn()');
+                        scope.$apply('saveScrollFn(' + rawElement.scrollTop + ')');
                     }
                 });
             }
