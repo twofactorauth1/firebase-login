7c012a90c3c1a690908a482a8cc37d685b26efc0
Brad Risse <bradly.risse@gmail.com>
2014-11-21 12:45:51 -0800

    Merge remote-tracking branch 'origin/zeeshan_dev_angular2' into Frontend
    Conflicts:
    bower.json

--- public/angular_admin/controllers/website.js
+++ public/angular_admin/controllers/website.js
@@ -269,6 +269,7 @@ define([
                 // $scope.components = that.originalCurrentPageComponents;
                 var pageId = $scope.currentPage._id;
                 //$scope.deactivateAloha && $scope.deactivateAloha();
+                $scope.deactivateAloha();
                 WebsiteService.getPageComponents(pageId,function(components) {
                     $scope.components = components;
 
@@ -384,14 +385,14 @@ define([
                     iFrame && iFrame.contentWindow && iFrame.contentWindow.triggerEditModeOff && iFrame.contentWindow.triggerEditModeOff();
                     //iFrame.contentWindow.triggerFontUpdate($scope.website.settings.font_family);
                     //document.getElementById('iframe-website').contentWindow.location.reload(true);
-                    $scope.deactivateAloha && $scope.deactivateAloha();
+
                     iFrame && iFrame.contentWindow && iFrame.contentWindow.savePostMode && iFrame.contentWindow.savePostMode();
 
                //     document.getElementById("iframe-website").setAttribute("src", route + '?editor=true');
 
 
                 });
-
+                //$scope.deactivateAloha();
                 var data = {
                     _id: $scope.website._id,
                     accountId: $scope.website.accountId,
