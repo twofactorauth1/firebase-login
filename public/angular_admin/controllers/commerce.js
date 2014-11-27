0b8ef06c4024489f3bf429578b21da8478ff2e04
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2014-11-27 12:13:57 +0530

    Default Order Products #709

--- public/angular_admin/controllers/commerce.js
+++ public/angular_admin/controllers/commerce.js
@@ -44,9 +44,6 @@ define(['app', 'productService', 'paymentService', 'headroom', 'ngHeadroom', 'ng
             showProductCost: true
         };
 
-        $scope.productOrder = 'name';
-        $scope.productSortReverse = false;
-
         $scope.$watch('sortOrder', function(newValue, oldValue) {
             newValue = parseInt(newValue);
             if (newValue === 0) {
@@ -67,12 +64,64 @@ define(['app', 'productService', 'paymentService', 'headroom', 'ngHeadroom', 'ng
             } else if (newValue == 5) {
                 $scope.productOrder = 'lastActivity';
                 $scope.productSortReverse = true;
+            } else if (newValue == 4) {
+                $scope.productOrder = 'total_sales';
+                $scope.productSortReverse = false;              
+            } else if (newValue == 5) {
+                $scope.productOrder = 'regular_price';
+                $scope.productSortReverse = false;
+            } else if (newValue == 6) {
+                $scope.productOrder = 'regular_price';
+                $scope.productSortReverse = true;
+            } else if (newValue == 7) {
+                $scope.productOrder = 'starred';
+                $scope.productSortReverse = false;
             }
-            else
-                {
-                    $scope.sortOrder = 1;
-                }
         });
+        var initializeSortOrder = 0;
+        $scope.$watch('sortOrderSettings', function(newValue, oldValue) {
+            if (initializeSortOrder >= 2) {
+               newValue = parseInt(newValue);
+               $scope.sortOrder = newValue;
+                if (newValue === 0) {
+                    $scope.productOrder = 'name';
+                    $scope.productSortReverse = false;
+                } else if (newValue == 1) {
+                    $scope.productOrder = 'name';
+                    $scope.productSortReverse = false;
+                } else if (newValue == 2) {
+                    $scope.productOrder = 'name';
+                    $scope.productSortReverse = true;
+                } else if (newValue == 3) {
+                    $scope.productOrder = 'created.date';
+                    $scope.productSortReverse = false;
+                } else if (newValue == 4) {
+                    $scope.productOrder = 'total_sales';
+                    $scope.productSortReverse = false;              
+                } else if (newValue == 5) {
+                    $scope.productOrder = 'regular_price';
+                    $scope.productSortReverse = false;
+                } else if (newValue == 6) {
+                    $scope.productOrder = 'regular_price';
+                    $scope.productSortReverse = true;
+                } else if (newValue == 7) {
+                    $scope.productOrder = 'starred';
+                    $scope.productSortReverse = false;
+                }    
+                if (newValue && $scope.userPreferences && $scope.userPreferences.commerceSettings) {
+                    $scope.userPreferences.commerceSettings.productOrder = $scope.productOrder;
+                    $scope.userPreferences.commerceSettings.productSortReverse = $scope.productSortReverse;
+                    $scope.savePreferencesFn();
+                } 
+            }
+            initializeSortOrder += 1;
+            
+
+        });
+        
+        $scope.savePreferencesFn = function() {
+            UserService.updateUserPreferences($scope.userPreferences, true, function() {});
+        };
 
         ProductService.getProducts(function(products) {
             $scope.products = products;
@@ -80,7 +129,48 @@ define(['app', 'productService', 'paymentService', 'headroom', 'ngHeadroom', 'ng
         });
         UserService.getUserPreferences(function(preferences) {
             $scope.userPreferences = preferences;
-    });
+            var commerceSettings = $scope.userPreferences.commerceSettings;
+            if (commerceSettings) {
+                $scope.userPreferences.commerceSettings = commerceSettings;
+                $scope.productOrder = $scope.userPreferences.commerceSettings.productOrder;
+                $scope.productSortReverse = $scope.userPreferences.commerceSettings.productSortReverse;
+                
+                
+                var productOrder = $scope.productOrder;
+                var productSortReverse = $scope.productSortReverse;
+                var orderNum;
+                if (productOrder === 'name') {
+                    if(productSortReverse)
+                        orderNum = 2;   
+                    else
+                        orderNum = 1;
+                }
+                else if (productOrder === 'created.date') {
+                   orderNum = 3; 
+                }
+                else if (productOrder === 'total_sales') {
+                   orderNum = 4; 
+                }
+                else if (productOrder === 'regular_price') {
+                   if(productSortReverse)
+                        orderNum = 6;   
+                    else
+                        orderNum = 5;
+                }
+                else if (productOrder === 'starred') {
+                   orderNum = 7; 
+                }               
+                $scope.sortOrder = orderNum;
+                $scope.sortOrderSettings = orderNum;
+            } else {
+                    $scope.userPreferences.commerceSettings = {
+                    productOrder: 'name',
+                    productSortReverse: false                    
+                };
+                $scope.sortOrder = 1;
+                $scope.sortOrderSettings = 1;
+            }
+        });
 
   }]);
 });
