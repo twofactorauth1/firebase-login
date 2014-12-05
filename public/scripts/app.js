1751a2a4d957e792202c6f85b0d703d59c87ddbd
Brad Risse <bradly.risse@gmail.com>
2014-12-04 19:41:53 -0800

    Add ui.bootstrap back

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,6 +26,7 @@ var mainApp = angular
         'iso.directives',
         'timer',
         'ui',
+        'ui.bootstrap',
         "com.2fdevs.videogular",
         "com.2fdevs.videogular.plugins.controls",
         "com.2fdevs.videogular.plugins.overlayplay",
