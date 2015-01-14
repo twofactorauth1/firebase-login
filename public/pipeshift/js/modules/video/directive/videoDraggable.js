246d667f77ccfa70961ad617a5ace1f3b3e0ebb1
Jaideep Das <jaideep.jdof@gmail.com>
2015-01-14 13:58:44 +0530

    added drag functionality

--- public/pipeshift/js/modules/video/directive/videoDraggable.js
+++ public/pipeshift/js/modules/video/directive/videoDraggable.js
@@ -1,13 +1,9 @@
 angular.module('var.directives').directive('videoDraggable', function () {
     return {scope: {
-        index: '=',
-        dragFn: '='
+        index: '='
     }, link: function (scope, elm, attr) {
         elm.attr("draggable", true);
         elm.bind("dragstart", function (event) {
-            if (scope.dragFn) {
-              scope.dragFn();
-            }
             event.originalEvent.dataTransfer.setData('videoIndex', scope.index+"");
         });
     }};
