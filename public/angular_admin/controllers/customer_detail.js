cddb0e1f53750a5ba239b25f7abd46dd83242669
Umesh Duggal <umeshduggal1@gmail.com>
2015-01-16 16:00:18 +0530

    Single Map Loading #1423

--- public/angular_admin/controllers/customer_detail.js
+++ public/angular_admin/controllers/customer_detail.js
@@ -1,9 +1,7 @@
-define(['app', 'customerService', 'stateNavDirective', 'ngProgress', 'toasterService', 'leaflet-directive', 'keenService', 'timeAgoFilter','activityDirective', 'formatText'], function(app) {
-    app.register.controller('CustomerDetailCtrl', ['$scope', 'CustomerService', '$stateParams', '$state', 'ngProgress', 'ToasterService', 'keenService',
-        function($scope, CustomerService, $stateParams, $state, ngProgress, ToasterService, keenService) {
+define(['app', 'customerService', 'stateNavDirective', 'ngProgress', 'toasterService', 'leaflet-directive', 'keenService', 'timeAgoFilter','activityDirective', 'formatText','blockUI'], function(app) {
+    app.register.controller('CustomerDetailCtrl', ['$scope', 'CustomerService', '$stateParams', '$state', 'ngProgress', 'ToasterService', 'keenService', 'blockUI',
+        function($scope, CustomerService, $stateParams, $state, ngProgress, ToasterService, keenService, blockUI) {
             ngProgress.start();
-            $scope.lat = 51;
-            $scope.lng = 0;
             $scope.$back = function() {
                 console.log('$scope.lastState.state ', $scope.lastState.state);
                 console.log('$scope.lastState.params ', $scope.lastState.params);
@@ -14,7 +12,8 @@ define(['app', 'customerService', 'stateNavDirective', 'ngProgress', 'toasterSer
                 }
             };
             $scope.customerId = $stateParams.id;
-
+            var mapBlockUI = blockUI.instances.get('mapBlockUI');
+            mapBlockUI.start();
             $scope.ip_geo_address = '';
             CustomerService.getCustomer($scope.customerId, function(customer) {
                 $scope.customer = customer;
@@ -75,6 +74,7 @@ define(['app', 'customerService', 'stateNavDirective', 'ngProgress', 'toasterSer
                             $scope.markers.mainMarker.lat = parseFloat(data.lat);
                             $scope.markers.mainMarker.lng = parseFloat(data.lon);
                         }
+                        myBlockUI.stop();
                     });
                 }
 
