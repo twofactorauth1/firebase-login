ace545df5a99c8b55433d0238b8d90a3d692a70c
Brad Risse <bradly.risse@gmail.com>
2015-01-21 13:35:03 -0800

    Floating Ckeditor, Left Nav

--- public/angular_admin/app.js
+++ public/angular_admin/app.js
@@ -172,8 +172,41 @@ define(['angularAMD', 'angularUiRouter', 'angularRoute', 'varMainModule', 'resiz
             p = p || $('.nav.nav-pills.nav-stacked.nav-bracket');
             toName = toState.name.split(/[A-Z]/g);
             fromName = fromState.name.split(/[A-Z]/g);
-            $('[href="#/' + toName[0] + '"]', p).parent().addClass('active')
-            $('[href="#/' + fromName[0] + '"]', p).parent().removeClass('active')
+            $('[href="#/' + toName[0] + '"]', p).parent().addClass('active');
+            $('[href="#/' + fromName[0] + '"]', p).parent().removeClass('active');
+             var self = this;
+              var $el, topPos,
+                  $mainNav = $("#leftnav ul");
+
+              $mainNav.append("<li id='magic-line'></li>");
+              var $magicLine = $("#magic-line");
+
+              $magicLine
+                  .height('63px')
+                  .css("top", $("#leftnav ul li.active").position().top)
+                  .data("origTop", $magicLine.position().top);
+
+
+                  $("#leftnav ul li").hover(function() {
+                      $el = $(this);
+                      topPos = $el.position().top;
+                      $magicLine.stop().animate({
+                          top: topPos
+                      });
+                  }, function() {
+                      console.log('$magicLine.data("origTop") ', $magicLine.data("origTop"));
+                      $magicLine.stop().animate({
+                          top: $magicLine.data("origTop")
+                      });
+                  });
+
+               $("#leftnav ul li").on('click', function() {
+                  var self = this;
+                  $el = $(this);
+                  console.log('clicked ', $el.position().top);
+                  self.topPos = $el.position().top;
+                  $magicLine.data("origTop", $el.position().top);
+               });
           }
         });
     }]);
