df32ad7695468143fec86df5cbbdec7691314065
Brad Risse <bradly.risse@gmail.com>
2015-01-22 01:39:23 -0800

    Cleanup

--- public/js/libs_misc/ckeditor/config.js
+++ public/js/libs_misc/ckeditor/config.js
@@ -27,7 +27,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The default plugins included in the basic setup define some buttons that
 	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Underline,Strike,Subscript,Superscript,Image,maximize,resize,Format,Indent,Outdent,Blockquote,lineheight,horizontalrule';
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Underline,Strike,Subscript,Superscript,Image,maximize,resize,Format,Indent,HorizontalRule,Outdent,Blockquote,lineheight';
 
 	// Dialog windows are also simplified.
 	config.removeDialogTabs = '';
