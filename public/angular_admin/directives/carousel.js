a49ebf04a096faae319f0c4b857032f8ceab2523
Brad Risse <bradly.risse@gmail.com>
2015-01-23 00:52:16 -0800

    Remove Unused Carousel

--- public/angular_admin/directives/carousel.js
+++
@@ -1,7 +0,0 @@
-angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
-    .controller('CarouselController', ['$scope', '$timeout', '$transition', '$q', function        ($scope, $timeout, $transition, $q) {
-}]).directive('carousel', [function() {
-    return {
-    	
-    }
-}]);
