ea0d6aa3a4e026b44289d458a3a9d228a4e4fe57
Kyle Miller <millkyl@gmail.com>
2015-01-30 15:28:31 -0600

    #1699 finish social config service.

--- api/1.0/social/socialconfig.api.js
+++ api/1.0/social/socialconfig.api.js
@@ -55,11 +55,53 @@ _.extend(api.prototype, baseApi.prototype, {
     },
 
     createSocialConfig: function(req, resp) {
+        var self = this;
+        self.log.debug('>> createSocialConfig');
+        self.checkPermission(req, self.sc.privs.MODIFY_SOCIALCONFIG, function(err, isAllowed) {
+            if (isAllowed !== true) {
+                return self.send403(res);
+            } else {
+                var socialConfig = new $$.m.SocialConfig(req.body);
+                var accountId = parseInt(self.accountId(req));
+                socialConfig.set('accountId', accountId);
+                socialConfigManager.createSocialConfig(socialConfig, function(err, config){
+                    self.log.debug('<< createSocialConfig');
+                    self.sendResultOrError(resp, err, config, "Error creating social config");
+                });
+            }
+        });
+
+
+
 
     },
 
     updateSocialConfig: function(req, resp) {
-
+        var self = this;
+        self.log.debug('>> updateSocialConfig');
+        self.checkPermission(req, self.sc.privs.MODIFY_SOCIALCONFIG, function(err, isAllowed) {
+            if (isAllowed !== true) {
+                return self.send403(res);
+            } else {
+                var socialConfig = new $$.m.SocialConfig(req.body);
+                var accountId = parseInt(self.accountId(req));
+                if(req.params.id) {
+                    socialConfig.set('_id', req.params.id);
+                }
+                /*
+                 * If the accountId and socialConfig.accountId do not match
+                 * return an error.  This could be an innocent mistake... or something
+                 * malicious.
+                 */
+                if(socialConfig.get('accountId') !== accountId) {
+                    return self.wrapError(resp, 400, 'Bad Request', 'The social config does not match one belonging to this account.');
+                }
+                socialConfigManager.updateSocialConfig(socialConfig, function(err, config){
+                    self.log.debug('<< updateSocialConfig');
+                    self.sendResultOrError(resp, err, config, "Error updating social config");
+                });
+            }
+        });
     }
 
 
--- socialconfig/socialconfig_manager.js
+++ socialconfig/socialconfig_manager.js
@@ -13,6 +13,11 @@ module.exports = {
     createSocialConfig: function(socialConfig, fn) {
         var self = this;
         log.debug('>> createSocialConfig');
+        //make sure we have the object and not just json
+        if(typeof socialConfig.id !== 'function') {
+            socialConfig = new $$.m.SocialConfig(socialConfig);
+        }
+
         socialconfigDao.saveOrUpdate(socialConfig, function(err, value){
             if(err) {
                 log.error('Error creating socialconfig: ' + err);
@@ -40,6 +45,48 @@ module.exports = {
                 return fn(null, value);
             }
         });
+    },
+
+    updateSocialConfig: function(socialConfig, fn) {
+        var self = this;
+        log.debug('>> updateSocialConfig');
+        //make sure we have the object and not just json
+        if(typeof socialConfig.id !== 'function') {
+            socialConfig = new $$.m.SocialConfig(socialConfig);
+        }
+
+        if(socialConfig.id() === null) {
+            var query = {accountId: socialConfig.get('accountId')};
+            socialconfigDao.findOne(query, $$.m.SocialConfig, function(err, value){
+                if(err) {
+                    log.error('Error finding socialconfig: ' + err);
+                    return fn(err, null);
+                } else {
+                    socialConfig.set('_id', value.id());
+                    socialconfigDao.saveOrUpdate(socialConfig, function(err, value){
+                        if(err) {
+                            log.error('Error updating socialconfig: ' + err);
+                            fn(err, null);
+                        } else {
+                            log.debug('<< updateSocialConfig');
+                            fn(null, value);
+                        }
+                    });
+                }
+            });
+        } else {
+            socialconfigDao.saveOrUpdate(socialConfig, function(err, value){
+                if(err) {
+                    log.error('Error updating socialconfig: ' + err);
+                    fn(err, null);
+                } else {
+                    log.debug('<< updateSocialConfig');
+                    fn(null, value);
+                }
+            });
+        }
+
+
     }
 
 };
