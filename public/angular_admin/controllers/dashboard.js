c2b87e30c7146cb406768642f6833488957ca791
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-03-04 19:27:58 +0530

    Refactor Dashboard Data Loading #2244

--- public/angular_admin/controllers/dashboard.js
+++ public/angular_admin/controllers/dashboard.js
@@ -156,41 +156,300 @@ define([
       $scope.runAnalyticsReports = function(account) {
         //console.log('account is: ', account);
         //console.log('date is: ', $scope.date);
-        ChartAnalyticsService.runReports($scope.date, account, function(data) {
-
-          $scope.desktop = data.desktop;
-          $scope.mobile = data.mobile;
-          $scope.visitorsData = data.visitorsData;
-          $scope.visitors = data.currentTotalVisitors;
-          $scope.pageviews = data.currentTotalPageviews;
-          $scope.pageviewsData = data.pageviewsData;
-          $scope.pageviewsPercent = data.pageviewsPercent;
-          $scope.visitorsPercent = data.visitorsPercent;
-          $scope.pageviewsPreviousData = data.pageviewsPreviousData;
-          $scope.sessions = data.totalSessions;
-          $scope.sessionsData = data.sessionData;
-          $scope.sessionsPercent = data.sessionsPercent;
-          $scope.bounces = data.totalBounces;
-          $scope.bouncesData = data.bouncesData;
-          $scope.bouncesPercent = data.bouncesPercent;
-          $scope.totalTypes = data.totalTypes;
-          $scope.trafficSourceData = data.trafficSourceData;
-          $scope.newVsReturning = data.newVsReturning;
-          $scope.locationData = data.locationData;
+        ChartAnalyticsService.runPagedReports($scope.date, account, function(data) {
           $scope.formattedTopPages = data.formattedTopPages;
           $scope.pagedformattedTopPages = data.pagedformattedTopPages;
-          $scope.visitDuration = data.visitDuration;
-          $scope.visitDurationPercent = data.visitDurationPercent;
-          $scope.avgSessionData = data.avgSessionData;
-          $scope.displayVisitors = $scope.visitors > 0;
-          $scope.renderAnalyticsCharts();
         });
+
+        ChartAnalyticsService.runReports($scope.date, account, function(data) {
+                 $scope.setReportData(data);
+        });        
       };
       UserService.getAccount(function(account) {
         $scope.analyticsAccount = account;
         $scope.runAnalyticsReports(account);
       });
 
+
+      $scope.setReportData = function(results)
+      {
+        var desktop, mobile;
+            for (var i = 0; i < results[1].result.length; i++) {
+                var category = results[1].result[i]['user_agent.device'];
+                if (category === 'desktop') {
+                    desktop = results[1].result[i].result;
+                }
+                if (category === 'mobile') {
+                    mobile = results[1].result[i].result;
+                } else {
+                    mobile = 0;
+                }
+            };
+
+            $scope.desktop = desktop;
+            $scope.mobile = mobile;
+            // ----------------------------------------
+            // Visitors
+            // ----------------------------------------
+
+            var visitorsData = [];
+            var currentTotalVisitors = 0;
+            for (var k = 0; k < results[2].result.length; k++) {
+                var subArr = [];
+                var value = results[2].result[k].value || 0;
+                currentTotalVisitors += value;
+                subArr.push(new Date(results[2].result[k].timeframe.start).getTime());
+                subArr.push(value);
+                visitorsData.push(subArr);
+            };
+
+            $scope.visitorsData = visitorsData;
+
+            // var readyVisitorsData = [];
+            // if (currentTotalVisitors > totalVisitors) {
+            //     totalVisitors = currentTotalVisitors;
+            //     readyVisitorsData = visitorsData;
+            //     if (firstQuery) {
+            //         readyVisitorsData = visitorsData;
+            //     } else {
+            //         $scope.analyticsOverviewConfig.series[2].data = visitorsData;
+            //     }
+            // }
+
+            var vistorsPreviousData = 0;
+            for (var h = 0; h < results[3].result.length; h++) {
+                var value = results[3].result[h].value || 0;
+                vistorsPreviousData += value;
+            };
+
+            var visitorsPercent = ChartAnalyticsService.calculatePercentage(vistorsPreviousData, currentTotalVisitors);
+            $scope.visitors = currentTotalVisitors;
+            $scope.visitorsPercent = visitorsPercent;
+            // ----------------------------------------
+            // Pageviews Metric
+            // ----------------------------------------
+
+            var pageviewsData = [];
+            var currentTotalPageviews = 0;
+            for (var j = 0; j < results[4].result.length; j++) {
+                var subArr = [];
+                var value = results[4].result[j].value || 0;
+                currentTotalPageviews += value;
+                subArr.push(new Date(results[4].result[j].timeframe.start).getTime());
+                subArr.push(value);
+                pageviewsData.push(subArr);
+            };
+            $scope.pageviews = currentTotalPageviews;
+            $scope.pageviewsData = pageviewsData;
+
+            // if ($scope.currentTotalPageviews > $scope.totalPageviews) {
+            //     $scope.totalPageviews = $scope.currentTotalPageviews;
+            //     $scope.pageviews = $scope.totalPageviews;
+            //     if ($scope.firstQuery) {
+            //         $scope.readyPageviewsData = $scope.pageviewsData;
+            //     } else {
+            //         $scope.analyticsOverviewConfig.series[0].data = $scope.pageviewsData;
+            //     }
+            // }
+
+            var pageviewsPreviousData = 0;
+            for (var r = 0; r < results[5].result.length; r++) {
+                var value = results[5].result[r].value || 0;
+                pageviewsPreviousData += value;
+            };
+
+            $scope.pageviewsPreviousData = pageviewsPreviousData;
+
+            var pageviewsPercent = ChartAnalyticsService.calculatePercentage(currentTotalPageviews, pageviewsPreviousData);
+            $scope.pageviewsPercent = pageviewsPercent;
+            // ----------------------------------------
+            // Sessions
+            // ----------------------------------------
+
+            _sessionsData = [];
+            _totalSessions = 0;
+            for (var j = 0; j < results[6].result.length; j++) {
+                var subArr = [];
+                var value = results[6].result[j].value || 0;
+                _totalSessions += value;
+                subArr.push(new Date(results[6].result[j].timeframe.start).getTime());
+                subArr.push(value);
+                _sessionsData.push(subArr);
+            };
+            $scope.sessions = _totalSessions;
+            $scope.sessionsData = _sessionsData;
+            // if (_totalSessions > $scope.sessions) {
+            //     $scope.sessions = _totalSessions;
+            //     if ($scope.firstQuery) {
+            //         $scope.sessionsData = _sessionsData;
+            //     } else {
+            //         $scope.analyticsOverviewConfig.series[1].data = _sessionsData;
+            //     }
+            // }
+
+            var sessionsPreviousData = 0;
+            for (var w = 0; w < results[7].result.length; w++) {
+                var value = results[7].result[w].value || 0;
+                sessionsPreviousData += value;
+            };
+
+            var sessionsPercent = ChartAnalyticsService.calculatePercentage(_totalSessions, sessionsPreviousData);
+
+            $scope.sessionsPercent = sessionsPercent;
+
+            var secsToConv = 0;
+            if (results[9].result && _totalSessions) {
+              secsToConv = (results[9].result / 1000) / _totalSessions;
+            }
+            var visitDuration = ChartAnalyticsService.secToTime(secsToConv);
+
+            if (results[15].result == null) {
+                results[15].result = 0;
+            }
+
+            $scope.visitDuration = visitDuration;
+
+            var previousVisitDuration = results[15].result;
+
+            var visitDurationPercent = ChartAnalyticsService.calculatePercentage(results[9].result, previousVisitDuration);
+            $scope.visitDurationPercent = visitDurationPercent;
+            // ----------------------------------------
+            // Average Visit Duration
+            // ----------------------------------------
+
+            var avgSessionData = [];
+            for (var b = 0; b < results[8].result.length; b++) {
+                var subArr = [];
+                var value = results[8].result[b].value || 0;
+                subArr.push(new Date(results[8].result[b].timeframe.start).getTime());
+                subArr.push(value);
+                avgSessionData.push(subArr);
+            };
+
+             $scope.avgSessionData = avgSessionData;
+            // ======================================
+            // Bounces
+            // ======================================
+
+            var _bouncesData = [];
+            var _totalBounces = 0;
+            for (var r = 0; r < results[10].result.length; r++) {
+                var subArr = [];
+                var value = results[10].result[r].value || 0;
+                _totalBounces += value;
+                subArr.push(new Date(results[10].result[r].timeframe.start).getTime());
+                subArr.push(value);
+                _bouncesData.push(subArr);
+            };
+
+            $scope.bounces = _totalBounces;
+            $scope.bouncesData = _bouncesData;
+
+            // if (_totalBounces >= $scope.bounces) {
+            //     $scope.bounces = _totalBounces;
+            //     if ($scope.firstQuery) {
+            //         $scope.bouncesData = _bouncesData;
+            //     } else {
+            //         $scope.timeonSiteConfig.series[1].data = _bouncesData;
+            //     }
+            // }
+
+            var bouncesPercent = ChartAnalyticsService.calculatePercentage(_totalBounces, results[11].result);
+            $scope.bouncesPercent = bouncesPercent;
+            // // ======================================
+            // // Traffic Sources
+            // // ======================================
+
+            var _trafficSourceData = [];
+            var _totalTypes = 0;
+            for (var i = 0; i < results[12].result.length; i++) {
+                var subObj = [];
+                if (results[12].result[i].source_type) {
+                    subObj.push(results[12].result[i].source_type.charAt(0).toUpperCase() + results[12].result[i].source_type.slice(1));
+                } else {
+                    subObj.push('Other');
+                }
+                subObj.push(results[12].result[i].result);
+                _totalTypes += results[12].result[i].result;
+                _trafficSourceData.push(subObj);
+            };
+             $scope.totalTypes = _totalTypes;
+             $scope.trafficSourceData = _trafficSourceData;
+
+            // if (_totalTypes >= $scope.totalTypes) {
+            //     $scope.totalTypes = _totalTypes;
+            //     if ($scope.firstQuery) {
+            //         $scope.totalTypes = _totalTypes;
+            //         $scope.trafficSourceData = _trafficSourceData;
+            //     } else {
+            //         $scope.trafficSourcesConfig.series[0].data = _trafficSourceData;
+            //     }
+            // }
+
+            // // ======================================
+            // // New vs. Returning Customers
+            // // ======================================
+
+            var newVsReturning = [
+                ['New', results[14].result],
+                ['Returning', results[13].result]
+            ];
+            $scope.newVsReturning = newVsReturning;
+
+            // // ======================================
+            // // Content
+            // // Time on Site, Bounces
+            // // ======================================
+
+            //  //"filters": [{"property_name":"url.domain","operator":"eq","property_value":"main.indigenous.local"}],
+
+            //  // "entrances":{
+            //  //            "analysis_type":"count"
+            //  //        },
+            //  //        "exits":{
+            //  //            "analysis_type":"count"
+            //  //        },
+            //  //        "bounces":{
+            //  //            "analysis_type":"count"
+            //  //        }
+            //  //    }
+
+            // ======================================
+            // Visitor Locations
+            // ======================================
+            var locationData = [];
+            console.log('locationData >>> ', results[0]);
+            for (var i = 0; i < results[0].result.length; i++) {
+                var subObj = {};
+                subObj.code = ChartAnalyticsService.stateToAbbr(results[0].result[i]['ip_geo_info.province']);
+                subObj.value = results[0].result[i].result;
+                locationData.push(subObj);
+            };
+            $scope.locationData = locationData;
+            // // ======================================
+            // // Page Depth
+            // // ======================================
+
+
+            // var _depthValues = [];
+            // for (var i = 0; i < results[16].result.length; i++) {
+            //     _depthValues.push( results[16].result[i].result );
+            // };
+
+            // var testing = $scope.countDuplicates(_depthValues);
+
+            // if($scope.firstQuery) {
+            //     ngProgress.complete();
+            //     $scope.renderAnalyticsChart();
+            //     $scope.firstQuery = false;
+            // }
+
+
+            //put all data is reportData
+            
+            $scope.displayVisitors = $scope.visitors > 0;                    
+            $scope.renderAnalyticsCharts();
+      }
 
       $scope.renderAnalyticsCharts = function() {
 
