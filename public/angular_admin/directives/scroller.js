81e8cdaa58a82e25a48ff07551a8291665f4aa2d
Jaideep Das <jaideep.jdof@gmail.com>
2015-03-23 19:39:42 +0530

    updated scroller directive

--- public/angular_admin/directives/scroller.js
+++ public/angular_admin/directives/scroller.js
@@ -2,12 +2,17 @@ define(['angularAMD'], function (angularAMD) {
     angularAMD.directive('indiscroller', function() {
         return {
             restrict: 'A',
+            scope: {
+              scrollFn: '=indiscrollerUpdate',
+              savePosFn: '=indiscrollerSave'
+            },
             link: function (scope, elem, attrs) {
+              console.log(scope.indiscrollerUpdate);
                 rawElement = elem[0];
                 elem.bind('scroll', function () {
                     if((rawElement.scrollTop + rawElement.offsetHeight + 1000 ) >= rawElement.scrollHeight){
-                        scope.$apply('customerScrollFn()');
-                        scope.$apply('saveScrollFn(' + rawElement.scrollTop + ')');
+                        scope.scrollFn();
+                        scope.savePosFn();
                     }
                 });
             }
