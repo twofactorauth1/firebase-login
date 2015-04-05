1006b014ba3df7cadf7f72de4c2381211a4fc06e
Brad Risse <bradly.risse@gmail.com>
2015-04-05 02:17:23 -0700

    Remove Templates/Pipeshift

--- public/pipeshift/js/modules/video/service/Subscriber.js
+++
@@ -1,7 +0,0 @@
-angular.module('var.services').factory('Subscriber', ['$resource', function ($resource) {
-    var Subscriber = $resource("/api/1.0/courses/:id/subscribers", {
-        id: '@id'
-    }, {
-    });
-    return Subscriber;
-}]);
