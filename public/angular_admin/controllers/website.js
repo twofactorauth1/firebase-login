ca7f50fa79b1ddf6e353ba964fb0f85aaae69ecf
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-04-07 17:36:12 +0530

    More fixes for #2516

--- public/angular_admin/controllers/website.js
+++ public/angular_admin/controllers/website.js
@@ -998,8 +998,9 @@ define([
               var matchingComponent = _.findWhere($scope.blogPage.components, {
                 _id: componentId
               });
-
-              //get all the editable variables and replace the ones in view with variables in DB
+              if(matchingComponent)
+              {
+                //get all the editable variables and replace the ones in view with variables in DB
               var componentEditable = editedPageComponents[i].querySelectorAll('.editable');
               if (componentEditable.length >= 1) {
                 for (var i2 = 0; i2 < componentEditable.length; i2++) {
@@ -1069,6 +1070,8 @@ define([
                   }
                 }
               }
+              }
+              
             }
           };
 
@@ -1136,8 +1139,9 @@ define([
             var matchingComponent = _.findWhere($scope.currentPage.components, {
               _id: componentId
             });
-
-            //get all the editable variables and replace the ones in view with variables in DB
+            if(matchingComponent)
+            {
+              //get all the editable variables and replace the ones in view with variables in DB
             var componentEditable = editedPageComponents[i].querySelectorAll('.editable');
             if (componentEditable.length >= 1) {
               for (var i2 = 0; i2 < componentEditable.length; i2++) {
@@ -1207,6 +1211,7 @@ define([
                 }
               }
             }
+          }
 
             $scope.backup = {};
           };
@@ -1616,8 +1621,9 @@ define([
         var matchingComponent = _.findWhere($scope.currentPage.components, {
           _id: componentId
         });
-
-        var editedComponent = iFrame.contentWindow.document.getElementsByTagName("body")[0].querySelectorAll('.component[data-id="' + componentId + '"]');
+        if(matchingComponent)
+        {
+          var editedComponent = iFrame.contentWindow.document.getElementsByTagName("body")[0].querySelectorAll('.component[data-id="' + componentId + '"]');
         if (editedComponent && editedComponent.length > 0) {
           //get all the editable variables and replace the ones in view with variables in DB
           var componentEditable = editedComponent[0].querySelectorAll('.editable');
@@ -1692,6 +1698,8 @@ define([
             }
           }
         }
+        }
+        
         return matchingComponent;
       }
 
