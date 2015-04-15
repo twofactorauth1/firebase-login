b195441ab7597b8c355346bf415b5b1d2eeb6170
Jaideep Das <jaideep.jdof@gmail.com>
2015-04-15 15:39:47 +0530

    added alt fetch logic for profile and pages

--- public/admin/assets/js/controllers/socialFeedCtrl.js
+++ public/admin/assets/js/controllers/socialFeedCtrl.js
@@ -350,8 +350,13 @@
                     $scope.addCommentAdminPage = $scope.fbAdminPages[0];
                     $scope.isLoaded = true;
 
-                    //$scope.altFetchProfileFn();
-                    $scope.altFetchPageFn();
+                    if ($scope.feedTypes) {
+                        $scope.altFetchProfileFn();
+                    }
+
+                    if ($scope.fbAdminPages) {
+                        $scope.altFetchPageFn();
+                    }
                 });
         });
 
@@ -359,12 +364,12 @@
             $scope.config.socialAccounts.forEach(function(socialAccount, socialAccountIndex) {
                 if (socialAccount.type == 'fb' && socialAccount.accountType == 'account') {
                     SocialConfigService.getFBProfile(socialAccount.id, function(profile) {
-                        console.log(profile);
+                        $scope.typeLogic.profile.fb(profile, socialAccount.socialId);
                     });
                 }
                 if (socialAccount.type == 'tw' && socialAccount.accountType == 'account') {
                     SocialConfigService.getTwitterProfile(socialAccount.id, function(profile) {
-                        console.log(profile);
+                        $scope.typeLogic.profile.tw(profile, socialAccount.socialId);
                     });
                 }
             });
@@ -374,7 +379,7 @@
             $scope.config.socialAccounts.forEach(function(socialAccount, socialAccountIndex) {
                 if (socialAccount.type == 'fb' && socialAccount.accountType == 'account') {
                     SocialConfigService.getFBPages(socialAccount.id, function(pages) {
-                        console.log(pages);
+                        $scope.typeLogic.pages.fb.account(pages, socialAccount.socialId);
                     });
                 }
             });
