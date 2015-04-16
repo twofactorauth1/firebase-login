32ca6ac3e8bd73e5555025a47d7f88367f75a099
Jaideep Das <jaideep.jdof@gmail.com>
2015-04-16 17:29:05 +0530

    adding onboarding

--- public/admin/assets/js/directives/onboarding.js
+++ public/admin/assets/js/directives/onboarding.js
@@ -5,7 +5,9 @@
 app.directive('indigOnboarding', function() {
   return {
     restrict: 'E',
+    template: '<onboarding-popover enabled="onboardingEnabled" steps="onboardingSteps" on-finish-callback="onboardingFinishFn" step-index="onboardingIndex"></onboarding-popover>',
     link: function(scope, elem, attrs) {
+      console.log(scope, elem, attrs);
     }
   };
 });
