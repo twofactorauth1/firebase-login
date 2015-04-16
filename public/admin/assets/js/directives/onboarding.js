b9854328d41bcbac3365c1f9ec35eb1f2754a12e
Jaideep Das <jaideep.jdof@gmail.com>
2015-04-16 18:07:08 +0530

    added onboarding directive

--- public/admin/assets/js/directives/onboarding.js
+++ public/admin/assets/js/directives/onboarding.js
@@ -2,12 +2,54 @@
 /**
  * A directive used for "setting up onboarding".
  */
-app.directive('indigOnboarding', function() {
+app.directive('indigOnboarding', function($location) {
   return {
     restrict: 'E',
     template: '<onboarding-popover enabled="onboardingEnabled" steps="onboardingSteps" on-finish-callback="onboardingFinishFn" step-index="onboardingIndex"></onboarding-popover>',
+    controller: function($scope, $element, $attrs) {
+      $scope.onboardingSteps = [{
+        overlay: false
+      }];
+    },
     link: function(scope, elem, attrs) {
-      console.log(scope, elem, attrs);
+      scope.onboardingEnabled = false;
+      scope.onboardingSteps = [{
+        overlay: false
+      }];
+      scope.onboardingIndex = 0;
+      scope.onboardingStepMap = {
+        pages: [{
+          overlay: true,
+          title: 'Task: Edit home page',
+          description: "Find the home page in the list to edit.",
+          position: 'centered'
+        }, {
+          position: 'bottom',
+          overlay: false,
+          title: 'Task: Click edit',
+          width: 400,
+          description: "Once you find the page click the edit button in the tile."
+        }, {
+          position: 'bottom',
+          overlay: false,
+          title: 'Task: Save edit',
+          width: 400,
+          description: 'After all your editing is done click save in top right of the view and your are done.'
+        }]
+      };
+
+      if ($location.$$search['onboarding']) {
+        scope.obType = $location.$$search['onboarding'];
+        if (scope.obType in scope.onboardingStepMap) {
+          console.info('Found onboarding steps >>>', scope.onboardingStepMap[scope.obType]);
+          scope.onboardingSteps = scope.onboardingStepMap[scope.obType];
+          scope.onboardingEnabled = true;
+        }
+      }
+
+      scope.onboardingFinishFn = function() {
+        scope.onboardingEnabled = false;
+      };
     }
   };
 });
