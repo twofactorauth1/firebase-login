48d5a6852ca15accb27b97dc435e16a928a2260e
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-04-17 20:56:32 +0530

    Fixes for add post modal

--- public/admin/assets/js/controllers/postsCtrl.js
+++ public/admin/assets/js/controllers/postsCtrl.js
@@ -90,51 +90,8 @@
               
               $scope.cancel();
               $scope.posts.unshift(data);
+              $scope.displayedPosts.unshift(data);
             })
-        };
-
-        $scope.createPostFromTemplate = function(post, $event) {
-            $scope.validateCreatePost(post);
-            console.log('$scope.createPostValidated ', $scope.createPostValidated);
-
-            if (!$scope.createPostValidated) {
-              $scope.titleError = true;
-              $scope.handleError = true;
-              return false;
-            } else {
-              $scope.titleError = false;
-              $scope.handleError = false;
-            }
-
-            var postData = {
-              title: post.title,
-              handle: post.handle,
-              mainmenu: post.mainmenu
-            };
-
-            var hasHandle = false;
-            for (var i = 0; i < $scope.posts.length; i++) {
-              if ($scope.posts[i].handle === post.handle) {
-                hasHandle = true;
-              }
-            };
-
-            if (!hasHandle) {
-              WebsiteService.createPostFromTemplate($scope.selectedTemplate._id, postData, function(newpost) {
-                toaster.pop('success', 'Post Created', 'The ' + newpost.title + ' post was created successfully.');
-                $scope.cancel();
-                $scope.posts.unshift(newpost);
-                $scope.displayedPosts.unshift(newpost);
-                post.title = "";
-                post.handle = "";
-                $scope.showChangeURL = false;
-                $scope.templateDetails = false;
-              });
-            } else {
-              toaster.pop('error', "Post URL " + post.handle, "Already exists");
-              $event.preventDefault();
-              $event.stopPropagation();
-            }
         };
 
         $scope.viewSingle = function(post) {
