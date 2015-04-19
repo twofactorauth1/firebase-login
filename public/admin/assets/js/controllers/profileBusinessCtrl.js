70e53ad2001bbd687bc33974b3c69b82c4e354da
Brad Risse <bradly.risse@gmail.com>
2015-04-18 19:18:50 -0700

    Business Profile

---
+++ public/admin/assets/js/controllers/profileBusinessCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+/** 
+ * controller for personal business page
+ */
+(function(angular) {
+    app.controller('ProfileBusinessCtrl', ["$scope", function($scope) {
+        console.log('profile business >>> ');
+    }]);
+})(angular);
