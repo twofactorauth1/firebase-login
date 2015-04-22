acf7c2c6888760aceef12ecbebcfd305a390ecac
Brad Risse <bradly.risse@gmail.com>
2015-04-22 00:54:50 -0700

    Product Status

--- public/admin/assets/js/controllers/productsCtrl.js
+++ public/admin/assets/js/controllers/productsCtrl.js
@@ -41,6 +41,20 @@
             window.location = '/admin/#/commerce/products/'+product._id;
         };
 
+        $scope.formatStatus = function(status) {
+            var formattedStatus = ' -- ';
+            if (status) {
+                var matchingStatus = _.findWhere($scope.productStatusTypes, {
+                    data: status
+                });
+                if (matchingStatus) {
+                    formattedStatus = matchingStatus.label;
+                }
+            }
+
+            return formattedStatus;
+        };
+
         /*
          * @clearFilter
          * - clear the filter for the status when the red X is clicked
@@ -52,6 +66,20 @@
             $scope.filterProduct[filter] = {};
             $scope.triggerInput(input);
         };
+
+        $scope.productTypes = [{
+            label: "Digital",
+            data: "digital"
+        }, {
+            label: "Subscription",
+            data: "Subscription"
+        }, {
+            label: "Physical",
+            data: "Physical"
+        }, {
+            label: "External Link",
+            data: "external_link"
+        }];
 
         $scope.productStatusTypes = [{
             label: "Backorder",
