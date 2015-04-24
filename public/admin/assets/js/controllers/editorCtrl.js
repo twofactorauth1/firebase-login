5c0b8a342a2230f75eea051f62c2e64b6ee4d59f
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-04-24 14:28:12 +0530

    component do not appear Sequentially #2664

--- public/admin/assets/js/controllers/editorCtrl.js
+++ public/admin/assets/js/controllers/editorCtrl.js
@@ -1201,6 +1201,23 @@
 
                 that.originalCurrentPageComponents = $scope.currentPage.components;
                 $scope.originalCurrentPage = angular.copy($scope.currentPage);
+                
+                WebsiteService.getPages(function(pages) {
+                    var currentPage = $scope.pageSelected;
+                    var parsed = angular.fromJson(pages);
+                    var arr = [];
+
+                    for (var x in parsed) {
+                        arr.push(parsed[x]);
+                    }
+                    $scope.allPages = arr;
+                    $scope.filterdedPages = $filter('orderBy')($scope.allPages, "title", false);
+                    that.allPages = arr;
+                    WebsiteService.getPageVersions($scope.currentPage._id, function(pageVersions) {
+                        $scope.pageVersions = pageVersions;
+                    });
+               
+            });
         };
 
         /*
