49d09815865b1b59a02f5e4867a0b35c3aece53c
cyrfer <cyrfer@gmail.com>
2015-04-28 22:36:13 -0700

    twitter: moved posts html into directives for readability. #2692

---
+++ public/admin/assets/js/directives/social-feed.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * to make the cards for Facebook posts on the social feed page.
+ */
+app.directive('facebookPost', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/admin/assets/views/social-facebook-post.html',
+        replace: true,
+        scope: {
+            post: '=',
+            actionLike: '&',
+            actionComment: '&'
+        }
+    };
+});
+
+/**
+ * to make the cards for Twitter tweets on the social feed page.
+ */
+app.directive('twitterFeed', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/admin/assets/views/social-twitter-feed.html',
+        replace: true,
+        scope: {
+            post: '=',
+            actionReply: '&',
+            actionRetweet: '&',
+            actionDirectMessage: '&'
+        }
+    };
+});
+
+/**
+ * to make the cards for Facebook posts on the social feed page.
+ */
+app.directive('twitterFollower', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/admin/assets/views/social-twitter-follower.html',
+        replace: true,
+        scope: {
+            post: '=',
+            actionTweetAt: '&',
+            actionFollowBack: '&',
+            actionDirectMessage: '&'
+        }
+    };
+});
