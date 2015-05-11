919ba028ab09ae1c857abaeb610a6e37f9162d6d
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-05-11 20:40:22 +0530

    Fixes for #2890

--- public/scripts/controllers/layoutCtrl.js
+++ public/scripts/controllers/layoutCtrl.js
@@ -1812,15 +1812,14 @@ mainApp.controller('LayoutCtrl', ['$scope', '$timeout', 'pagesService', 'website
                   }
         });
         $scope.setUnderbnavMargin = function() {
-            setTimeout(function() {
-               var navHeight = angular.element("#bs-example-navbar-collapse-1").height();
-               var margin = 200 + navHeight;
-               if(angular.element(".mt200"))
-                angular.element(".mt200").css("margin-top", -margin);
-               if(angular.element(".mastHeadUndernav"))
-                angular.element(".mastHeadUndernav").css("height", margin);
-            },300);
-
+            // setTimeout(function() {
+            //    var navHeight = angular.element("#bs-example-navbar-collapse-1").height();
+            //    var margin = 200 + navHeight;
+            //    if(angular.element(".mt200"))
+            //     angular.element(".mt200").css("margin-top", -margin);
+            //    if(angular.element(".mastHeadUndernav"))
+            //     angular.element(".mastHeadUndernav").css("height", margin);
+            // },300);
         };
     }
 
