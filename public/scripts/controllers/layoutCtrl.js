2f6215f40cbd456c0f7b87f42cecd73969dc6d32
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-05-12 12:51:44 +0530

    Masthead Under Nav can no longer edit settings #2896

--- public/scripts/controllers/layoutCtrl.js
+++ public/scripts/controllers/layoutCtrl.js
@@ -1812,14 +1812,28 @@ mainApp.controller('LayoutCtrl', ['$scope', '$timeout', 'pagesService', 'website
                   }
         });
         $scope.setUnderbnavMargin = function() {
-            // setTimeout(function() {
-            //    var navHeight = angular.element("#bs-example-navbar-collapse-1").height();
-            //    var margin = 200 + navHeight;
-            //    if(angular.element(".mt200"))
-            //     angular.element(".mt200").css("margin-top", -margin);
-            //    if(angular.element(".mastHeadUndernav"))
-            //     angular.element(".mastHeadUndernav").css("height", margin);
-            // },300);
+            setTimeout(function() {
+                if($scope.allowUndernav)
+                {
+                    var navHeight = angular.element("#bs-example-navbar-collapse-1").height();
+                    var margin = 200 + navHeight;
+                   if(angular.element(".mt200"))
+                        angular.element(".mt200").css("margin-top", -margin);
+                   if(angular.element(".mastHeadUndernav"))
+                        angular.element(".mastHeadUndernav").css("height", margin);
+                   if(angular.element(".masthead-actions"))
+                        angular.element(".masthead-actions").css("margin-top", margin-4);
+
+                }
+                else
+                {
+                    if(angular.element(".mt200"))
+                        angular.element(".mt200").css("margin-top", 0);
+                    if(angular.element(".masthead-actions"))
+                        angular.element(".masthead-actions").css("margin-top", 0);
+                }
+               
+            },300);
         };
     }
 
