f3adcaac83e6a1cc005c07ab22646d8698c1c8ab
Brad Risse <bradly.risse@gmail.com>
2015-05-16 21:57:19 -0700

    States validation

--- public/admin/assets/js/controllers/siteAnalyticsCtrl.js
+++ public/admin/assets/js/controllers/siteAnalyticsCtrl.js
@@ -120,26 +120,30 @@
     };
 
     $scope.locationReportData = function (result0) {
+      console.log('result0 ', result0);
       // ======================================
       // Visitor Locations
       // ======================================
 
 
       var locationData = [];
-      _.each(result0, function (location) {
-        var _geo_info = ChartAnalyticsService.stateToAbbr(location['ip_geo_info.province']);
-        if (_geo_info) {
-          var subObj = {};
-          subObj.code = _geo_info;
-          subObj.value = location.result;
-          var locationExists = _.find(locationData, function (loc) {
-            return loc.code === location.code;
-          });
-          if (!locationExists && subObj.value) {
-            locationData.push(subObj);
+      if (result0) {
+        _.each(result0, function (location) {
+          console.log('location >>> ', location);
+          var _geo_info = ChartAnalyticsService.stateToAbbr(location['ip_geo_info.province']);
+          if (_geo_info) {
+            var subObj = {};
+            subObj.code = _geo_info;
+            subObj.value = location.result;
+            var locationExists = _.find(locationData, function (loc) {
+              return loc.code === location.code;
+            });
+            if (!locationExists && subObj.value) {
+              locationData.push(subObj);
+            }
           }
-        }
-      });
+        });
+      }
 
       console.log('locationData >>> ', locationData);
 
@@ -304,9 +308,9 @@
 
       $scope.contentInteractionsReportData(results[8].result, results[10].result, results[11].result);
 
-      // // ======================================
-      // // Traffic Sources
-      // // ======================================
+      // ======================================
+      // Traffic Sources
+      // ======================================
 
       var _trafficSourceData = [];
       var _totalTypes = 0;
@@ -330,9 +334,9 @@
         $scope.trafficSourcesConfig.loading = false;
       });
 
-      // // ======================================
-      // // New vs. Returning Customers
-      // // ======================================
+      // ======================================
+      // New vs. Returning Customers
+      // ======================================
 
       var newVsReturning = [
         ['New', results[14].result],
@@ -347,23 +351,11 @@
       });
 
 
-      // // ======================================
-      // // Content
-      // // Time on Site, Bounces
-      // // ======================================
+      // ======================================
+      // Content
+      // Time on Site, Bounces
+      // ======================================
 
-      //  //"filters": [{"property_name":"url.domain","operator":"eq","property_value":"main.indigenous.local"}],
-
-      //  // "entrances":{
-      //  //            "analysis_type":"count"
-      //  //        },
-      //  //        "exits":{
-      //  //            "analysis_type":"count"
-      //  //        },
-      //  //        "bounces":{
-      //  //            "analysis_type":"count"
-      //  //        }
-      //  //    }
       $scope.locationReportData(results[0].result);
 
       $scope.displayVisitors = $scope.visitors > 0;
@@ -384,62 +376,5 @@
       }
     };
 
-    $scope.data = {
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      datasets: [{
-        label: 'My First dataset',
-        fillColor: 'rgba(220,220,220,0.2)',
-        strokeColor: 'rgba(220,220,220,1)',
-        pointColor: 'rgba(220,220,220,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
-        data: [65, 59, 80, 81, 56, 55, 40, 84, 64, 120, 132, 87]
-      }, {
-        label: 'My Second dataset',
-        fillColor: 'rgba(151,187,205,0.2)',
-        strokeColor: 'rgba(151,187,205,1)',
-        pointColor: 'rgba(151,187,205,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(151,187,205,1)',
-        data: [28, 48, 40, 19, 86, 27, 90, 102, 123, 145, 60, 161]
-      }]
-    };
-
-    $scope.options = {
-
-      maintainAspectRatio: false,
-      // Sets the chart to be responsive
-      responsive: true,
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines: true,
-      //String - Colour of the grid lines
-      scaleGridLineColor: 'rgba(0,0,0,.05)',
-      //Number - Width of the grid lines
-      scaleGridLineWidth: 1,
-      //Boolean - Whether the line is curved between points
-      bezierCurve: false,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension: 0.4,
-      //Boolean - Whether to show a dot for each point
-      pointDot: true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius: 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth: 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius: 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke: true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth: 2,
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill: true,
-      // Function - on animation progress onAnimationProgress: function () {},
-      // Function - on animation complete onAnimationComplete: function () {},
-      //String - A legend template
-      legendTemplate: '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-    };
   }]);
 }(angular));
