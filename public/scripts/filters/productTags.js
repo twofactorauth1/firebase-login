e68475fd75d5b477786e1f1d0c9c15049ebfd024
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-06-18 12:43:54 +0530

    Product setup and experience with CMS still not working. Product Component should inherit products and display dynamically

--- public/scripts/filters/productTags.js
+++ public/scripts/filters/productTags.js
@@ -1,14 +1,20 @@
 mainApp.filter('selectedTags', function() {
     return function(products, tags) {
-        if(products && tags && tags.length)
+        if(products)
         {
             return products.filter(function(product) {
-                for (var i in product.tags) {
+                if(!tags || tags.length === 0)
+                {
+                    return true;
+                }
+                else{
+                    for (var i in product.tags) {
                     if (product.status === 'active' && tags.indexOf(product.tags[i]) != -1) {
                         return true;
                     }
                 }
                 return false;
+            }                
             });
         }
     };
