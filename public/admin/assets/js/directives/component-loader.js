ecd82b1f4dd18048b436e00f44e18c73699244ce
cyrfer <cyrfer@gmail.com>
2015-06-27 16:41:01 -0700

    ng-switch for cms components.

--- public/admin/assets/js/directives/component-loader.js
+++ public/admin/assets/js/directives/component-loader.js
@@ -5,26 +5,26 @@
 
 app.directive('componentLoader', ['$filter', '$compile', function ($filter, $compile) {
   return {
-    template: '<div id="componentloader"></div>',
+    templateUrl: '/admin/assets/views/partials/component-loader.html',
 
-    link: function (scope, element, attributes, controller) {
-
-      var insertionPoint = element.find('#componentloader');
-      var components = scope.components;
-      scope.$watch('components', function (newValue, oldValue) {
-        if (newValue) {
-          var template = '';
-          _.each(newValue, function (value, index) {
-            var valueProp = 'value' + index;
-            scope[valueProp] = value;
-            var directiveString = "<div " + value.type + "-component component=" + valueProp + " version="+ value.version +"></div>";
-
-            var compiledElement = $compile(directiveString)(scope);
-
-            insertionPoint.append(compiledElement);
-          });
-        }
-      });
-    }
+    //link: function (scope, element, attributes, controller) {
+    //
+    //  var insertionPoint = element.find('#componentloader');
+    //  var components = scope.components;
+    //  scope.$watch('components', function (newValue, oldValue) {
+    //    if (newValue) {
+    //      var template = '';
+    //      _.each(newValue, function (value, index) {
+    //        var valueProp = 'value' + index;
+    //        scope[valueProp] = value;
+    //        var directiveString = "<div " + value.type + "-component component=" + valueProp + " version="+ value.version +"></div>";
+    //
+    //        var compiledElement = $compile(directiveString)(scope);
+    //
+    //        insertionPoint.append(compiledElement);
+    //      });
+    //    }
+    //  });
+    //}
   }
 }]);
