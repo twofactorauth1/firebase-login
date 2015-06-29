6d6a2ad39ed2a7c3b72fa1c27439f23e9762aa36
Brad Risse <bradly.risse@gmail.com>
2015-06-28 23:28:08 -0700

    Editor Changes

---
+++ public/scripts/controllers/layoutCtrl.js
@@ -0,0 +1,13 @@
+'use strict';
+
+mainApp.controller('LayoutCtrl', ['$scope', '$timeout', 'pagesService', 'websiteService', 'postsService', 'userService', 'accountService', 'ENV', '$window', '$location', '$route', '$routeParams', '$filter', '$document', '$anchorScroll', '$sce', 'postService', 'paymentService', 'productService', 'courseService', 'ipCookie', '$q', 'customerService', 'pageService', 'analyticsService', 'leafletData', 'cartService',
+  function ($scope, $timeout, pagesService, websiteService, postsService, userService, accountService, ENV, $window, $location, $route, $routeParams, $filter, $document, $anchorScroll, $sce, PostService, PaymentService, ProductService, CourseService, ipCookie, $q, customerService, pageService, analyticsService, leafletData, cartService) {
+    $scope.isEditing = false;
+
+    pagesService(function (err, data) {
+      $scope.page = data;
+      $scope.components = data.components;
+    });
+
+  }
+]);
