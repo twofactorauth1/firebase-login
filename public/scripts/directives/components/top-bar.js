81cff5c290cfc19fb725ce85173147a4db935096
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-07-01 14:53:05 +0530

    Top bar component changes

--- public/scripts/directives/components/top-bar.js
+++ public/scripts/directives/components/top-bar.js
@@ -6,7 +6,12 @@ app.directive('topBarComponent', function () {
     },
     templateUrl: '/components/component-wrap.html',
     link: function (scope, element, attrs, ctrl) {
-      scope.isEditing = true;
+      scope.getUrl = function (value) {
+        if (value && !/http[s]?/.test(value)) {
+          value = 'http://' + value;
+        }
+        return value;
+      };
     }
   }
 });
