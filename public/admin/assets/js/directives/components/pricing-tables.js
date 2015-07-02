0f4703c3cdab6bc6e3e06d173b9fa55cde1e4443
Brad Risse <bradly.risse@gmail.com>
2015-07-02 13:04:26 -0700

    Pre Pricing Table

--- public/admin/assets/js/directives/components/pricing-tables.js
+++ public/admin/assets/js/directives/components/pricing-tables.js
@@ -4,6 +4,21 @@ app.directive('pricingTablesComponent', function () {
       component: '=',
       version: '='
     },
-    templateUrl: '/components/component-wrap.html'
+    templateUrl: '/components/component-wrap.html',
+    link: function (scope, element, attrs, ctrl) {
+      scope.isEditing = true;
+      scope.addPricingTableFeature = function (componentId, index) {
+        console.log('add feature');
+      }
+      scope.deletePricingTableFeature = function (componentId, index) {
+        console.log('delete feature');
+      }
+      scope.addPricingTable = function (componentId, index) {
+      	console.log('add table');
+      }
+      scope.deletePricingTable = function (componentId, index) {
+      	console.log('delete table');
+      }
+    }
   }
 });
