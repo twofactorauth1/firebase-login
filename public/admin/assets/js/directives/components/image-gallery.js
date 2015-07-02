7f9f94dd06dee5ef9703edc270c90b2a6352328a
Brad Risse <bradly.risse@gmail.com>
2015-07-01 20:22:52 -0700

    Image Gallery

--- public/admin/assets/js/directives/components/image-gallery.js
+++ public/admin/assets/js/directives/components/image-gallery.js
@@ -1,9 +1,30 @@
-app.directive('imageGalleryComponent', function () {
+app.directive('imageGalleryComponent', ['$timeout', function ($timeout) {
   return {
     scope: {
       component: '=',
-      version: '='
+      version: '=',
+      media: '&'
     },
-    templateUrl: '/components/component-wrap.html'
+    templateUrl: '/components/component-wrap.html',
+    link: function (scope, element, attrs, ctrl) {
+      scope.isEditing = true;
+      /*
+       * @addImageToGallery
+       * -
+       */
+
+      scope.addImageToGallery = function (componentId, index) {
+      	scope.media({componentId: componentId, index: index});
+      };
+
+      /*
+       * @deleteImageFromGallery
+       * -
+       */
+
+      scope.deleteImageFromGallery = function (componentId, index) {
+        scope.component.images.splice(index, 1);
+      };
+    }
   }
-});
+}]);
