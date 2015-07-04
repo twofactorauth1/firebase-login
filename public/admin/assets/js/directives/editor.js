1b0a19d57ce965f7cd9f2d5b02e0de3c7ee3c102
Brad Risse <bradly.risse@gmail.com>
2015-07-04 00:20:41 -0700

    Bind Ngmodel to ckeditor data

--- public/admin/assets/js/directives/editor.js
+++ public/admin/assets/js/directives/editor.js
@@ -3,42 +3,50 @@
 /*jslint unparam:true*/
 app.directive("elem", function ($timeout) {
   return {
+    require: '?ngModel',
     replace: true,
     transclude: true,
     scope: {
       title: '@ngModel',
-      ngModel: '=',
       className: '@className'
     },
     template: '<div class="edit-wrap"><span class="editable-title">{{title | formatText}}</span><div class="editable {{className}}" ng-bind-html="ngModel | unsafe"></div></div>',
-    link: function (scope, element, attrs, ctrl) {
-      $timeout(function () {
+    link: function (scope, element, attrs, ngModel) {
 
-        scope.update = function (e) {
-          scope.$apply(function () {
-            scope.ngModel = e.editor.getData();
-          });
-        };
-
-        scope.delay = null;
-        scope.initial = true;
-
-        var elem = angular.element(element[0].querySelector('.editable'))[0];
-
-        CKEDITOR.inline(elem, {
-          on: {
-            instanceReady: function (ev) {
-              var editor = ev.editor;
-              editor.setReadOnly(false);
-              editor.on('change', function (e) {
-                scope.update(e);
-              });
-            }
-          },
-          sharedSpaces: {
-            top: 'editor-toolbar'
-          }
+      scope.update = function (e) {
+        scope.$apply(function () {
+          ngModel.$setViewValue(e.editor.getData());
         });
+      };
+
+      var elem = angular.element(element[0].querySelector('.editable'))[0];
+
+      CKEDITOR.inline(elem, {
+        on: {
+          instanceReady: function (ev) {
+            var editor = ev.editor;
+            editor.setReadOnly(false);
+            editor.setData(ngModel.$viewValue);
+            editor.on('change', function (e) {
+              scope.update(e);
+            });
+            editor.on('dataReady', function (e) {
+              scope.update(e);
+            });
+            editor.on('key', function (e) {
+              scope.update(e);
+            });
+            editor.on('paste', function (e) {
+              scope.update(e);
+            });
+            editor.on('selectionChange', function (e) {
+              scope.update(e);
+            });
+          }
+        },
+        sharedSpaces: {
+          top: 'editor-toolbar'
+        }
       });
     }
   };
