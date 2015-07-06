9fb7c3653cfa3fff1cf1ac6df6a903bd77f5e004
Brad Risse <bradly.risse@gmail.com>
2015-07-06 13:04:27 -0700

    #3487

--- public/admin/assets/js/directives/components/single-post.js
+++ public/admin/assets/js/directives/components/single-post.js
@@ -7,6 +7,51 @@ app.directive('singlePostComponent', function () {
     templateUrl: '/components/component-wrap.html',
     link: function (scope, element, attrs) {
       scope.isEditing = true;
+      /*
+       * @dateOptions
+       * -
+       */
+
+      scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+      };
+
+      /*
+       * @open
+       * -
+       */
+
+      scope.open = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        scope.opened = !scope.opened;
+      };
+
+      /*
+       * @endOpen
+       * -
+       */
+
+      scope.endOpen = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        scope.startOpened = false;
+        scope.endOpened = !scope.endOpened;
+      };
+
+      /*
+       * @startOpen
+       * -
+       */
+
+      scope.startOpen = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        scope.endOpened = false;
+        scope.startOpened = !scope.startOpened;
+      };
     }
   }
 });
