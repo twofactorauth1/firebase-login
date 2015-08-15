08e40cde3f8c2e5d436ae8b94572d79b3368e8f8
cyrfer <cyrfer@gmail.com>
2015-08-15 12:49:01 -0700

    fixed non-Active products appearing, #4005

--- public/scripts/services/productService.js
+++ public/scripts/services/productService.js
@@ -4,32 +4,43 @@
  * */
 'use strict';
 mainApp.service('productService', function ($http) {
-  var baseUrl = '/api/1.0/products/';
+  var baseUrl = '/api/1.0/products';
 
   this.getProduct = function (productId, fn) {
-    var apiUrl = baseUrl + [productId].join('/');
+    var apiUrl = [baseUrl, productId].join('/');
     $http.get(apiUrl)
-      .success(function (data, status, headers, config) {
-        fn(data);
-      });
+        .success(function (data, status, headers, config) {
+          fn(data);
+        });
   };
 
   this.getAllProducts = function (fn) {
     var apiUrl = baseUrl;
     $http.get(apiUrl, {
-        cache: true
-      })
-      .success(function (data, status, headers, config) {
-        fn(data);
-      });
+      cache: true
+    })
+        .success(function (data, status, headers, config) {
+          fn(data);
+        });
+  };
+
+  this.getActiveProducts = function (fn) {
+    var apiUrl = [baseUrl, 'active'].join('/');
+    console.log(apiUrl);
+    $http.get(apiUrl, {
+      cache: true
+    })
+        .success(function (data, status, headers, config) {
+          fn(data);
+        });
   };
 
   this.getTax = function (postcode, fn) {
-    var apiUrl = baseUrl + ['tax', postcode].join('/');
+    var apiUrl = [baseUrl, 'tax', postcode].join('/');
     $http.get(apiUrl)
-      .success(function (data, status, headers, config) {
-        fn(data);
-      });
+        .success(function (data, status, headers, config) {
+          fn(data);
+        });
   };
 
 });
