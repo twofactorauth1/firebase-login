06084b7eb8cc3b0962639ef7cd531cb302b92690
Kyle Miller <millkyl@gmail.com>
2015-08-25 12:40:36 -0500

    Re-disable console logging in prod.

--- gruntfile.js
+++ gruntfile.js
@@ -274,7 +274,7 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 compress: {
-                    //drop_console: true
+                    drop_console: true
                 }
             },
             js: {
