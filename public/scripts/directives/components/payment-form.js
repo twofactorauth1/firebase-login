a93839df7c21f12328cc91df18b80c2cfc4560f7
Kyle Miller <millkyl@gmail.com>
2015-08-25 13:31:42 -0500

    Remove pixel from head.  Only fire after signup.

--- public/scripts/directives/components/payment-form.js
+++ public/scripts/directives/components/payment-form.js
@@ -379,8 +379,12 @@ app.directive('paymentFormComponent', ['$filter', '$q', 'productService', 'payme
                                     app_id: "b3st2skm"
                                 };
                                 //send facebook tracking info
-                                window._fbq = window._fbq || [];
-                                window._fbq.push(['track', '6032779610613', {'value':'0.00','currency':'USD'}]);
+                                //window._fbq = window._fbq || [];
+                                //window._fbq.push(['track', '6032779610613', {'value':'0.00','currency':'USD'}]);
+                                !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+                                fbq('init', '1477395922560651');
+                                fbq('track', 'PageView');
                                 console.log('sent facebook message');
                                 //send affiliate purchase info
                                 var leadData = {
