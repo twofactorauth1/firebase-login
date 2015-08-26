f678dcaff0fe7360a005c726efebcf25fbadf698
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-08-26 18:23:53 +0530

    #3828

--- public/scripts/directives/components/products.js
+++ public/scripts/directives/components/products.js
@@ -677,6 +677,7 @@ app.directive('productsComponent', ['$timeout', 'paymentService', 'productServic
        angular.element(document).ready(function () {
         $timeout(function () {
           angular.element('#cart-checkout-modal').on('hidden.bs.modal', function () {            
+            console.log("modal closed");
             $timeout(function () {
               scope.$apply(function () {
                 if (scope.checkoutModalState === 5) {
