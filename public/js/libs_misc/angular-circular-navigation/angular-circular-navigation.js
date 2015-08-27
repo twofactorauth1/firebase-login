b933f477f48f32dfa0710a9b3af986a31db7d1b0
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-08-27 17:33:06 +0530

    IE issue 4167

--- public/js/libs_misc/angular-circular-navigation/angular-circular-navigation.js
+++ public/js/libs_misc/angular-circular-navigation/angular-circular-navigation.js
@@ -18,15 +18,12 @@
           passedComponent: '=',
           passedComponentLength: '@'
         },
-        template: '<div ng-mouseenter="toggleMenu()"><button class="cn-button {{options.button.size}}" ng-style="{background: options.button.background ? \'{{options.button.background}}\' : \'{{options.background}}\', color: options.button.color ? \'{{options.button.color}}\' : \'{{options.color}}\'}"><span ng-class="options.button.cssClass"></span><div class="clearfix"></div><p class="text-small" ng-text-truncate="passedComponent.type" ng-tt-chars-threshold="10" ng-tt-no-toggling></p></button>' +
+        template: '<div ng-mouseenter="toggleMenu()" ng-mouseleave="hideIt()"><button class="cn-button {{options.button.size}}" ng-style="{background: options.button.background ? \'{{options.button.background}}\' : \'{{options.background}}\', color: options.button.color ? \'{{options.button.color}}\' : \'{{options.color}}\'}"><span ng-class="options.button.cssClass"></span><div class="clearfix"></div><p class="text-small" ng-text-truncate="passedComponent.type" ng-tt-chars-threshold="10" ng-tt-no-toggling></p></button>' +
           '<div class="cn-wrapper {{options.size}} items-{{options.items.length}}" ng-class="{\'opened-nav\': options.isOpen}"><ul>' +
           '<li ng-repeat="item in options.items">' +
-          '<a ng-if="!hideItem(item)" ng-hide="item.empty" ng-click="perform(options, item)" ng-class="{\'is-active\': item.isActive}" class="{{item.cssClass}}" ng-style="{background: item.background ? \'{{item.background}}\' : \'{{options.background}}\', color: item.color ? \'{{item.color}}\' : \'{{options.color}}\'}">' +
-          '<span ng-bind-html="item.content"></span>' +
-          '</a><a ng-if="hideItem(item)" ng-hide="item.empty" class="{{item.cssClass}} linkDisabled" ng-style="{color: item.color ? \'{{item.color}}\' : \'{{options.color}}\'}">' +
-          '<span ng-bind-html="item.content"></span>' +
-          '</a>' +
-          '</li></ul></div>',
+          '<a ng-hide="item.empty || hideItem(item)" ng-click="perform(options, item)" ng-class="{\'is-active\': item.isActive}" class="{{item.cssClass}}" ng-style="{background: item.background ? \'{{item.background}}\' : \'{{options.background}}\', color: item.color ? \'{{item.color}}\' : \'{{options.color}}\'}">' +
+          '<span ng-bind-html="item.content"></span></div>' +
+          '</a></li></ul></div>',
         link: function (scope, $element, $attrs) {
           
           scope.options = angular.copy(scope.passedOptions);
