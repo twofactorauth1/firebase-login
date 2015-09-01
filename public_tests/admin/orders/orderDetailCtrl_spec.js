901713b8ca255e7bd4f6bcc60b3e770d22fb8f3b
Brad Risse <bradly.risse@gmail.com>
2015-09-01 12:10:20 -0700

    Order Detail test Revert

--- public_tests/admin/orders/orderDetailCtrl_spec.js
+++ public_tests/admin/orders/orderDetailCtrl_spec.js
@@ -24,48 +24,52 @@ describe('test orderDetailCtrl', function () {
     $scope = $rootScope.$new();
     $controller = _$controller_;
 
+    $controller('OrderDetailCtrl', {
+      '$rootScope': $rootScope,
+      '$scope': $scope
+    });
   }));
 
-  // describe('$scope.totalWithDiscount', function () {
-  //   it('should equal 2', function () {
-  //     expect($scope.totalWithDiscount(1, 1)).toEqual(2);
-  //   });
-  // });
+  describe('$scope.totalWithDiscount', function() {
+    it('should equal 2', function () {
+      expect($scope.totalWithDiscount(1, 1)).toEqual(2);
+    });
+  });
 
-  // describe('$scope.getUsers', function() {
-  //   it('should return users', function() {
+  describe('$scope.getUsers', function() {
+    it('should return users', function() {
 
-  //     // JKG- difficult to test preconditions
-  //     // pre-conditions
-  //     //expect($scope.users).toBeDefined();
-  //     //expect($scope.users.length).toBe(0);
+      // JKG- difficult to test preconditions
+      // pre-conditions
+      //expect($scope.users).toBeDefined();
+      //expect($scope.users.length).toBe(0);
 
-  //     // garbage data because the point is not the data schema,
-  //     // but the point is that some data was moved.
-  //     var perfectMembers = [
-  //       {
-  //         name: 'user1',
-  //       },
-  //       {
-  //         name: 'user2',
-  //       },
-  //     ];
+      // garbage data because the point is not the data schema,
+      // but the point is that some data was moved.
+      var perfectMembers = [
+        {
+          name: 'user1',
+        },
+        {
+          name: 'user2',
+        },
+      ];
 
-  //     // JKG - why all the extra api hits? should just be /members, right?
-  //     $httpBackend.whenGET('assets/i18n/en.json').respond(perfectMembers);
-  //     $httpBackend.whenGET('/api/1.0/contact').respond(perfectMembers);
-  //     $httpBackend.whenGET('/api/1.0/user/members').respond(perfectMembers);
-  //     $httpBackend.whenGET('/api/1.0/products').respond(perfectMembers);
-  //     $httpBackend.whenGET('/api/1.0/orders/').respond(perfectMembers);
+      // JKG - why all the extra api hits? should just be /members, right?
+      $httpBackend.whenGET('assets/i18n/en.json').respond(perfectMembers);
+      $httpBackend.whenGET('/api/1.0/contact').respond(perfectMembers);
+      $httpBackend.whenGET('/api/1.0/user/members').respond(perfectMembers);
+      $httpBackend.whenGET('/api/1.0/products').respond(perfectMembers);
+      $httpBackend.whenGET('/api/1.0/orders/').respond(perfectMembers);
 
-  //     // execute function to be tested
-  //     $scope.getUsers();
+      // execute function to be tested
+      $scope.getUsers();
 
-  //     $httpBackend.flush();
+      $httpBackend.flush();
 
-  //     // post-conditions
-  //     expect($scope.users.length).toBeGreaterThan(0);
-  //   })
-  // });
+      // post-conditions
+      expect($scope.users.length).toBeGreaterThan(0);
+    })
+  });
 
 });
