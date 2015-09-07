3d54850bd459e0c2bb890ed14ff5f625c43e6795
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-09-07 13:47:22 +0530

    Console error for contact us component for new account

--- public/admin/assets/js/directives/components/contact-us.js
+++ public/admin/assets/js/directives/components/contact-us.js
@@ -56,7 +56,7 @@ app.directive('contactUsComponent', ['AccountService', 'GeocodeService', '$timeo
 
       scope.checkBusinessAddress = function (fn) {
         AccountService.getAccount(function (account) {
-          if (account.business.addresses.length > -1) {
+          if (account.business.addresses && account.business.addresses.length > -1) {
             scope.component.location = account.business.addresses[0];
             if (fn) {
               fn();
