93556221113ba9ad6984a45fcfa17c7fea8be236
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-09-09 18:58:26 +0530

    Contact Us Address #3213

--- cms/model/components/contact-us.js
+++ cms/model/components/contact-us.js
@@ -143,6 +143,14 @@ var component = $$.m.ModelBase.extend({
                     show: false
                 },
                 color : "#FFFFFF"
+            },
+
+             /**
+             * Custom fields Hours and Address(If not custom then business details)
+             */
+            custom:{
+                hours: false,
+                address: false
             }
         }
     },
