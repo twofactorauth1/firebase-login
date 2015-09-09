03f8859c10636b0a932194fd99d883f0e8c9d486
Kyle Miller <millkyl@gmail.com>
2015-09-09 11:07:31 -0500

    Updating account list

--- utils/accountActivity.js
+++ utils/accountActivity.js
@@ -26,7 +26,8 @@ var accountActivity = {
     runReport: function(callback) {
         var self = this;
         var reportAry = [109, 45, 97, 79, 21, 37, 38, 12, 15, 126, 129, 130, 132, 133, 134, 135, 136, 137,
-                            138, 139, 140, 141, 142, 158, 161,169,170,171];
+                            138, 139, 140, 141, 142, 158, 161,169,170,171,172,173,174, 175,176,177,178,179,
+                            181,182,183,184];
         var activityAry = [];
         async.each(reportAry, function(accountId, cb){
             self.getActivityForAccount(accountId, function(err, activity){
