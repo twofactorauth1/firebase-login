108135ef78e9f36cacdce45f090ae5796dc39e7d
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-10-05 10:50:36 +0530

    #4535

--- public/scripts/directives/components/contact-us.js
+++ public/scripts/directives/components/contact-us.js
@@ -32,7 +32,7 @@ app.directive('contactUsComponent', ['geocodeService', 'accountService', '$timeo
       }
       function hexToRgb(hex, opacity) {      
         var c;
-        opacity = opacity || 1;
+        opacity = angular.isDefined(opacity) ? opacity : 1;
         c = hex.substring(1).split('');
         if(c.length== 3){
             c= [c[0], c[0], c[1], c[1], c[2], c[2]];
