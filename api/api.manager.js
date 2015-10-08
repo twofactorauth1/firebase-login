cb824685f7c410d967e2fa116fd7144913787961
Kyle Miller <millkyl@gmail.com>
2015-10-08 14:27:26 -0500

    #4340 prevent status changes on draft campaigns until running.  Delete old apis

--- api/api.manager.js
+++ api/api.manager.js
@@ -34,8 +34,6 @@ var twonetAdapterApi = require('./1.0/twonetadapter.api');
 var runkeeperApi = require('./1.0/runkeeperadapter.api');
 
 // Campaigns
-var campaignManager = require('./1.0/campaignmanager.api');
-var campaignAPI = require('./1.0/campaign.api');
 var campaignsAPI = require('./1.0/campaigns.api');
 
 // Products
