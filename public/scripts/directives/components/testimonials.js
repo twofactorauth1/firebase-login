432bc6945623731b5719b6f3e5ccdd06fe72980b
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-10-08 13:20:52 +0530

    Slider config... testimonials

--- public/scripts/directives/components/testimonials.js
+++ public/scripts/directives/components/testimonials.js
@@ -4,21 +4,19 @@ app.directive('testimonialsComponent', ['$timeout', function ($timeout) {
       component: '='
     },
     templateUrl: '/components/component-wrap.html',
-    link: function (scope, element, attrs, ctrl) {
-      scope.touchMove = true;
-      scope.draggable = true;
-      
-      if(!scope.component.slider)
-      {
-        scope.component.slider = {
-          speed: 300, autoPlay: true, autoPlayInterval: 5000
-        };
-      }
-      scope.autoplay = scope.component.slider.autoPlay;
-
+    link: function (scope, element, attrs, ctrl) {      
     	$(document).ready(function () {
           $timeout(function () {
-            scope.$apply(function () {
+            scope.$apply(function () {              
+              if(!scope.component.slider)
+              {
+                scope.component.slider = {
+                  speed: 300, autoPlay: true, autoPlayInterval: 5000
+                };
+              }
+              scope.touchMove = true;
+              scope.draggable = true;
+              scope.autoplay = scope.component.slider.autoPlay;
               scope.dataLoaded = true;
             });
           },0);
