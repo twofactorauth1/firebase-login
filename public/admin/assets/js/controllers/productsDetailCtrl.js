b4a36d7505429ffc2988f13c7807d18292a11089
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-10-16 13:48:33 +0530

    #4736

--- public/admin/assets/js/controllers/productsDetailCtrl.js
+++ public/admin/assets/js/controllers/productsDetailCtrl.js
@@ -99,9 +99,8 @@
       $scope.product = product;
       console.log('product ', product);
       var p_icon = $scope.product.icon;
-      if(!p_icon.startsWith("fa-"))
+      if(p_icon && !p_icon.startsWith("fa-"))
         p_icon = "fa-cube";
-
 
       angular.element('#convert').iconpicker({
         iconset: 'fontawesome',
