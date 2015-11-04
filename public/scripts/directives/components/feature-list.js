64927522527b6c26d9752f31ffdec358b18775e3
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-11-04 19:09:24 +0530

    component layout issue #4929

--- public/scripts/directives/components/feature-list.js
+++ public/scripts/directives/components/feature-list.js
@@ -9,15 +9,22 @@ app.directive('featureListComponent',['$window', function ($window) {
     link: function (scope, element, attrs, ctrl) {
 		scope.resizeFeatureTiles = function (argument) {
 			var parent_id = scope.component.anchor || scope.component._id;
-			var element = angular.element("#"+parent_id + " div.feature-height")
+			var element = angular.element("#"+parent_id + " div.feature-height");
 			if (element && element.length) {
 			  var maxFeatureHeight = Math.max.apply(null, element.map(function () {
 			    return this.offsetHeight;
 			  }).get());
-			  element.css("min-height", maxFeatureHeight);
+			  scope.maxFeatureHeight = maxFeatureHeight;
+			  element.css("min-height", maxFeatureHeight + 1);
 			}
 		};
-		angular.element($window).bind('resize', function () {
+		function resetHeight(){
+			var parent_id = scope.component.anchor || scope.component._id;
+			var element = angular.element("#"+parent_id + " div.feature-height");
+			element.css("min-height", scope.maxFeatureHeight);
+		}
+		angular.element($window).bind('resize', function () {	
+			resetHeight();		
 			scope.resizeFeatureTiles();
 		});
 		angular.element(document).ready(function () {
