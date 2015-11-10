60a07e00adba1a81e2226c2ad41e7944a7a3b231
Chris Ricci <chris@indigenous.io>
2015-11-10 17:43:53 -0600

    Add blog-v2 (with no sidebar, tags).

---
+++ cms/model/components/blog-2.js
@@ -0,0 +1,107 @@
+/**
+ * COPYRIGHT INDIGENOUS SOFTWARE, INC., LLC 2014
+ *
+ * All use or reproduction of any or all of this content must be approved.
+ * Please contact info@indigenous.io for approval or questions.
+ */
+
+/**
+ * Freeform component
+ *
+ * Stores data that represents a freeform HTML formatted region
+ */
+require('../../../models/base.model.js');
+
+var component = $$.m.ModelBase.extend({
+
+    defaults: function() {
+        return {
+
+            /**
+             * The ID of this copmonent
+             */
+            _id: null,
+
+            /**
+             * Some themes may use this anchor to create
+             * navigation directly to thise component
+             */
+            anchor: null,
+
+            /**
+             * The type of component this is
+             */
+            type: "blog",
+
+            /**
+             * The version of the component
+             *
+             */
+            version: 1,
+
+            /**
+             * The Title of the component that shows up on the top
+             * (optional)
+             */
+            title:"Blog",
+
+            /**
+             *
+             *
+             */
+            subtitle:"This is the subtitle.",
+
+            /**
+             *
+             *
+             */
+            text: "This is the main text for the blog.",
+
+            /**
+             *
+             *
+             */
+            bg: {
+                img : {
+                    url : "",
+                    width : null,
+                    height : null,
+                    parallax : false,
+                    blur : false,
+                    overlay: false,
+                    show: false
+                },
+                color : ""
+            },
+
+            tagCloud: {
+                bgcolor: null,
+                txtcolor: null,
+                txthovercolor: null
+            },
+
+            /**
+             * The default sort order of component
+             */
+            postorder: null
+        }
+    },
+
+
+    initialize: function(options) {
+
+    }
+
+
+}, {
+
+    validate: function() {
+
+    }
+});
+
+$$.m.cms = $$.m.cms || {};
+$$.m.cms.modules = $$.m.cms.modules || {};
+$$.m.cms.modules.Blog = component;
+
+module.exports = component;
