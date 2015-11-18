770d77a7990ecbfecee55b3ea5a1b62fd43d4bff
Jack Bishop <jack@indigenous.io>
2015-11-18 11:38:08 -0500

    SSB - add .editorconfig, set inline styles on sections.

--- public/admin/assets/js/ssb-site-builder/ssb-components/ssb-page-section/ssb-page-section.controller.js
+++ public/admin/assets/js/ssb-site-builder/ssb-components/ssb-page-section/ssb-page-section.controller.js
@@ -6,62 +6,70 @@ ssbPageSectionController.$inject = ['$scope', '$attrs', '$filter', 'SimpleSiteBu
 /* @ngInject */
 function ssbPageSectionController($scope, $attrs, $filter, SimpleSiteBuilderService, $stateParams, $transclude) {
 
-    console.info('page-section directive init...')
+  console.info('page-section directive init...')
 
-    var vm = this;
+  var vm = this;
 
-    vm.init = init;
-    vm.componentClass = componentClass;
-    vm.sectionClass = sectionClass;
-    
+  vm.init = init;
+  vm.componentClass = componentClass;
+  vm.sectionClass = sectionClass;
 
-    function sectionClass(layout) {
-        var ngClass = {};
+  //TODO: use https://github.com/martinandert/react-inline to generate inline styles for sections/components
 
-        ngClass['ssb-page-section-layout-' + layout] = true;
+  function sectionClass(section) {
+    var classString = '';
 
-        // {'padding-top': component.spacing.pt + 'px',
-        // 'padding-bottom': component.spacing.pb + 'px',
-        // 'padding-left': component.spacing.pl + 'px',
-        // 'padding-right': component.spacing.pr + 'px',
-        // 'margin-top': component.spacing.mt + 'px',
-        // 'margin-bottom': component.spacing.mb + 'px',
-        // 'margin-left': component.spacing.ml == 'auto' ? component.spacing.ml: component.spacing.ml + 'px',
-        // 'margin-right': component.spacing.mr == 'auto' ? component.spacing.mr : component.spacing.mr + 'px',
-        // 'max-width': component.spacing.mw == '100%' ? component.spacing.mw : component.spacing.mw  + 'px'}
+    classString += 'ssb-page-section-layout-' + layout];
 
-        return ngClass;
+    return classString;
+  }
+
+  function sectionStyle(section) {
+    var styleString = '';
+
+    styleString += 'padding-top: ' + component.spacing.pt + 'px;';
+    styleString += 'padding-bottom: ' + component.spacing.pb + 'px;';
+    styleString += 'padding-left: ' + component.spacing.pl + 'px;';
+    styleString += 'padding-right: ' + component.spacing.pr + 'px;';
+    styleString += 'margin-top: ' + component.spacing.mt + 'px;';
+    styleString += 'margin-bottom: ' + component.spacing.mb + 'px;';
+    styleString += 'margin-left: ' + component.spacing.ml == 'auto' ? component.spacing.ml: component.spacing.ml + 'px;';
+    styleString += 'margin-right: ' + component.spacing.mr == 'auto' ? component.spacing.mr : component.spacing.mr + 'px;';
+    styleString += 'max-width: ' + component.spacing.mw == '100%' ? component.spacing.mw : component.spacing.mw  + 'px;';
+
+    return styleString;
+  }
+
+  function componentClass(index) {
+    // console.log('index: ' + index);
+    // console.log('vm.uiState.activeComponentIndex: ' + vm.uiState.activeComponentIndex);
+    var classObj = {
+        'ssb-active-component': (index === vm.uiState.activeComponentIndex)
+        // 'ssb-active-component': (index === vm.page.sections[vm.uiState.activeComponentIndex].components[vm.uiState.activeComponentIndex])
+    };
+
+    if (vm.section.layout === '1-col') {
+        classObj['col-md-12'] = true;
     }
 
-    function componentClass(index) {
-        // console.log('index: ' + index);
-        // console.log('vm.uiState.activeComponentIndex: ' + vm.uiState.activeComponentIndex);
-        var ngClass = {
-            'ssb-active-component': (index === vm.uiState.activeComponentIndex)
-            // 'ssb-active-component': (index === vm.page.sections[vm.uiState.activeComponentIndex].components[vm.uiState.activeComponentIndex])
-        };
-
-        if (vm.section.layout === '1-col') {
-            ngClass['col-md-12'] = true;
-        }
-
-        if (vm.section.layout === '2-col') {
-            ngClass['col-md-6'] = true;
-        }
-
-        if (vm.section.layout === '3-col') {
-            ngClass['col-md-4'] = true;
-        }
-
-        ngClass['ssb-component-index-' + index] = true;
-
-        return ngClass;
+    if (vm.section.layout === '2-col') {
+        classObj['col-md-6'] = true;
     }
 
-    function init(element) {
-    	vm.element = element;
+    if (vm.section.layout === '3-col') {
+        classObj['col-md-4'] = true;
     }
+
+    classObj['ssb-component-index-' + index] = true;
+
+    return classObj;
+  }
+
+  function init(element) {
+  	vm.element = element;
+  }
 
 }
+
 
 })();
