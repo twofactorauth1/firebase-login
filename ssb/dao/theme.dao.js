f63ca199338de22794606ff615c78f41fa3ebf43
Kyle Miller <millkyl@gmail.com>
2015-11-20 20:49:30 -0600

    Add missing ssb files.  WIP

---
+++ ssb/dao/theme.dao.js
@@ -0,0 +1,25 @@
+/**
+ * COPYRIGHT INDIGENOUS SOFTWARE, INC., LLC 2015
+ *
+ * All use or reproduction of any or all of this content must be approved.
+ * Please contact info@indigenous.io for approval or questions.
+ */
+
+var baseDao = require('./../../dao/base.dao.js');
+var template = require('../model/theme.js');
+
+var dao = {
+
+    options: {
+        name: "ssb.theme.dao",
+        defaultModel: $$.m.ssb.Theme
+    }
+
+};
+
+
+dao = _.extend(dao, baseDao.prototype, dao.options).init();
+
+$$.dao.SSBThemeDao = dao;
+
+module.exports = dao;
