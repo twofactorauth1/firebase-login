ce1c921d2705b70374da6c45fbe53c82e1b07baa
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-11-27 17:32:50 +0530

    Removed cache implementation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -43,7 +43,7 @@ var mainApp = angular
     if (window.history && window.history.pushState) {
       $locationProvider.html5Mode(true).hashPrefix('!');
     }
-    $httpProvider.interceptors.push('noCacheInterceptor');
+    //$httpProvider.interceptors.push('noCacheInterceptor');
     $routeProvider
       .when('/', {
         templateUrl: '../views/cache.html',
@@ -63,17 +63,18 @@ var mainApp = angular
             controller: 'LayoutCtrl as layout'
       });
 
-  }]).factory('noCacheInterceptor', function () {
-      return {
-        request: function (config) {
-            if(config.method=='GET' && config.url.indexOf('/api/') === 0 ){
-                var separator = config.url.indexOf('?') === -1 ? '?' : '&';
-                config.url = config.url+separator+'noCache=' + new Date().getTime();
-            }
-            return config;
-        }
-     };
-  })
+  }])
+  // .factory('noCacheInterceptor', function () {
+  //     return {
+  //       request: function (config) {
+  //           if(config.method=='GET' && config.url.indexOf('/api/') === 0 ){
+  //               var separator = config.url.indexOf('?') === -1 ? '?' : '&';
+  //               config.url = config.url+separator+'noCache=' + new Date().getTime();
+  //           }
+  //           return config;
+  //       }
+  //    };
+  // })
 .run(function ($rootScope, $location, $anchorScroll, $routeParams, $document, $timeout, ipCookie, analyticsService) {
 
     var runningInterval;
