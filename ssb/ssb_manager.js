8600ce9188e7bca41263e6f31c56c19135cb339d
Kyle Miller <millkyl@gmail.com>
2015-11-30 14:12:57 -0600

    Save sections before page

--- ssb/ssb_manager.js
+++ ssb/ssb_manager.js
@@ -232,7 +232,18 @@ module.exports = {
                     }
                 });
             },
-            function updateThePage(existingPage, cb){
+            function updateSections(existingPage, cb) {
+                var sections = page.get('sections');
+                sectionDao.saveSections(sections, function(err, updatedSections){
+                    if(err) {
+                        self.log.error('Error saving sections:', err);
+                        cb(err);
+                    } else {
+                        cb(null, existingPage, updatedSections);
+                    }
+                });
+            },
+            function updateThePage(existingPage, updatedSections, cb){
                 var sections = page.get('sections');
                 page.set('modified', modified);
                 var jsonSections = [];
@@ -246,22 +257,9 @@ module.exports = {
                         self.log.error('Error updating page:', err);
                         cb(err);
                     } else {
-                        cb(null, existingPage, updatedPage);
-                    }
-                });
-            },
-            function updateSections(existingPage, updatedPage, cb){
-                var sections = page.get('sections');
-                //figure out if we need to delete any
-                sectionDao.saveSections(sections, function(err, updatedSections){
-                    if(err) {
-                        self.log.error('Error saving sections:', err);
-                        cb(err);
-                    } else {
                         cb(null, existingPage, updatedPage, updatedSections);
                     }
                 });
-
             },
             function deleteRemovedSections(existingPage, updatedPage, updatedSections, cb){
                 //var updatedSections = updatedPage.get('sections');
