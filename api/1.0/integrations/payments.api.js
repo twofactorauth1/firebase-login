ce7cace9b0eefe6f9a7edd3ccc2a325965fbfa3c
Kyle Miller <millkyl@gmail.com>
2015-12-01 11:50:00 -0600

    #5154 fix sourcing of stripe customer ID

--- api/1.0/integrations/payments.api.js
+++ api/1.0/integrations/payments.api.js
@@ -1852,30 +1852,45 @@ _.extend(api.prototype, baseApi.prototype, {
     getMyUpcomingInvoice: function(req, resp) {
         var self = this;
         self.log.debug('>> getMyUpcomingInvoice');
-        var customerId = self.customerId(req);
-        var subscriptionId = req.body.subscriptionId;
-        stripeDao.getUpcomingInvoice(customerId, subscriptionId, null, function(err, value){
-            self.log.debug('<< getMyUpcomingInvoice');
-            return self.sendResultOrError(resp, err, value, "Error retrieving upcoming invoice.", 404);
+        var accountId = parseInt(self.accountId(req));
+        accountDao.getAccountByID(accountId, function(err, account){
+            if(err) {
+                self.log.error('Error getting account by ID:', err);
+                return self.wrapError(resp, 500, 'Error getting invoice', 'There was an error getting upcoming invoices', '');
+            } else {
+                var customerId = account.get('billing').stripeCustomerId;
+                var subscriptionId = req.body.subscriptionId;
+                stripeDao.getUpcomingInvoice(customerId, subscriptionId, null, function(err, value){
+                    self.log.debug('<< getMyUpcomingInvoice');
+                    return self.sendResultOrError(resp, err, value, "Error retrieving upcoming invoice.", 404);
+                });
+            }
         });
+
     },
 
     getMyInvoices: function(req, resp) {
         var self = this;
         self.log.debug('>> getMyInvoices');
-        var customerId = self.customerId(req);
+        var accountId = parseInt(self.accountId(req));
+        accountDao.getAccountByID(accountId, function(err, account) {
+            if (err) {
+                self.log.error('Error getting account by ID:', err);
+                return self.wrapError(resp, 500, 'Error getting invoice', 'There was an error getting upcoming invoices', '');
+            } else {
+                var customerId = account.get('billing').stripeCustomerId;
+                var dateFilter = req.body.dateFilter;
+                var ending_before = req.body.ending_before;
+                var limit = req.body.limit;
+                var starting_after = req.body.starting_after;
 
-        var dateFilter = req.body.dateFilter;
-        var ending_before = req.body.ending_before;
-        var limit = req.body.limit;
-        var starting_after = req.body.starting_after;
-
-        stripeDao.listInvoices(customerId, dateFilter, ending_before, limit, starting_after, null,
-            function(err, value){
-                self.log.debug('<< getMyInvoices');
-                return self.sendResultOrError(resp, err, value, "Error listing invoices.");
-            });
-
+                stripeDao.listInvoices(customerId, dateFilter, ending_before, limit, starting_after, null,
+                    function(err, value){
+                        self.log.debug('<< getMyInvoices');
+                        return self.sendResultOrError(resp, err, value, "Error listing invoices.");
+                    });
+            }
+        });
     },
 
     getInvoicesForAccount: function(req, resp) {
