13ebb3e0b9edbef388f36415632d0a5b7efff882
Jack Bishop <jack@indigenous.io>
2015-12-02 03:37:17 -0500

    SSB - clean up, theme stylesheets, color picker circles

--- gruntfile.js
+++ gruntfile.js
@@ -101,7 +101,9 @@ module.exports = function(grunt) {
                     // '../indigeweb/public/css/angular-admin.css': [ 'public/less/angular-admin.less' ]
                     '../indigeweb/public/admin/assets/css/frontend-admin.css': [ 'public/less/frontend-admin.less' ],
                     '../indigeweb/public/css/styles.css': [ 'public/less/frontend.less' ],
-                    '../indigeweb/public/admin/assets/css/styles.css': [ 'public/less/styles.less' ]
+                    '../indigeweb/public/admin/assets/css/styles.css': [ 'public/less/styles.less' ],
+                    // '../indigeweb/public/admin/assets/css/ssb-site-builder/styles.css': [ 'public/less/ssb-site-builder/frontend/styles.less' ],
+                    '../indigeweb/public/css/ssb-site-builder/ssb-themes/ssb-theme-young-soul.css': [ 'public/less/ssb-site-builder/ssb-frontend/ssb-themes/ssb-theme-young-soul.less' ]
                 }
             }
         },
