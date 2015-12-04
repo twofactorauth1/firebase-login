5edb8ac89aae9fa2926118b1272b05750c2943a8
Kyle Miller <millkyl@gmail.com>
2015-12-03 23:39:10 -0600

    #5185 separate workstreams into completed vs incomplete.

--- public/admin/assets/js/dashboard/dashboard.controller.js
+++ public/admin/assets/js/dashboard/dashboard.controller.js
@@ -8,14 +8,25 @@
     var vm = this;
 
     vm.state = {};
+    vm.state.account = $scope.account;
 
     $scope.$watch(function() { return DashboardService.workstreams }, function(workstreams) {
         vm.state.workstreams = workstreams;
         var analyticsWidgets = [];
+        var incompleteWorkstreams = [];
+        var completeWorkstreams = [];
         _.each(workstreams, function(workstream){
             analyticsWidgets = analyticsWidgets.concat(workstream.analyticWidgets);
+            if(workstream.completed===true) {
+                completeWorkstreams.push(workstream);
+            } else {
+                incompleteWorkstreams.push(workstream);
+            }
         });
         vm.state.analyticsWidgets = analyticsWidgets;
+        vm.state.completeWorkstreams = completeWorkstreams;
+        vm.state.incompleteWorksreams = incompleteWorkstreams;
+
     }, true);
 
     (function init() {
