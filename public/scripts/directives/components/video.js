2b531be1dd5a584e69913c27816bca9c4d8c77ce
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2015-12-09 19:13:39 +0530

    #4731

--- public/scripts/directives/components/video.js
+++ public/scripts/directives/components/video.js
@@ -8,10 +8,11 @@ app.directive('videoComponent',['$sce', function ($sce) {
       scope.flvVideoUrl = function (iframeUrl, url, autoPlay) {
         var parsedUrl = urlParser.parse(url);
         var retUrl = "";
-        if (parsedUrl)
+        if (parsedUrl) {
           retUrl = iframeUrl + parsedUrl.id + '?showinfo=0&rel=0&hd=1';
           if(autoPlay)
             retUrl = retUrl + '&autoplay=1';
+        }
         else
           retUrl = iframeUrl
         return $sce.trustAsResourceUrl(retUrl);
