b528463c9d264f125329bc14752a40fb235ef365
Jack Bishop <jack@indigenous.io>
2016-01-13 14:49:14 -0500

    SSB rendering

---
+++ public/scripts/services/embeddedSiteDataService.js
@@ -0,0 +1,127 @@
+/*
+ * Get site data from <script> and return to angular app
+ *
+ * Data format:
+ *
+ * {
+ *      ...
+ *      pages: {
+ *          'page-handle' : {}
+ *          ...
+ *      }
+ *      ...
+ * }
+ *
+ */
+
+'use strict';
+/*global mainApp*/
+mainApp.factory('embeddedSiteDataService', ['$http', '$location', '$cacheFactory', function ($http, $location) {
+
+    var service = {};
+
+    service.urlPathFallbacks = urlPathFallbacks;
+    service.getSiteData = getSiteData;
+    service.getPageData = getPageData;
+
+
+    /*
+     * Set equivalent paths and fallbacks
+     *
+     */
+    function urlPathFallbacks()  {
+        var path = $location.$$path.replace('/page/', '');
+
+        if (path === "/" || path === "") {
+            path = "index";
+        }
+
+        if (path === "/signup") {
+            path = "signup";
+        }
+
+        if (path.indexOf("blog/") > -1) {
+            path = 'single-post';
+        }
+
+        if (path.indexOf("post/") > -1) {
+            path = 'single-post';
+        }
+
+        if (path === 'blog' || path === '/blog' || path.indexOf("tag/") > -1 || path.indexOf("category/") > -1 || path.indexOf("author/") > -1) {
+            path = 'blog';
+        }
+
+        if (path.indexOf('/') === 0) {
+            path = path.replace('/', '');
+        }
+
+        service.path = path;
+    }
+
+
+    /*
+     * Get data set from server on global window object
+     *
+     */
+    function getSiteData() {
+
+        if (window.indigenous && window.indigenous.precache && window.indigenous.precache.siteData) {
+
+            service.siteData = window.indigenous.precache.siteData;
+
+        }
+    }
+
+
+    /*
+     * Get page data from siteData
+     * - Keeps legacy interface from cacheCtrl usage
+     *
+     */
+    function getPageData(websiteId, callback) {
+
+        var cb = callback;
+
+        if (typeof service.siteData.pages[service.path] !== undefined) {
+
+            callback(null, service.siteData.pages[service.path]);
+
+        } else {
+
+            $http.get(apiURL + websiteId + '/page/coming-soon', {
+                cache: true
+            }).success(function (page) {
+
+                if (page !== null) {
+
+                    service.siteData.pages[page.handle] = page;
+                    callback(null, page);
+
+                } else {
+
+                    callback("page not found", null);
+
+                }
+
+            }).error(function (err) {
+
+                callback(err, null);
+
+            });
+
+        }
+
+    }
+
+
+    (function init() {
+
+        service.getSiteData();
+        service.urlPathFallbacks();
+
+    })();
+
+    return service;
+
+}]);
