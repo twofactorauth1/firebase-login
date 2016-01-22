f5e2fa4ee3ab001e5fd63b29a4c0a061f501afb4
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2016-01-22 20:54:54 +0530

    #5463 Set as home page

--- public/admin/assets/js/ssb-site-builder/ssb-controls/ssb-page-settings-modal/ssb-page-settings-modal.controller.js
+++ public/admin/assets/js/ssb-site-builder/ssb-controls/ssb-page-settings-modal/ssb-page-settings-modal.controller.js
@@ -9,10 +9,48 @@ app.controller('SiteBuilderPageSettingsModalController', ['$timeout', 'parentVm'
 	vm.pageId = pageId;
 	vm.saveSettings = saveSettings;
 	vm.deletePage = deletePage;
+  vm.setAsHomePage = setAsHomePage;
+  vm.duplicatePage = duplicatePage;
+  function duplicatePage(){
+    // to do
+  }
   function saveSettings() {
   	SimpleSiteBuilderService.savePage(vm.page, true).then(function(page) {
 			toaster.pop('success', 'Setting Saved', 'The page settings saved successfully.');
 		})
+  }
+  function setAsHomePage(status) {    
+    if(status){
+      if(vm.parentVm.state.pages["index"]){
+        console.log("Homepage already exists");
+        SweetAlert.swal({
+            title: "Are you sure?",
+            text: "CAUTION: Home page already exists. Do you want to set this page as 'Homepage'?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes — I'll set this page as 'Homepage'",
+            cancelButtonText: "No — I will use existing 'Homepage' for now.",
+            closeOnConfirm: true,
+            closeOnCancel: true
+          }, function (isConfirm) {
+          if (isConfirm) {
+            //to do
+              //vm.page.handle = "index";
+          } else {
+            angular.element('.modal.in').show();
+            vm.homePage = false;
+          }
+        })
+      }
+      else{
+        vm.page.handle = 'index';
+      }
+    }
+    else{
+      vm.page = angular.copy(vm.originalPage);
+    }
+    
   }
   function deletePage(page) {
     angular.element('.modal.in').hide();
@@ -52,8 +90,9 @@ app.controller('SiteBuilderPageSettingsModalController', ['$timeout', 'parentVm'
 
   SimpleSiteBuilderService.getPage(vm.pageId, true).then(function(page) {
     vm.page = page.data;
+    vm.originalPage = angular.copy(vm.page);
     // Special case if selected page is current page
-    if(vm.parentVm.state.page && vm.parentVm.state.page._id   === vm.pageId){
+    if(vm.parentVm.state.page && vm.parentVm.state.page._id === vm.pageId){
       vm.page.title = vm.parentVm.state.page.title;
       vm.page.handle = vm.parentVm.state.page.handle;
     }
