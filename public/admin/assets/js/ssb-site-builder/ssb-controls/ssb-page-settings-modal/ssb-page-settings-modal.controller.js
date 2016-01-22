670b3e441f3ab25c35b3412bfa11f46f051cc8c3
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2016-01-22 17:52:41 +0530

    #5559

--- public/admin/assets/js/ssb-site-builder/ssb-controls/ssb-page-settings-modal/ssb-page-settings-modal.controller.js
+++ public/admin/assets/js/ssb-site-builder/ssb-controls/ssb-page-settings-modal/ssb-page-settings-modal.controller.js
@@ -9,48 +9,54 @@ app.controller('SiteBuilderPageSettingsModalController', ['$timeout', 'parentVm'
 	vm.pageId = pageId;
 	vm.saveSettings = saveSettings;
 	vm.deletePage = deletePage;
-	function saveSettings() {
-    	SimpleSiteBuilderService.savePage(vm.page).then(function(page) {
-  			toaster.pop('success', 'Setting Saved', 'The page settings saved successfully.');
-  		})
+  function saveSettings() {
+  	SimpleSiteBuilderService.savePage(vm.page, true).then(function(page) {
+			toaster.pop('success', 'Setting Saved', 'The page settings saved successfully.');
+		})
+  }
+  function deletePage(page) {
+    angular.element('.modal.in').hide();
+    var _deleteText = "Do you want to delete this page";
+    if(page.handle === 'index')
+    {
+      var _deleteText = "This is home page of the website. Do you want to delete this page";
     }
-    function deletePage(page) {
-      angular.element('.modal.in').hide();
-      var _deleteText = "Do you want to delete this page";
-      if(page.handle === 'index')
-      {
-        var _deleteText = "This is home page of the website. Do you want to delete this page";
+    SweetAlert.swal({
+      title: "Are you sure?",
+      text: _deleteText,
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, delete page!",
+      cancelButtonText: "No, do not delete page!",
+      closeOnConfirm: false,
+      closeOnCancel: true
+    }, function (isConfirm) {
+      if (isConfirm) {
+          SimpleSiteBuilderService.deletePage(page).then(function(response){
+              console.log('page deleted');
+              SweetAlert.swal("Saved!", "Page is deleted.", "success");
+              angular.element('.modal.in').show();
+              vm.parentVm.closeModal();
+              if(vm.parentVm.uiState.selectedPage._id === page._id){                	
+              	$timeout(function () {
+            			$location.path('/website/site-builder/pages/');
+          		}, 0);
+              }
+          })  
+      } else {
+        angular.element('.modal.in').show();
       }
-      SweetAlert.swal({
-        title: "Are you sure?",
-        text: _deleteText,
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Yes, delete page!",
-        cancelButtonText: "No, do not delete page!",
-        closeOnConfirm: false,
-        closeOnCancel: true
-      }, function (isConfirm) {
-        if (isConfirm) {
-            SimpleSiteBuilderService.deletePage(page).then(function(response){
-                console.log('page deleted');
-                SweetAlert.swal("Saved!", "Page is deleted.", "success");
-                angular.element('.modal.in').show();
-                vm.parentVm.closeModal();
-                if(vm.parentVm.uiState.selectedPage._id === page._id){                	
-                	$timeout(function () {
-              			$location.path('/website/site-builder/pages/');
-            		}, 0);
-                }
-            })  
-        } else {
-          angular.element('.modal.in').show();
-        }
-      });
-    };
-	SimpleSiteBuilderService.getPage(vm.pageId).then(function(page) {
-  		vm.page = page.data;
-  	})
+    });
+  };
+
+  SimpleSiteBuilderService.getPage(vm.pageId, true).then(function(page) {
+    vm.page = page.data;
+    // Special case if selected page is current page
+    if(vm.parentVm.state.page && vm.parentVm.state.page._id   === vm.pageId){
+      vm.page.title = vm.parentVm.state.page.title;
+      vm.page.handle = vm.parentVm.state.page.handle;
+    }
+  })
 
 }]);
