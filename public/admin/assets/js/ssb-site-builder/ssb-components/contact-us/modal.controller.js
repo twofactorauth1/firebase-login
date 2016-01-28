7548afde537e93c408069ebd20ffeb6f300d714c
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2016-01-28 19:51:50 +0530

    SB: contact us settings #5594

--- public/admin/assets/js/ssb-site-builder/ssb-components/contact-us/modal.controller.js
+++ public/admin/assets/js/ssb-site-builder/ssb-components/contact-us/modal.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 /*global app*/
-app.controller('SiteBuilderContactUsModalController', ['$timeout', 'parentVm', 'toaster', 'SimpleSiteBuilderService', 'GeocodeService', function ($timeout, parentVm, toaster, SimpleSiteBuilderService, GeocodeService) {
+app.controller('SiteBuilderContactUsModalController', ['$scope', '$timeout', 'parentVm', 'toaster', 'SimpleSiteBuilderService', 'GeocodeService', 'hoursConstant', function ($scope, $timeout, parentVm, toaster, SimpleSiteBuilderService, GeocodeService, hoursConstant) {
 
     var vm = this;
 
@@ -12,23 +12,14 @@ app.controller('SiteBuilderContactUsModalController', ['$timeout', 'parentVm', '
     vm.place.address = null;
     vm.errorMapData = false;
     vm.showAddress = false;
+    vm.hours = hoursConstant;
 
     vm.saveComponent = saveComponent;
     vm.updateContactUsAddress = updateContactUsAddress;
     vm.setLatLon = setLatLon;
     vm.validateGeoAddress = validateGeoAddress;
     vm.validateHours = validateHours;
-
-    function saveComponent(is_address) {
-
-        if(is_address){
-            vm.uiState.componentControl.refreshMap();
-            vm.place.address = GeocodeService.stringifyAddress(vm.component.location);
-        } else {
-            vm.uiState.componentControl.refreshHours();
-        }
-
-    }
+   
 
     function updateContactUsAddress() {
         if (!angular.equals(vm.originalContactMap, vm.component.location)) {
@@ -44,16 +35,14 @@ app.controller('SiteBuilderContactUsModalController', ['$timeout', 'parentVm', '
     }
 
     function validateGeoAddress(fn) {
-
         GeocodeService.validateAddress(vm.component.location, vm.locationAddress, function (data, results) {
-
             if (data && results.length === 1) {
                 $timeout(function () {
                     $scope.$apply(function () {
                         vm.setLatLon(results[0].geometry.location.lat(), results[0].geometry.location.lng());
                         vm.errorMapData = false;
                         angular.copy(vm.component.location, vm.originalContactMap);
-                        vm.contactMap.refreshMap();
+                        vm.parentVm.uiState.componentControl.refreshMap();                        
                     });
                 }, 0);
             } else {
@@ -64,14 +53,29 @@ app.controller('SiteBuilderContactUsModalController', ['$timeout', 'parentVm', '
                     });
                 }, 0);
             }
-
             if (fn) {
                 fn();
             }
-
         });
-
     };
+
+    function saveComponent(is_address) {
+    if(is_address){
+      vm.parentVm.uiState.componentControl.refreshMap();
+      vm.place.address = GeocodeService.stringifyAddress(vm.component.location);
+    }
+    else
+        vm.parentVm.uiState.componentControl.refreshHours();        
+    };
+
+    vm.contactHoursInvalid = false;
+    vm.contactHours = [];
+    var i = 0;
+    for (i; i <= 6; i++) {
+      vm.contactHours.push({
+        "valid": true
+      });
+    }
 
     function validateHours(hours, index) {
         vm.contactHours[index].valid = true;
@@ -142,7 +146,66 @@ app.controller('SiteBuilderContactUsModalController', ['$timeout', 'parentVm', '
         } else {
             vm.contactHoursInvalid = false;
         }
-    }
+    };
+
+    var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        postal_code: 'short_name',
+        country: 'short_name'
+    };
+
+    var fillInAddress = function (place) {
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        setDefaultAddress();
+        var i = 0;
+        var addressType, val;
+        for (i; i < place.address_components.length; i++) {
+          addressType = place.address_components[i].types[0];
+          if (componentForm[addressType]) {
+            val = place.address_components[i][componentForm[addressType]];
+            if (addressType === 'street_number') {
+              vm.component.location.address = val;
+            } else if (addressType === 'route') {
+              vm.component.location.address2 = val;
+            } else if (addressType === 'locality') {
+              vm.component.location.city = val;
+            } else if (addressType === 'administrative_area_level_1') {
+              vm.component.location.state = val;
+            } else if (addressType === 'postal_code') {
+              vm.component.location.zip = val;
+            } else if (addressType === 'country') {
+              vm.component.location.country = val;
+            }
+          }
+        }
+        vm.component.location.lat = place.geometry.location.lat();
+        vm.component.location.lon = place.geometry.location.lng();
+    };
+
+    var setDefaultAddress = function () {
+        vm.component.location.address = "";
+        vm.component.location.address2 = "";
+        vm.component.location.city = "";
+        vm.component.location.state = "";
+        vm.component.location.zip = "";
+        vm.component.location.country = "";
+    };
+    
+    vm.place.address = GeocodeService.stringifyAddress(vm.component.location);
+    $scope.$watch('vm.place.address', function (newValue) {
+        if (newValue) {
+          if (angular.isObject(newValue)) {
+            fillInAddress(newValue);
+            vm.locationAddress = newValue;
+            vm.setLatLon();
+            vm.validateGeoAddress();
+          }
+        }
+    });
 
     (function init() {
 
