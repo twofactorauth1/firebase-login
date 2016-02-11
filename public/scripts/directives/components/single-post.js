20293a1b1973a5cf55cb9e559ebef871dd39d866
Chris Ricci <chris@indigenous.io>
2016-02-11 17:15:38 -0600

    Scrub blog article title of nested HTML. #5391

--- public/scripts/directives/components/single-post.js
+++ public/scripts/directives/components/single-post.js
@@ -22,7 +22,7 @@ app.directive('singlePostComponent', ['$window', '$location', 'accountService', 
           PostService.getSinglePost(_handle, data.website.websiteId, function (post) {
             console.log('post data ', post);
             scope.blog.post = post;
-            $window.document.title = $window.document.title + ": " + post.post_title;
+            $window.document.title = $window.document.title + ": " + post.post_title.replace(/<\/?[^>]+(>|$)/g, "");
           });
         });
 
