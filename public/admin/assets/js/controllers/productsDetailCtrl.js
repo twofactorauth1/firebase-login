abfb9f62ec76d4893d19f7a0f37b65b89ce13728
Jaideep Das <jaideep.jdof@gmail.com>
2016-03-20 20:16:37 +0530

    #5567 - added new resolve for SSB components

--- public/admin/assets/js/controllers/productsDetailCtrl.js
+++ public/admin/assets/js/controllers/productsDetailCtrl.js
@@ -51,7 +51,7 @@
             return $scope.insertMedia;
           },
           isSingleSelect: function () {
-              return true;
+              return false;
           }
         }
       });
