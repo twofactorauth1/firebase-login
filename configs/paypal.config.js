81115c82d64d5650390551061bb19fa218327bed
Kyle Miller <millkyl@gmail.com>
2016-03-23 11:25:06 -0500

    Update paypal config with new account info

--- configs/paypal.config.js
+++ configs/paypal.config.js
@@ -7,9 +7,9 @@
 
 var paypalClientID = process.env.PAYPAL_CLIENT_ID || 'Adai01SoHe9BcFr9GkfTNzys2bqUdqdpSphHVzouKPKQxxDRe1eBRKGaKNNETr5tYNf02OfG-e-YzOnO';
 var paypalClientSecret = process.env.PAYPAL_CLIENT_SECRET || 'EMLp1mnLTtXX6PSgZBqnS2U6Cp4-KSUPvUxg1lpDgKEv4fHX3DUo5Ux980Qhu90Aat4fwQSZMlOZJcV5';
-var paypalAPIUsername = process.env.PAYPAL_API_USERNAME || 'kyle-facilitator_api1.indigenous.io';
-var paypalAPIPassword = process.env.PAYPAL_API_PASSWORD || '9UQNLK8V9ZNHFSBM';
-var paypalAPISignature = process.env.PAYPAL_API_SIGNATURE || 'AFcWxV21C7fd0v3bYYYRCpSSRl31AUrCEslod6r62qh4l6zrsCME.JV-';
+var paypalAPIUsername = process.env.PAYPAL_API_USERNAME || 'paypal-facilitator_api1.indigenous.io';
+var paypalAPIPassword = process.env.PAYPAL_API_PASSWORD || 'N3TBZGQMYEW66MTH';
+var paypalAPISignature = process.env.PAYPAL_API_SIGNATURE || 'AFcWxV21C7fd0v3bYYYRCpSSRl31AHk7UU9Y3mtj6J5BdfSV61PJ2Mu';
 var paypalCheckoutURL = process.env.PAYPAL_CHECKOUT_URL || 'https://www.sandbox.paypal.com/webapps/adaptivepayment/flow/pay';
 var paypalIsSandbox = process.env.PAYPAL_SANDBOX || true;
 var paypalApplicationID = process.env.PAYPAL_APP_ID || 'APP-80W284485P519543T';
@@ -20,9 +20,9 @@ var paypalApplicationID = process.env.PAYPAL_APP_ID || 'APP-80W284485P519543T';
 //AFcWxV21C7fd0v3bYYYRCpSSRl31ARIjICnFMYduIfRqDGmPXFSW1Wus
 //App ID: APP-4WL47068WP184600P
 
-var realUserName = 'kyle_api1.indigenous.io';
-var realPassword = 'L5VSWK2GJ8ZXFPDS';
-var realSig = 'AFcWxV21C7fd0v3bYYYRCpSSRl31ARIjICnFMYduIfRqDGmPXFSW1Wus';
+var realUserName = 'paypal_api1.indigenous.io';
+var realPassword = '7ZQUHXA6AHBDZ4E5';
+var realSig = 'A3B-XY1YH2mVmpxut0JgN3TxM.FrAEkGvbCO9E4HhaLzYxnG-h-3Z84L';
 var realUrl = 'https://www.paypal.com/webapps/adaptivepayment/flow/pay';
 var realApplicationID = 'APP-4WL47068WP184600P';
 
