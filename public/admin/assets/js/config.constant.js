31fa983c1e307bf4562b0b9b8fe01de474667428
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2016-03-30 17:59:37 +0530

    SB: default to media modal for Froala image insert #6008

--- public/admin/assets/js/config.constant.js
+++ public/admin/assets/js/config.constant.js
@@ -802,7 +802,7 @@ app.constant('JS_REQUIRES', {
       "../js/libs/froala-wysiwyg-editor/js/plugins/entities.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/font_family.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/font_size.min.js",
-      "../js/libs/froala-wysiwyg-editor/js/plugins/image.min.js",
+      "../js/libs_misc/froala-wysiwyg-editor/js/plugins/image-manager.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/inline_style.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/line_breaker.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/link.min.js",
