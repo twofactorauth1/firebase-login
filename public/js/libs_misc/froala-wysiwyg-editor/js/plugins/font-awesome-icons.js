75e7ade5da859e3c756ab6463fc87ea9427f6bb7
Jaideep Das <jaideep.jdof@gmail.com>
2016-04-04 20:48:09 +0530

    #6043 - changed listing to show fa icons

--- public/js/libs_misc/froala-wysiwyg-editor/js/plugins/font-awesome-icons.js
+++ public/js/libs_misc/froala-wysiwyg-editor/js/plugins/font-awesome-icons.js
@@ -14,173 +14,11 @@
     }), a.extend(a.FroalaEditor.DEFAULTS, {
         fontAwesomeIconsStep: 8,
         fontAwesomeIconsSet: [{
-            code: "1f600",
-            desc: "Grinning face"
+            code: "adjust",
+            desc: "Adjust"
         }, {
-            code: "1f601",
-            desc: "Grinning face with smiling eyes"
-        }, {
-            code: "1f602",
-            desc: "Face with tears of joy"
-        }, {
-            code: "1f603",
-            desc: "Smiling face with open mouth"
-        }, {
-            code: "1f604",
-            desc: "Smiling face with open mouth and smiling eyes"
-        }, {
-            code: "1f605",
-            desc: "Smiling face with open mouth and cold sweat"
-        }, {
-            code: "1f606",
-            desc: "Smiling face with open mouth and tightly-closed eyes"
-        }, {
-            code: "1f607",
-            desc: "Smiling face with halo"
-        }, {
-            code: "1f608",
-            desc: "Smiling face with horns"
-        }, {
-            code: "1f609",
-            desc: "Winking face"
-        }, {
-            code: "1f60a",
-            desc: "Smiling face with smiling eyes"
-        }, {
-            code: "1f60b",
-            desc: "Face savoring delicious food"
-        }, {
-            code: "1f60c",
-            desc: "Relieved face"
-        }, {
-            code: "1f60d",
-            desc: "Smiling face with heart-shaped eyes"
-        }, {
-            code: "1f60e",
-            desc: "Smiling face with sunglasses"
-        }, {
-            code: "1f60f",
-            desc: "Smirking face"
-        }, {
-            code: "1f610",
-            desc: "Neutral face"
-        }, {
-            code: "1f611",
-            desc: "Expressionless face"
-        }, {
-            code: "1f612",
-            desc: "Unamused face"
-        }, {
-            code: "1f613",
-            desc: "Face with cold sweat"
-        }, {
-            code: "1f614",
-            desc: "Pensive face"
-        }, {
-            code: "1f615",
-            desc: "Confused face"
-        }, {
-            code: "1f616",
-            desc: "Confounded face"
-        }, {
-            code: "1f617",
-            desc: "Kissing face"
-        }, {
-            code: "1f618",
-            desc: "Face throwing a kiss"
-        }, {
-            code: "1f619",
-            desc: "Kissing face with smiling eyes"
-        }, {
-            code: "1f61a",
-            desc: "Kissing face with closed eyes"
-        }, {
-            code: "1f61b",
-            desc: "Face with stuck out tongue"
-        }, {
-            code: "1f61c",
-            desc: "Face with stuck out tongue and winking eye"
-        }, {
-            code: "1f61d",
-            desc: "Face with stuck out tongue and tightly-closed eyes"
-        }, {
-            code: "1f61e",
-            desc: "Disappointed face"
-        }, {
-            code: "1f61f",
-            desc: "Worried face"
-        }, {
-            code: "1f620",
-            desc: "Angry face"
-        }, {
-            code: "1f621",
-            desc: "Pouting face"
-        }, {
-            code: "1f622",
-            desc: "Crying face"
-        }, {
-            code: "1f623",
-            desc: "Persevering face"
-        }, {
-            code: "1f624",
-            desc: "Face with look of triumph"
-        }, {
-            code: "1f625",
-            desc: "Disappointed but relieved face"
-        }, {
-            code: "1f626",
-            desc: "Frowning face with open mouth"
-        }, {
-            code: "1f627",
-            desc: "Anguished face"
-        }, {
-            code: "1f628",
-            desc: "Fearful face"
-        }, {
-            code: "1f629",
-            desc: "Weary face"
-        }, {
-            code: "1f62a",
-            desc: "Sleepy face"
-        }, {
-            code: "1f62b",
-            desc: "Tired face"
-        }, {
-            code: "1f62c",
-            desc: "Grimacing face"
-        }, {
-            code: "1f62d",
-            desc: "Loudly crying face"
-        }, {
-            code: "1f62e",
-            desc: "Face with open mouth"
-        }, {
-            code: "1f62f",
-            desc: "Hushed face"
-        }, {
-            code: "1f630",
-            desc: "Face with open mouth and cold sweat"
-        }, {
-            code: "1f631",
-            desc: "Face screaming in fear"
-        }, {
-            code: "1f632",
-            desc: "Astonished face"
-        }, {
-            code: "1f633",
-            desc: "Flushed face"
-        }, {
-            code: "1f634",
-            desc: "Sleeping face"
-        }, {
-            code: "1f635",
-            desc: "Dizzy face"
-        }, {
-            code: "1f636",
-            desc: "Face without mouth"
-        }, {
-            code: "1f637",
-            desc: "Face with medical mask"
+            code: "anchor",
+            desc: "Anchor"
         }],
         fontAwesomeIconsButtons: ["fontAwesomeIconsBack", "|"],
         fontAwesomeIconsUseImage: !0
@@ -212,8 +50,8 @@
         }
 
         function f() {
-            for (var a = '<div style="text-align: center">', c = 0; c < b.opts.fontAwesomeIconsSet.length; c++) 0 !== c && c % b.opts.fontAwesomeIconsStep === 0 && (a += "<br>"), a += '<span class="fr-command fr-fontAwesomeIcon fr-emoticon" data-cmd="insertFontAwesomeIcon" title="' + b.language.translate(b.opts.fontAwesomeIconsSet[c].desc) + '" data-param1="' + b.opts.fontAwesomeIconsSet[c].code + '">' + (b.opts.fontAwesomeIconsUseImage ? '<img src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.0.1/assets/svg/' + b.opts.fontAwesomeIconsSet[c].code + '.svg"/>' : "&#x" + b.opts.fontAwesomeIconsSet[c].code + ";") + "</span>";
-            return b.opts.fontAwesomeIconsUseImage && (a += '<p style="font-size: 12px; text-align: center; padding: 0 5px;">Emoji free by <a href="http://emojione.com/" target="_blank" rel="nofollow">Emoji One</a></p>'), a += "</div>"
+            for (var a = '<div style="text-align: center">', c = 0; c < b.opts.fontAwesomeIconsSet.length; c++) 0 !== c && c % b.opts.fontAwesomeIconsStep === 0 && (a += "<br>"), a += '<span class="fr-command fr-fontAwesomeIcon fr-emoticon" data-cmd="insertFontAwesomeIcon" title="' + b.language.translate(b.opts.fontAwesomeIconsSet[c].desc) + '" data-param1="' + b.opts.fontAwesomeIconsSet[c].code + '">' + "<span class='fa fa-" + b.opts.fontAwesomeIconsSet[c].code + "'></span>" + "</span>";
+            return b.opts.fontAwesomeIconsUseImage && (a), a += "</div>"
         }
 
         function g(c, d) {
