d1eb8e290f9672b663de32a4cd216e9c818d7d9a
Jaideep Das <jaideep.jdof@gmail.com>
2016-04-20 15:30:55 +0530

    #6155 - refactored code

--- public/admin/assets/js/ssb-site-builder/ssb-controls/ssb-action-buttons/ssb-action-buttons.component.js
+++ public/admin/assets/js/ssb-site-builder/ssb-controls/ssb-action-buttons/ssb-action-buttons.component.js
@@ -2,7 +2,7 @@
 
 app.directive('ssbActionButtons', ssbActionButtons);
 
-function ssbActionButtons(SimpleSiteBuilderService, $timeout) {
+function ssbActionButtons() {
 
     return {
         restrict: 'E',
@@ -18,36 +18,6 @@ function ssbActionButtons(SimpleSiteBuilderService, $timeout) {
         controllerAs: 'vm',
         bindToController: true,
         link: function(scope, element, attrs, ctrl) {
-            scope.pageVersions = [];
-            scope.hideHistoryTT = false;
-            scope.spinTimeoutPromise = null;
-
-            scope.$watch(ctrl.state, function(newValue, oldValue) {
-                if (angular.isDefined(ctrl.state.page.published)) {
-                    if (ctrl.state.page.published.date < ctrl.state.page.modified.date) {
-                        if (scope.spinTimeoutPromise) {
-                            $timeout.cancel(scope.spinTimeoutPromise);
-                        }
-                        scope.spinTimeoutPromise = $timeout(function () {
-                            $('.fa.fa-globe.fa-spin').removeClass('fa-spin');
-                        }, 10000);
-                    }
-                }
-            }, true);
-
-            scope.historyDropdownFn = function (open) {
-                scope.hideHistoryTT = open;
-                if (open) {
-                    $('.tooltip').hide();
-                    SimpleSiteBuilderService.getPageVersions(scope.vm.state.page._id, function (data) {
-                        scope.pageVersions = data;
-                    });
-                } else {
-                    scope.pageVersions = [];
-                    $('.tooltip').show();
-                }
-            };
-
             ctrl.init(element);
         }
     };
