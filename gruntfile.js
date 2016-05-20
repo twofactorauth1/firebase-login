d5dc9c3f28a1d9e72ec22fad38e4bd0f91031fbd
Jack Bishop <jack@indigenous.io>
2016-05-20 13:43:53 -0400

    #6419 - Dev: cssnano process takes 60+ seconds, too slow

--- gruntfile.js
+++ gruntfile.js
@@ -468,7 +468,7 @@ module.exports = function(grunt) {
                 processors: [
                     // require('pixrem')(), // add fallbacks for rem units
                     require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-                    require('cssnano')() // minify the result
+                    require('cssnano')({discardDuplicates: false}) // minify the result
                 ]
             },
             dist: {
