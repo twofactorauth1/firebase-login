d48dff5091b98cf1a1eb82e99808c18007539444
Jaideep Das <jaideep.jdof@gmail.com>
2016-05-30 17:14:24 +0530

    #6201 - added peer contact functions for phone and address

--- models/contact.js
+++ models/contact.js
@@ -218,6 +218,38 @@ var contact = $$.m.ModelBase.extend({
         }
     },
 
+    getPrimaryPhone: function() {
+      var details = this.get('details');
+      if (details == null || details.length == 0) {
+        return '';
+      }
+
+      var phones = _.flatten(_.pluck(details, 'phones'));
+      if (!phones.length) {
+        return ''
+      }
+
+      var phone = _.findWhere(phones, {default: true}) ? _.findWhere(phones, {default: true}) : phones[0];
+      phone = phone.extension ? phone.extension + '-' + phone.number : phone.number;
+      return phone;
+    },
+
+    getPrimaryAddress: function() {
+      var details = this.get('details');
+      if (details == null || details.length == 0) {
+        return '';
+      }
+
+      var addresses = _.flatten(_.pluck(details, 'addresses'));
+      if (!addresses.length) {
+        return ''
+      }
+
+      var address = _.findWhere(addresses, {defaultBilling: true}) ? _.findWhere(addresses, {defaultBilling: true}) : addresses[0];
+
+      return _.values(_.pick(address, 'address', 'address2', 'city', 'state','zip', 'country', 'countryCode')).join(',\n');
+    },
+
 
   getPhones: function() {
     var details = this.get("details"),
