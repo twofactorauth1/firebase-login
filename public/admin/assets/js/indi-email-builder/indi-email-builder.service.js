c9e06ae4e965ac9a32474dfdd43a9940d4969af7
Jaideep Das <jaideep.jdof@gmail.com>
2016-06-23 01:21:31 +0530

    #6623 - added email fetch call.

--- public/admin/assets/js/indi-email-builder/indi-email-builder.service.js
+++ public/admin/assets/js/indi-email-builder/indi-email-builder.service.js
@@ -9,15 +9,15 @@
     /* @ngInject */
     function EmailBuilderService($http, AccountService) {
         var emailService = {};
+        var baseCmsAPIUrlv1 = '/api/1.0/cms';
         var baseWebsiteAPIUrlv1 = '/api/1.0/cms/website';
 
         emailService.loading = {value: 0};
         emailService.getEmails = getEmails;
+        emailService.getEmail = getEmail;
 
         /**
-         * Get latest website object for this account
-         * @param {string} id - website _id
-         * @param {boolean} isLoading - if loading this data is in progress
+         * Get list of all emails for the account
          */
         function getEmails() {
 
@@ -30,6 +30,23 @@
             }
 
             return emailRequest($http.get([baseWebsiteAPIUrlv1, emailService.websiteId, 'emails'].join('/')).success(success).error(error));
+        }
+
+        /**
+         * Get get email by ID
+         * @param {string} id - email _id
+         */
+        function getEmail(id) {
+
+            function success(data) {
+                emailService.emails = data;
+            }
+
+            function error(error) {
+                console.error('EmailBuilderService getEmail error: ', JSON.stringify(error));
+            }
+
+            return emailRequest($http.get([baseCmsAPIUrlv1, 'email', id].join('/')).success(success).error(error));
         }
 
         /**
