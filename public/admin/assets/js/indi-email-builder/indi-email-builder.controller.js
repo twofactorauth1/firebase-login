3b684a31c0d57594b56c816ab737e14a12839a15
Jaideep Das <jaideep.jdof@gmail.com>
2016-06-24 20:01:51 +0530

    #6623 - added basic HTML structure from ssb-site-builder.component.js

--- public/admin/assets/js/indi-email-builder/indi-email-builder.controller.js
+++ public/admin/assets/js/indi-email-builder/indi-email-builder.controller.js
@@ -2,9 +2,9 @@
 
     app.controller('EmailBuilderController', indiEmailBuilderController);
 
-    indiEmailBuilderController.$inject = ['$scope', '$rootScope', 'EmailBuilderService', '$stateParams', '$state', 'toaster'];
+    indiEmailBuilderController.$inject = ['$scope', 'EmailBuilderService', '$stateParams', '$state', 'toaster', 'AccountService', 'WebsiteService', '$modal'];
     /* @ngInject */
-    function indiEmailBuilderController($scope, $rootScope, EmailBuilderService, $stateParams, $state, toaster) {
+    function indiEmailBuilderController($scope, EmailBuilderService, $stateParams, $state, toaster, AccountService, WebsiteService, $modal) {
 
         console.info('email-builder directive init...');
 
@@ -13,13 +13,43 @@
         vm.init = init;
         vm.emailId = $stateParams.id;
         vm.dataLoaded = false;
+        vm.account = null;
+        vm.website = null;
         vm.email = null;
+        vm.modalInstance = null;
+        vm.openModalFn = openModalFn;
+        vm.closeModalFn = closeModalFn;
+
         vm.froalaConfig = angular.copy($.FroalaEditor.config);
         _.extend(vm.froalaConfig, {toolbarInline: false, scrollableContainer: '#email-froala-scrollable-container', placeholderText: 'Type your email here'});
+
+        function openModalFn(modalId) {
+            vm.modalInstance = $modal.open({
+                templateUrl: modalId,
+                keyboard: true,
+                size: 'lg',
+                scope: $scope
+            });
+        }
+
+        function closeModalFn() {
+            if (vm.modalInstance) {
+                vm.modalInstance.close();
+            }
+        }
 
         function init(element) {
             vm.element = element;
             vm.element.find('#email-froala-editor').froalaEditor(vm.froalaConfig);
+
+            AccountService.getAccount(function (data) {
+                vm.account = data;
+            });
+
+            WebsiteService.getWebsite(function (data) {
+                vm.website = data;
+            });
+
             EmailBuilderService.getEmail(vm.emailId)
                     .then(function (res) {
                         if (!res.data._id) {
