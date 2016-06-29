0187fe3e6469dc9e9b9223aecdaa22247416cd52
Jaideep Das <jaideep.jdof@gmail.com>
2016-06-29 12:59:49 +0530

    #6623 - restructuring component loading

--- public/admin/assets/js/indi-email-builder/email-component-loader/email-component-loader.component.js
+++ public/admin/assets/js/indi-email-builder/email-component-loader/email-component-loader.component.js
@@ -1,50 +1,32 @@
 (function () {
 
-    app.directive('emailComponentLoader', emailComponentLoader);
+  app.directive('emailComponentLoader', emailComponentLoader);
 
-    emailComponentLoader.$inject = ['$compile', '$timeout'];
-    /* @ngInject */
-    function emailComponentLoader($compile, $timeout) {
-        return {
-            restrict: 'E',
-            controller: 'EmailBuilderComponentLoaderController',
-            controllerAs: 'vm',
-            bindToController: true,
-            scope: {
-                component: "=",
-                website: "=",
-                account: '=',
-                componentClass: '&',
-                componentStyle: '&',
-                componentIndex: '='
-            },
-            replace: true,
-            templateUrl: '/admin/assets/js/indi-email-builder/email-component-loader/email-component-loader.component.html',
-            link: function (scope, element, attrs, ctrl) {
-//                console.log(scope);
-//                var newEl;
-//                var template = '<div ' + ctrl.component.type + '-email-component ' +
-//                        'id="email-component_' + ctrl.component._id + '" ' +
-//                        'component="vm.component" ' +
-//                        'website="vm.website" ' +
-//                        'account="vm.account" ' +
-//                        'class="email-component email-{{vm.component.type}} {{vm.componentClass(vm.component)}}" ' +
-//                        'ng-attr-style="{{vm.componentStyle(vm.component)}}" ' +
-//                        'ng-mouseenter="vm.hover($event);" ' +
-//                        'component-index="vm.componentIndex">' +
-//                        '</div>';
-//
-//                var compiled = $compile(template)(scope);
-//                element.replaceWith(compiled);
-//
-//                $timeout(function () {
-//                    newEl = angular.element('#email-component_' + ctrl.component._id);
-//                    ctrl.init(newEl);
-//                });
+  emailComponentLoader.$inject = ['$compile', '$timeout'];
+  /* @ngInject */
+  function emailComponentLoader($compile, $timeout) {
+    return {
+      restrict: 'A',
+      controller: 'EmailBuilderComponentLoaderController',
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        component: "=",
+        website: "=",
+        account: '=',
+        componentClass: '&',
+        componentStyle: '&',
+        componentIndex: '=',
+        componentCount: '='
+      },
+      replace: false,
+      templateUrl: '/admin/assets/js/indi-email-builder/email-component-loader/email-component-loader.component.html',
+      link: function (scope, element, attrs, ctrl) {
 
-            }
-        }
 
+      }
     }
+
+  }
 
 })();
--- public/admin/assets/js/indi-email-builder/email-component-loader/email-component-loader.controller.js
+++ public/admin/assets/js/indi-email-builder/email-component-loader/email-component-loader.controller.js
@@ -11,24 +11,24 @@
         var vm = this;
 
         vm.init = init;
-        vm.hoverFn = hoverFn;
-        vm.clickFn = clickFn;
-        vm.selector = '#email-component_' + vm.component._id;
-
-        vm.froalaConfig = angular.copy($.FroalaEditor.config);
-//        vm.froalaConfig.scrollableContainer = '#email-froala-scrollable-container';
-        _.extend(vm.froalaConfig, {toolbarInline: false, scrollableContainer: '#email-froala-scrollable-container',  toolbarContainer: '#email-froala-container', placeholderText: 'Type your email here'});
-
-        function hoverFn(e) {}
-
-        function clickFn(e) {
-//            $('.email-component').froalaEditor('destroy');
-            e.stopPropagation();
-            if (!$(vm.selector).data('froala.editor')) {
-                $(vm.selector).froalaEditor(vm.froalaConfig);
-                console.info('froala initiated for', vm.selector);
-            }
-        }
+//        vm.hoverFn = hoverFn;
+//        vm.clickFn = clickFn;
+//        vm.selector = '#email-component_' + vm.component._id;
+//
+//        vm.froalaConfig = angular.copy($.FroalaEditor.config);
+////        vm.froalaConfig.scrollableContainer = '#email-froala-scrollable-container';
+//        _.extend(vm.froalaConfig, {toolbarInline: false, scrollableContainer: '#email-froala-scrollable-container',  toolbarContainer: '#email-froala-container', placeholderText: 'Type your email here'});
+//
+//        function hoverFn(e) {}
+//
+//        function clickFn(e) {
+////            $('.email-component').froalaEditor('destroy');
+//            e.stopPropagation();
+//            if (!$(vm.selector).data('froala.editor')) {
+//                $(vm.selector).froalaEditor(vm.froalaConfig);
+//                console.info('froala initiated for', vm.selector);
+//            }
+//        }
 
         function init(element) {
             vm.element = element;
