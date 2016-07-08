df3b069174974b7d4eeb507a80905f9cded46feb
Jack Bishop <jack@indigenous.io>
2016-07-08 12:34:05 -0400

    #6842 - hide blog pages and SB leftbar Blog button if blog is turned off

--- public/admin/assets/js/filters/sortListPages.js
+++ public/admin/assets/js/filters/sortListPages.js
@@ -1,15 +1,27 @@
 'use strict';
 
 app.filter('sortListPages', function () {
-  return function (pages, id) {
+  return function (pages, id, account) {
 
         if (pages) {
             var pageArr = _(pages).chain()
                         .where({_id: id})
                         .pluck("_id")
                         .value()
+
             var _sortOrder = _.invert(_.object(_.pairs(pageArr)));
-            var _list =  _.sortBy(pages, function(x) {
+
+            var _filteredPages = pages;
+
+            if (account && !account.showhide.blog) {
+                _filteredPages = _(pages).chain()
+                        .filter(function(page) {
+                            return page.handle !== 'blog-list' && page.handle !== 'blog-post';
+                        })
+                        .value();
+            }
+
+            var _list =  _.sortBy(_filteredPages, function(x) {
                 return _sortOrder[x._id]
             });
 
