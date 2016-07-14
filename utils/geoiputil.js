88a37bbfce8d51db1b5e193a1ba0b77cc3b9e47e
Kyle Miller <millkyl@gmail.com>
2016-07-14 14:47:02 -0500

    #6909 Integrate local maxmind geoip db

--- utils/geoiputil.js
+++ utils/geoiputil.js
@@ -8,8 +8,20 @@
 var request = require('request');
 var log =  $$.g.getLogger("geoiputil");
 var providerURL = 'http://ipinfo.io';
+var maxmind = require('maxmind');
+var initialized = false;
 
 var geoiputil = {
+
+    mmdb: null,
+
+    _initialize: function() {
+        var self = this;
+        if(!initialized) {
+            self.mmdb = maxmind.open('./utils/GeoLite2-City.mmdb');
+            initialized = true;
+        }
+    },
 
 
     getGeoForIP: function(ip, fn) {
@@ -40,9 +52,45 @@ var geoiputil = {
 
 
         });
-    }
+    },
 
-}
+    getMaxMindGeoForIP: function(ip, fn) {
+        var self = this;
+        self._initialize();
+        var result = self.mmdb.get(ip);
+        result = self._sanitizeMMResult(ip, result);
+        //log.debug('maxmind thinks ip[' + ip + '] is:', result);
+        fn(null, result);
+    },
+
+    _sanitizeMMResult: function(ip, result) {
+        var obj = {
+            ip: ip
+        };
+        if(result.city && result.city.names) {
+            obj.city = result.city.names.en;
+        }
+        if(result.continent && result.continent.names) {
+            obj.continent_code = result.continent.code;
+            obj.continent = result.continent.names.en;
+        }
+        if(result.country) {
+            obj.country = result.country.iso_code;
+        }
+        if(result.location) {
+            obj.timezone = result.location.time_zone;
+            obj.loc = result.location.latitude + ',' + result.location.longitude;
+        }
+        if(result.postal) {
+            obj.postal = result.postal.code;
+        }
+        if(result.subdivisions && result.subdivisions[0] && result.subdivisions[0].names) {
+            obj.state = result.subdivisions[0].iso_code;
+            obj.region = result.subdivisions[0].names.en;
+        }
+        return obj;
+    }
+};
 
 
 $$.u = $$.u || {};
