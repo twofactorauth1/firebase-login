ed713ea3c3bfee449681e5a0c54e9e1a59333d84
Jaideep Das <jaideep.jdof@gmail.com>
2016-08-01 17:31:43 +0530

    #6559 - google map api loading dynamic

--- public/scripts/directives/externalScripts.js
+++ public/scripts/directives/externalScripts.js
@@ -10,7 +10,8 @@ angular.module('mainApp')
 
                     var scriptLookup = {
                         '<script src="https://www.paypalobjects.com/js/external/dg.js" async></script>': ['products', 'ssb-form-donate'],
-                        '<script src="https://js.stripe.com/v2/?tmp" async></script>': ['products', 'ssb-form-donate']
+                        '<script src="https://js.stripe.com/v2/?tmp" async></script>': ['products', 'ssb-form-donate'],
+                        '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMI1SIOEHJm83bMZ-lWjzZN1nXdk6S0X0" async></script>': ['contact-us']
                     };
 
                     scope.$on('external.scripts.page.data', function (event, args) {
