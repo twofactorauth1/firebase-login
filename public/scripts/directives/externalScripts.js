398db46fc1053fcb41c2e162f7e1e0bb80ef8b73
Jaideep Das <jaideep.jdof@gmail.com>
2016-08-01 19:31:54 +0530

    #6559 - added frontend constants file

--- public/scripts/directives/externalScripts.js
+++ public/scripts/directives/externalScripts.js
@@ -1,6 +1,6 @@
 angular.module('mainApp')
-    .directive('externalScripts', ['$sce',
-        function ($sce) {
+    .directive('externalScripts', ['$sce', 'externalScriptLookup',
+        function ($sce, externalScriptLookup) {
             return {
                 restrict: 'E',
                 replace: true,
@@ -8,11 +8,7 @@ angular.module('mainApp')
                 link: function (scope, elem, attr) {
                     scope.externalScripts = '';
 
-                    var scriptLookup = {
-                        '<script src="https://www.paypalobjects.com/js/external/dg.js" async></script>': ['products', 'ssb-form-donate'],
-                        '<script src="https://js.stripe.com/v2/?tmp" async></script>': ['products', 'ssb-form-donate'],
-                        '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMI1SIOEHJm83bMZ-lWjzZN1nXdk6S0X0" async></script>': ['contact-us']
-                    };
+                    var scriptLookup = externalScriptLookup;
 
                     scope.$on('external.scripts.page.data', function (event, args) {
                         scope.externalScripts = '';
