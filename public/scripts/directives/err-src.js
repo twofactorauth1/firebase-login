214e48e54d4da68913d1c18926edccdbc9e38508
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2016-08-30 16:34:42 +0530

    Broken image appear on product component at backend and frontend of application This happens if user does not apply any image on product #7211

---
+++ public/scripts/directives/err-src.js
@@ -0,0 +1,65 @@
+'use strict';
+/**
+ * Any element that gets a 404 image get replaced
+ */
+app.directive('errSrc', function () {
+  return {
+    scope: {
+      errSrc: '@',
+      errContact: '='
+    },
+    link: function (scope, element, attrs) {
+
+      var colors = ["turquoise", "emerland", "peterriver", "amethyst", "wetasphalt", "greensea", "nephritis", "belizehole", "wisteria", "midnightblue", "sunflower", "carrot", "alizarin", "clouds", "concrete", "orange", "pumpkin", "pomegranate", "silver", "asbestos"];
+
+      scope.$watch(function () {
+        return attrs['ngSrc'];
+      }, function (value) {
+        if (!value && scope.errContact) {
+          element.hide();
+          var contentTr = angular.element('<div class="animated bounce two-letter-label ' + _.sample(colors) + '">' + scope.getContactLetters() + '</div>');
+          contentTr.insertAfter(element);
+        } 
+        else if(!value)
+        {
+           element.attr('src', scope.errSrc);
+        }
+      });
+
+      element.bind('error', function () {
+        if (scope.errSrc.length > 0) {
+          element.attr('src', scope.errSrc);
+        } else {
+          element.hide();
+          var contentTr = angular.element('<div class="animated bounce two-letter-label ' + _.sample(colors) + '">' + scope.getContactLetters() + '</div>');
+          contentTr.insertAfter(element);
+        }
+      });
+
+      var icons = ['beer', 'diamond', 'fighter-jet', 'gamepad', 'flag-checkered', 'key', 'leaf', 'motorcycle', 'ship', 'space-shuttle'];
+
+      scope.getContactLetters = function () {
+        var letters = '';
+        var contact = scope.errContact;
+        //get first letter of email
+        if (scope.errContact && scope.errContact.details && scope.errContact.details.length > 0
+            && scope.errContact.details[0].emails && scope.errContact.details[0].emails.length > 0
+            && scope.errContact.details[0].emails[0].email) {
+          letters = scope.errContact.details[0].emails[0].email.charAt(0);
+        }
+        //get initials
+        if (scope.errContact && scope.errContact.first) {
+          letters = scope.errContact.first.charAt(0);
+        }
+        if (scope.errContact && scope.errContact.last) {
+          letters = letters + ' ' + scope.errContact.last.charAt(0);
+        }
+
+        if (letters.length <= 0) {
+          letters = '<span class="fa fa-' + _.sample(icons) + '"></span>';
+        }
+        return letters;
+      }
+    }
+  }
+});
