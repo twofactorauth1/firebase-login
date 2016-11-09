527d61047cd4855a93ae4d12d4c3e75d7d7b59ed
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2016-11-09 18:09:00 +0530

    Save state of analytics widgets in site analytics and platform analytics, in cookie #7534

---
+++ public/admin/assets/js/site-analytics/analytics-widget-state-service.js
@@ -0,0 +1,68 @@
+'use strict';
+/*global app, window, $$*/
+/*jslint unparam:true*/
+(function () {
+
+    app.factory('AnalyticsWidgetStateService', AnalyticsWidgetStateService);
+
+    AnalyticsWidgetStateService.$inject = ['ipCookie'];
+    /* @ngInject */
+    function AnalyticsWidgetStateService(ipCookie) {
+        var analylicService = {};
+
+        analylicService.analyticsWidgetStateConfig = {
+            "overview": false,
+            "locations": false,
+            "interactions": false,
+            "device": false,
+            "newVReturning": false,
+            "trafficSources": false,
+            "pageanalytics": false,
+            "ua": false,
+            "userAgents": false,
+            "rev": false,
+            "os": false,
+            "campaigns": false
+        };
+        
+        analylicService.getAnalyticsWidgetStates = getAnalyticsWidgetStates;
+        analylicService.setAnalyticsWidgetStates = setAnalyticsWidgetStates;
+        
+        function getAnalyticsWidgetStates() {
+            analylicService.analyticsWidgetStateConfig;
+            if(ipCookie("analyticsWidgetState")){
+                analylicService.analyticsWidgetStateConfig = ipCookie("analyticsWidgetState");
+            }
+            else{
+                setCookie(analylicService.analyticsWidgetStateConfig);
+            }
+        }
+
+        function setAnalyticsWidgetStates(name, value){
+            var ckAnalyticsWidgetState = ipCookie("analyticsWidgetState");
+            if(ckAnalyticsWidgetState){
+                ckAnalyticsWidgetState[name] = value;
+                ipCookie.remove("analyticsWidgetState", {
+                    path: "/"
+                });
+                setCookie(ckAnalyticsWidgetState);
+            }
+        }
+
+        function setCookie(cookie){
+            ipCookie("analyticsWidgetState", cookie, {
+                expires: 20*365,
+                path: "/"
+            });
+        }
+
+
+        (function init() {
+            
+        })();
+
+
+        return analylicService;
+    }
+
+})();
