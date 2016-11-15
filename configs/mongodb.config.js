28af9ce44f6a2ae6395fef51193f1086cb00be4d
Kyle Miller <millkyl@gmail.com>
2016-11-15 13:35:37 -0600

    #7370 changed passwords in config.  already changed environment vars

--- configs/mongodb.config.js
+++ configs/mongodb.config.js
@@ -11,8 +11,8 @@ var config = require('./app.config');
  * Set up the connection strings for each environment.
  */
 var localString = "mongodb://localhost/test_indigenous";
-var testString = "mongodb://indiweb-test:testing123@lighthouse.1.mongolayer.com:10303,lighthouse.0.mongolayer.com:10303/test_indigenous?replicaSet=set-54f7c735c346b1d3bf0005b0";
-var prodString = "mongodb://indiapp:1ndigenous2015!@lighthouse.1.mongolayer.com:10188/prod_indigenous";
+var testString = "mongodb://indiweb-test2016:testing456@lighthouse.1.mongolayer.com:10303,lighthouse.0.mongolayer.com:10303/test_indigenous?replicaSet=set-54f7c735c346b1d3bf0005b0";
+var prodString = "mongodb://indiapp2:Ind1genous2016!@lighthouse.1.mongolayer.com:10188,lighthouse.0.mongolayer.com:10188/prod_indigenous?replicaSet=set-53f6b4cc19627093350038e8";
 
 //LOCAL
 //var connectionString = localString;
