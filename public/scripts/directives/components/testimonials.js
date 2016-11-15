25752ffee3e5f19c0b99add8c11cbbe8bc853685
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2016-11-15 12:20:26 +0530

    nav + hero loads after rest of page, and all at once expanding into place #7607

--- public/scripts/directives/components/testimonials.js
+++ public/scripts/directives/components/testimonials.js
@@ -5,23 +5,17 @@ app.directive('testimonialsComponent', ['$timeout', function ($timeout) {
     },
     templateUrl: '/components/component-wrap.html',
     link: function (scope, element, attrs, ctrl) {
-    	$(document).ready(function () {
-          $timeout(function () {
-            scope.$apply(function () {
-              if(!scope.component.slider)
-              {
-                scope.component.slider = {
-                  speed: 300, autoPlay: true, autoPlayInterval: 5000
-                };
-              }
-              scope.touchMove = true;
-              scope.draggable = true;
-              scope.autoplay = scope.component.slider.autoPlay;
-              scope.dataLoaded = true;
-              scope.accessibility = true;
-            });
-          },0);
-      });
+      if(!scope.component.slider)
+      {
+        scope.component.slider = {
+          speed: 300, autoPlay: true, autoPlayInterval: 5000
+        };
+      }
+      scope.touchMove = true;
+      scope.draggable = true;
+      scope.autoplay = scope.component.slider.autoPlay;
+      scope.dataLoaded = true;
+      scope.accessibility = true;
     }
   }
 }]);
