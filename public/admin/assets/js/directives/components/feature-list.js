0df070579779c9041f213b7154ac87aaf3ff10ba
Chris Ricci <chris@indigenous.io>
2016-11-21 14:57:09 -0600

    #7636 - Use default font color for arrow on feature list

--- public/admin/assets/js/directives/components/feature-list.js
+++ public/admin/assets/js/directives/components/feature-list.js
@@ -16,7 +16,7 @@ app.directive('featureListComponent',["$window", "$timeout", function ($window, 
                 index = 0;
             }
             var newFeature = {
-            "top": "<div style='text-align:center'><span class=\"fa fa-arrow-right\" style=\"color:#ffffff;font-size:96px;\">&zwnj;</span></div>",
+            "top": "<div style='text-align:center'><span class=\"fa fa-arrow-right\" style=\"font-size:96px;\">&zwnj;</span></div>",
             "content": "<div style=\"text-align: center;\"><br><span style=\"font-size:24px;\">Feature Title</span></div><div style=\"text-align: center;\"><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi ab, placeat. Officia qui molestiae incidunt est adipisci.</div><div style=\"text-align: center;\"><br><a class=\"btn ssb-theme-btn\" data-cke-saved-href=\"http://\" href=\"http://\">Learn More</a></div>"
             };
             scope.component.features.splice(index + 1, 0, newFeature);
@@ -37,7 +37,7 @@ app.directive('featureListComponent',["$window", "$timeout", function ($window, 
             }
 
             if(component.blockbgcolor){
-                styleString += 'background: ' + component.blockbgcolor;   
+                styleString += 'background: ' + component.blockbgcolor;
             }
 
             return styleString;
