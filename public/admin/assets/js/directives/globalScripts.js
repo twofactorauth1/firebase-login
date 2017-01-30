8b789a3d0c1086391d5f24ab0992a647980f4bc5
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-01-30 18:01:17 +0530

    Loading of global scripts in admin

--- public/admin/assets/js/directives/globalScripts.js
+++ public/admin/assets/js/directives/globalScripts.js
@@ -1,30 +1,27 @@
-app.directive('userScripts', [ '$sce',
+app.directive('globalScripts', [ '$sce',
     function( $sce) {
         return {
             restrict: 'E',
             replace: true,
-            template: '<span ng-bind-html="pageScripts"></span>',
+            template: '<span ng-bind-html="globalScripts"></span>',
             scope: {
                 website: '=',
-                handle: '=',
                 account: '='
             },
             link: function(scope, elem, attr) {
-                scope.pageScripts = '';
+                scope.globalScripts = '';
 
                 var processFn = function() {
-                    scope.pageScripts = '';
+                    scope.globalScripts = '';
                     var account = scope.account;
                     var website = scope.website;
-                    var handle = scope.handle;
-                    console.info('User Script >>>', handle);
                    
                     if (account.showhide.userScripts) {                               
                         if (website && website.resources && website.resources.userScripts && website.resources.toggles && website.resources.toggles.userScripts) {
-                            if (angular.isDefined(website.resources.userScripts[scope.handle])) {
-                                scope.pageScripts += '\n\n' + website.resources.userScripts[scope.handle].sanitized;
+                            if (angular.isDefined(website.resources.userScripts.global)) {
+                                scope.globalScripts += '\n\n' + website.resources.userScripts.global.sanitized;
                             }
-                            scope.pageScripts = $sce.trustAsHtml(scope.pageScripts);
+                            scope.globalScripts = $sce.trustAsHtml(scope.globalScripts);
                         }
                     }
                 };
