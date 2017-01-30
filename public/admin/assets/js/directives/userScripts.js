3b1d1cf925ed89baeec21e4539715c155f9435d0
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-01-30 14:00:13 +0530

    #7994 removed unwanted params

--- public/admin/assets/js/directives/userScripts.js
+++ public/admin/assets/js/directives/userScripts.js
@@ -1,36 +1,35 @@
+app.directive('userScripts', [ '$sce',
+    function( $sce) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div ng-bind-html="pageScripts"></span>',
+            scope: {
+                website: '=',
+                handle: '=',
+                account: '='
+            },
+            link: function(scope, elem, attr) {
+                scope.pageScripts = '';
 
-    app.directive('userScripts', [ '$sce', '$location',
-        function( $sce, $location) {
-            return {
-                restrict: 'E',
-                replace: true,
-                template: '<div ng-bind-html="pageScripts"></span>',
-                scope: {
-                    website: '=',
-                    handle: '=',
-                    account: '='
-                },
-                link: function(scope, elem, attr) {
+                var processFn = function() {
                     scope.pageScripts = '';
-
-                    var processFn = function() {
-                        scope.pageScripts = '';
-                        var account = scope.account;
-                        var website = scope.website;
-                        var handle = scope.handle;
-                        console.info('User Script >>>', handle);
-                       
-                        if (account.showhide.userScripts) {                               
-                            if (website && website.resources && website.resources.userScripts && website.resources.toggles && website.resources.toggles.userScripts) {
-                                if (angular.isDefined(website.resources.userScripts[scope.handle])) {
-                                    scope.pageScripts += '\n\n' + website.resources.userScripts[scope.handle].sanitized;
-                                }
-                                scope.pageScripts = $sce.trustAsHtml(scope.pageScripts);
+                    var account = scope.account;
+                    var website = scope.website;
+                    var handle = scope.handle;
+                    console.info('User Script >>>', handle);
+                   
+                    if (account.showhide.userScripts) {                               
+                        if (website && website.resources && website.resources.userScripts && website.resources.toggles && website.resources.toggles.userScripts) {
+                            if (angular.isDefined(website.resources.userScripts[scope.handle])) {
+                                scope.pageScripts += '\n\n' + website.resources.userScripts[scope.handle].sanitized;
                             }
+                            scope.pageScripts = $sce.trustAsHtml(scope.pageScripts);
                         }
-                    };
-                    processFn();
-                }
-            };
-        }
-    ]);
+                    }
+                };
+                processFn();
+            }
+        };
+    }
+]);
