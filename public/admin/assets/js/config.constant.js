f1afa1fa0c2bd12ef4915ff7642378829d1d15bc
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-02-17 12:40:21 +0530

    Allow target to be selected in link. #8076

--- public/admin/assets/js/config.constant.js
+++ public/admin/assets/js/config.constant.js
@@ -875,7 +875,7 @@ app.constant('JS_REQUIRES', {
       "../js/libs_misc/froala-wysiwyg-editor/js/plugins/image-manager.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/inline_style.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/line_breaker.min.js",
-      "../js/libs/froala-wysiwyg-editor/js/plugins/link.min.js",
+      "../js/libs_misc/froala-wysiwyg-editor/js/plugins/link.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/lists.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/paragraph_format.min.js",
       "../js/libs/froala-wysiwyg-editor/js/plugins/paragraph_style.min.js",
