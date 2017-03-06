f1c6ff73aef4eec18a96bf26f09fe2e91032c5bc
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-03-06 16:51:53 +0530

    Added less for invetory details and back to inventory functionality

--- public/admin/assets/var/demo/js/inventory/inventory-details/inventory-details.controller.js
+++ public/admin/assets/var/demo/js/inventory/inventory-details/inventory-details.controller.js
@@ -2,15 +2,21 @@
 
 app.controller('InventoryDetailsController', inventoryDetailsController);
 
-inventoryDetailsController.$inject = ['$scope', '$attrs', '$filter', '$modal', '$timeout', '$stateParams', '$location', 'InventoryService'];
+inventoryDetailsController.$inject = ['$scope', '$state', '$attrs', '$filter', '$modal', '$timeout', '$stateParams', '$location', 'InventoryService'];
 /* @ngInject */
-function inventoryDetailsController($scope, $attrs, $filter, $modal, $timeout, $stateParams, $location, InventoryService) {
+function inventoryDetailsController($scope, $state, $attrs, $filter, $modal, $timeout, $stateParams, $location, InventoryService) {
 
     var vm = this;
 
     vm.init = init;
 
     console.log($stateParams.inventoryId);
+
+    vm.backToInventory = backToInventory;
+
+    function backToInventory(){
+        $state.go("app.inventory");
+    }
 
     InventoryService.getSingleInventory($stateParams.inventoryId).then(function(response){
         vm.inventory = response;
