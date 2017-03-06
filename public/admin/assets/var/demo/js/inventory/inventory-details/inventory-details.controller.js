36f147db890b81256ff52c03f0b67e5fb1c66071
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-03-06 20:02:09 +0530

    #8172 Sales Timeline data asnd design

--- public/admin/assets/var/demo/js/inventory/inventory-details/inventory-details.controller.js
+++ public/admin/assets/var/demo/js/inventory/inventory-details/inventory-details.controller.js
@@ -18,6 +18,41 @@ function inventoryDetailsController($scope, $state, $attrs, $filter, $modal, $ti
         $state.go("app.inventory");
     }
 
+    vm.salesTimelineData = [
+        {
+            _date: '01/17/2017',
+            _time: ' 03:50 AM',
+            userName: "Alberta Dennis",
+            qty: 1,
+            revenue: "$81",
+            grown: true 
+        },{
+            _date: '01/17/2016',
+            _time: ' 02:20 AM',
+            userName: "Kyle Warner",
+            qty: 5,
+            revenue: "$405",
+            grown: true 
+        },
+        {
+            _date: '01/17/2016',
+            _time: ' 01:10 AM',
+            userName: "Alice Peter",
+            qty: 3,
+            revenue: "$243",
+            grown: false 
+        },
+        {
+            _date: '01/17/2016',
+            _time: ' 00:30 AM',
+            userName: "Noah Long",
+            qty: 1,
+            revenue: "$81",
+            grown: true 
+        }
+    ]
+
+
     InventoryService.getSingleInventory($stateParams.inventoryId).then(function(response){
         vm.inventory = response;
     })
