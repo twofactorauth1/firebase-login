68d10f3bdc5dbb766ba2e395867644511ab07d99
Kyle Miller <millkyl@gmail.com>
2017-04-04 16:14:02 -0500

    add config for purchase order bucket

--- configs/aws.config.js
+++ configs/aws.config.js
@@ -52,7 +52,7 @@ module.exports = {
         SCREENSHOTS: 'indigenous-screenshots',
         DB_ARCHIVES: 'indigenous-db-archives',
         REPORTS: 'indigenous-reports',
-        PURCHASE_ORDERS: 'purchase-orders'
+        PURCHASE_ORDERS: 'indigenous-purchase-orders'
     },
     "accessKeyId":AWS_ACCESS_KEY,
     "secretAccessKey":AWS_SECRET_ACCESS_KEY,
