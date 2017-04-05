72156efeda0e141478a6ae5685da02a8ef64e6da
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-04-05 17:17:33 +0530

    #8333 PO Detail view api and left pane implementation

--- public/admin/assets/var/demo/js/purchase-order/purchase-order-details/purchase-order-details.controller.js
+++ public/admin/assets/var/demo/js/purchase-order/purchase-order-details/purchase-order-details.controller.js
@@ -10,17 +10,27 @@ function purchaseOrderDetailsController($scope, $state, $attrs, $filter, $modal,
 
     vm.init = init;
 
+    vm.state = {};
+
     console.log($stateParams.purchaseOrderId);
 
     vm.backToPurchaseOrders = backToPurchaseOrders;
+    vm.getPurchaseOrderDetails = getPurchaseOrderDetails;
 
     function backToPurchaseOrders(){
         $state.go("app.purchaseorders");
     }
 
+    function getPurchaseOrderDetails(orderId){
+        PurchaseOrderService.getPurchaseOrderDetails(orderId).then(function(response){
+            vm.state.purchaseOrder = response.data;
+        })
+    }
+
 
     function init(element) {
         vm.element = element;
+        vm.getPurchaseOrderDetails($stateParams.purchaseOrderId);
     }
 
 }
