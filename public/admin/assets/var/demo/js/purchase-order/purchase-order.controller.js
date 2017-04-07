79024996869e03f6d73d2296e11c143866bdbf07
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-04-07 11:21:40 +0530

    Securematics > PO > Add support for XLS files #8420

--- public/admin/assets/var/demo/js/purchase-order/purchase-order.controller.js
+++ public/admin/assets/var/demo/js/purchase-order/purchase-order.controller.js
@@ -66,7 +66,7 @@ function purchaseOrderComponentController($scope, $attrs, $filter, $modal, $time
 
 
     function checkIfInValid(po){
-        if(po && po.attachment && po.attachment.type == 'application/pdf'){
+        if(po && po.attachment){
             return false;
         }
         else{
