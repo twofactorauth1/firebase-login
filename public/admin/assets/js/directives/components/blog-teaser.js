66606022718cca76c9805e3aee9ed856636400c6
Ashish <ashu8006kumar@gmail.com>
2017-04-12 13:49:01 +0530

    blog-teaser changes applied #8455

--- public/admin/assets/js/directives/components/blog-teaser.js
+++ public/admin/assets/js/directives/components/blog-teaser.js
@@ -9,6 +9,7 @@ app.directive('blogTeaserComponent', ['WebsiteService', '$filter', function (Web
     },
     templateUrl: '/components/component-wrap.html',
     link: function (scope, element, attrs) {
+        debugger
       scope.isEditing = true;
     },
     controller: function ($scope, WebsiteService, $compile) {
@@ -33,7 +34,40 @@ app.directive('blogTeaserComponent', ['WebsiteService', '$filter', function (Web
           }
         };
       };
-
+      $scope.titleStyle = function (component) {
+        var styleString = ' ';
+		if(component && component.settings){
+            if(component.settings.title){
+                if(component.settings.title.fontSize){
+                    styleString += 'font-size: ' + component.settings.title.fontSize + 'px !important;';
+                }
+                if(component.settings.title.fontFamily){
+                    styleString += 'font-family: ' + component.settings.title.fontFamily + 'px !important;';
+                }
+                if(component.settings.title.color){
+                    styleString += 'color: ' + component.settings.title.color + "!important;";
+                }
+            }
+		}
+		return styleString;
+      };
+      $scope.descriptionStyle = function (component) {
+        var styleString = ' ';
+		if(component && component.settings){
+            if(component.settings.description){
+                if(component.settings.description.fontSize){
+                    styleString += 'font-size: ' + component.settings.description.fontSize + 'px !important;';
+                }
+                if(component.settings.description.fontFamily){
+                    styleString += 'font-family: ' + component.settings.description.fontFamily + 'px !important;';
+                }
+                if(component.settings.description.color){
+                    styleString += 'color: ' + component.settings.description.color + "!important;";
+                }
+            }
+		}
+		return styleString;
+      };
       $scope.customSortOrder = function (component) {
         if (component.postorder == 1 || component.postorder == 3 || component.postorder == 5) {
           return false;
--- public/scripts/directives/components/blog-teaser.js
+++ public/scripts/directives/components/blog-teaser.js
@@ -31,7 +31,40 @@ app.directive('blogTeaserComponent', ['postsService', '$filter', function (posts
             return Date.parse($filter('date')(blogpost.publish_date || blogpost.created.date, "MM/dd/yyyy"));
         };
       };
-
+      $scope.titleStyle = function (component) {
+        var styleString = ' ';
+		if(component && component.settings){
+            if(component.settings.title){
+                if(component.settings.title.fontSize){
+                    styleString += 'font-size: ' + component.settings.title.fontSize + 'px !important;';
+                }
+                if(component.settings.title.fontFamily){
+                    styleString += 'font-family: ' + component.settings.title.fontFamily + 'px !important;';
+                }
+                if(component.settings.title.color){
+                    styleString += 'color: ' + component.settings.title.color + "!important;";
+                }
+            }
+		}
+		return styleString;
+      };
+      $scope.descriptionStyle = function (component) {
+        var styleString = ' ';
+		if(component && component.settings){
+            if(component.settings.description){
+                if(component.settings.description.fontSize){
+                    styleString += 'font-size: ' + component.settings.description.fontSize + 'px !important;';
+                }
+                if(component.settings.description.fontFamily){
+                    styleString += 'font-family: ' + component.settings.description.fontFamily + 'px !important;';
+                }
+                if(component.settings.description.color){
+                    styleString += 'color: ' + component.settings.description.color + "!important;";
+                }
+            }
+		}
+		return styleString;
+      };
       $scope.customSortOrder = function (component) {
         if (component.postorder == 1 || component.postorder == 3 || component.postorder == 5) {
           return false;
