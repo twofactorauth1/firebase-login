c6e74c8b5bd2e738dcddb62c161451d0b723d8ba
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-04-13 14:57:19 +0530

    Completed (Customer Ledger) List UI

--- public/admin/assets/var/demo/js/invoices/invoice.controller.js
+++ public/admin/assets/var/demo/js/invoices/invoice.controller.js
@@ -12,6 +12,20 @@ function invoiceComponentController($scope, $attrs, $filter, $modal, $timeout, $
 
     vm.state = {};
 
+
+    vm.uiState = {
+        loading: true
+    };
+
+
+    $scope.$watch(function() { return InvoiceService.invoices }, function(invoices) {
+        if(angular.isDefined(invoices)){
+            vm.state.invoices = invoices;
+            vm.state.totalInvoices = invoices.length;
+            vm.uiState.loading = false;
+        }
+    }, true);
+
     function init(element) {
         vm.element = element;
     }
--- public/admin/assets/var/demo/js/invoices/invoice.service.js
+++ public/admin/assets/var/demo/js/invoices/invoice.service.js
@@ -15,6 +15,8 @@
 
         var baseInvoiceAPIUrl = '/api/1.0/integrations/zi/inventory';
 
+        invoiceService.getInvoices = getInvoices;
+
         invoiceService.loading = {value: 0};
 
         function invoiceRequest(fn) {
@@ -27,9 +29,34 @@
             return fn;
         }
 
+        /**
+            * Get list of all invoices
+        */
+        function getInvoices() {
+            var invoices = [];
+            var _d = null;
+            var _rand = null;
+            for (var i = 0; i < 30; i++) { 
+                _rand = (Math.random() * 1000000).toFixed(2);
+                _d = {
+
+                    invoiceNumber: Math.floor((Math.random() * 1000000) + 1),
+                    poNumber: Math.floor((Math.random() * 100000) + 1),
+                    invoiceDate: new Date(new Date().setDate(new Date().getDate() - 1)),
+                    dueDate: new Date(new Date().setDate(new Date().getDate() + 1)),
+                    invoiceTotal:_rand,
+                    invoiceBalance: _rand,
+                    invoiceStatus: 'Open'
+                } 
+                invoices.push(_d);   
+            }
+            
+            invoiceService.invoices = invoices;
+        }
+
 
 		(function init() {
-            
+            invoiceService.getInvoices();
 		})();
 
 
