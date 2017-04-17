b84156b97382429949d215b0e8b980f53237c043
Ashish <ashu8006kumar@gmail.com>
2017-04-17 13:52:09 +0530

    #8362 UI cleaning

--- public/admin/assets/js/ssb-site-builder/ssb-components/ssb-recent-post/ssb-recent-post.controller.js
+++ public/admin/assets/js/ssb-site-builder/ssb-components/ssb-recent-post/ssb-recent-post.controller.js
@@ -45,6 +45,9 @@ function ssbBlogRecentPostComponentController(SimpleSiteBuilderBlogService, $sco
             }, 2000)
             checkHasFeaturedPosts();
             if(vm.blog.posts.length>1){
+                if(vm.blog.posts.length>6){
+                  vm.blog.posts=   vm.blog.posts.slice(0, 6);
+                }
                 vm.element.closest("div.ssb-page-section").css({'display': 'block'});
             }else{
                  vm.element.closest("div.ssb-page-section").css({'display': 'none'});
