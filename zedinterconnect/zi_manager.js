951f8a053788aaad7e00ac63d337bd04c95d5c8c
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-04-17 12:54:01 +0530

    Customer Ledger-Invoice list replaced  with list of customers

--- zedinterconnect/zi_manager.js
+++ zedinterconnect/zi_manager.js
@@ -197,7 +197,7 @@ module.exports = {
     getLedger: function(accountId, userId, cardCodeFrom, cardCodeTo, dateString, fn) {
         var self = this;
         self.log.debug(accountId, userId, '>> getLedger');
-        var path = 'query/Indigenous/CustomerAging2.aspx?0=' + cardCodeFrom + '&1=' + cardCodeTo + '&2=' + dateString + '&accept=application/json';
+        var path = 'query/Indigenous/CustomerAging2.aspx?0=' + cardCodeFrom + '&1=' + cardCodeTo + '&accept=application/json';
 
         self._ziRequest(path, function(err, value) {
             if (err) {
