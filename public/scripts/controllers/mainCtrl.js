ddea5dcebb4ca617074996eda025be090f01fd0e
Kyle Miller <millkyl@gmail.com>
2017-04-21 11:34:45 -0500

    reformat for tabs

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -1,37 +1,37 @@
 'use strict';
 
 mainApp.controller('MainCtrl', ['$scope', 'websiteService', 'accountService', 'themeService', 'pagesService', 'ENV', '$location', '$document', '$anchorScroll', '$window', 'userService',
-  function ($scope, websiteService, accountService, themeService, pagesService, ENV, $location, $document, $anchorScroll, $window, userService) {
+    function ($scope, websiteService, accountService, themeService, pagesService, ENV, $location, $document, $anchorScroll, $window, userService) {
 
-    var account, pages, website, that = this;
-    that.segmentIOWriteKey = ENV.segmentKey;
+        var account, pages, website, that = this;
+        that.segmentIOWriteKey = ENV.segmentKey;
 
-    var body = document.body,
-      html = document.documentElement;
+        var body = document.body,
+            html = document.documentElement;
 
-    $scope.init = function (value) {
-      $scope.websiteId = value;
-    };
+        $scope.init = function (value) {
+            $scope.websiteId = value;
+        };
 
-    var height = Math.max(body.scrollHeight, body.offsetHeight,
-      html.clientHeight, html.scrollHeight, html.offsetHeight);
-    $scope.minHeight = height;
+        var height = Math.max(body.scrollHeight, body.offsetHeight,
+            html.clientHeight, html.scrollHeight, html.offsetHeight);
+        $scope.minHeight = height;
 
-    $scope.isSection = function (value) {
-      if (value == 'section') {
-        return true;
-      } else {
-        return false;
-      }
-    };
+        $scope.isSection = function (value) {
+            if (value == 'section') {
+                return true;
+            } else {
+                return false;
+            }
+        };
 
-    $scope.showAdminBar = false;
+        $scope.showAdminBar = false;
 
-    userService.isAuthenticatedSession(function (data) {
-      if (data === true) {
-        $scope.showAdminBar = true;
-      }
-    });
+        userService.isAuthenticatedSession(function (data) {
+            if (data === true) {
+                $scope.showAdminBar = true;
+            }
+        });
 
-  }
+    }
 ]);
