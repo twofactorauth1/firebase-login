b03032752475cfc9c35bbebd1361b9114c3362e0
Ashish <ashu8006kumar@gmail.com>
2017-05-02 12:38:44 +0530

    #8626 handling to slider images added .

--- public/scripts/directives/ind-slider.js
+++ public/scripts/directives/ind-slider.js
@@ -1,10 +1,36 @@
-app.directive('indSlider',function($timeout){
+app.directive('indSlider',function($timeout,$window){
 	return {
 		restrict: 'A',
 		link: function(scope,element,attrs) {
 			$timeout(function() {
 				$(element).slick(scope.$eval(attrs.indSlider));
 			}, 0);
+            angular.element($window).bind('resize', function(){
+
+          $timeout(function() {
+              var sliderHeight=$(element).height();
+              var textWrapHeight=$(element).closest("[testimonials-component]").siblings(".flex-container-absolute-column").height();
+              if(sliderHeight<textWrapHeight){
+                  console.log("change height")
+                  if($(element).hasClass('slick-initialized')){
+                  $(element).slick("unslick");
+                     $timeout(function () {
+                        scope.$apply(function () {
+                            $(element).closest("[testimonials-component]").attr("height",textWrapHeight+"px")
+                           $(element).slick(scope.$eval(attrs.indSlider));
+                        })
+                     }, 100);
+                  }
+              }
+			}, 0);
+
+         // manuall $digest required as resize event
+         // is outside of angular
+         scope.$digest();
+       });
+
+
+
 		}
 	}
 });
