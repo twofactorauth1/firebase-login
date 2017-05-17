523cf4065e7f1e5aa9c305e4f845898e79a9bde4
Ashish <ashu8006kumar@gmail.com>
2017-05-17 16:58:27 +0530

    timeout set to zero #8739

--- public/admin/assets/js/directives/ind-slider.js
+++ public/admin/assets/js/directives/ind-slider.js
@@ -6,15 +6,15 @@ app.directive('indSlider',function($timeout){
          $(element).slick(scope.$eval(attrs.indSlider));
      }, 500);
      scope.$on('updatetestimonialHeight.component', function(event, args) {
-          if($(element).hasClass('slick-initialized')){
-                $(element).slick("unslick");
-                $timeout(function () {
-                    scope.$apply(function () {
-                       $(element).slick(scope.$eval(attrs.indSlider));
-                    })
-                }, 100);
-            }
-        });
+         if($(element).hasClass('slick-initialized')){
+             $(element).slick("unslick");
+             $timeout(function () {
+                 scope.$apply(function () {
+                     $(element).slick(scope.$eval(attrs.indSlider));
+                 });
+             }, 0);
+         }
+     });
      scope.$on('$refreshSlickSlider', function (event, index) {
         if($(element).hasClass('slick-initialized')){
             $(element).slick("unslick");
