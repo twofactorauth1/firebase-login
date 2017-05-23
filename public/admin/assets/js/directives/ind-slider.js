6e028fd4f465c0642445db2280f71de74cef6fa0
Ashish <ashu8006kumar@gmail.com>
2017-05-23 19:06:42 +0530

    #8739 chaneg applied

--- public/admin/assets/js/directives/ind-slider.js
+++ public/admin/assets/js/directives/ind-slider.js
@@ -2,21 +2,23 @@ app.directive('indSlider',function($timeout){
  return {
    restrict: 'A',
    link: function(scope,element,attrs) {
-     $timeout(function() {
-         $(element).slick(scope.$eval(attrs.indSlider));
-     }, 500);
-     scope.$on('updatetestimonialHeight.component', function(event, args) {
+       $timeout(function() {
+           $(element).slick(scope.$eval(attrs.indSlider));
+       }, 500);
+       scope.$on('updatetestimonialHeight.component', function(event, args) {
+         scope.applyingNewWidth=true;
          if($(element).hasClass('slick-initialized')){
              $(element).slick("unslick");
              $timeout(function () {
                  scope.$apply(function () {
                      $(element).slick(scope.$eval(attrs.indSlider));
+                     scope.applyingNewWidth=false;
                  });
              }, 0);
          }
-     });
-     scope.$on('$refreshSlickSlider', function (event, index) {
-        if($(element).hasClass('slick-initialized')){
+       });
+       scope.$on('$refreshSlickSlider', function (event, index) {
+         if($(element).hasClass('slick-initialized')){
             $(element).slick("unslick");
             $timeout(function () {
                 scope.$apply(function () {
@@ -25,9 +27,8 @@ app.directive('indSlider',function($timeout){
                         $(element).slick("slickGoTo", index);
                 })
             }, 100);
-        }
-    });
-
+         }
+       });
    }
  }
 });
