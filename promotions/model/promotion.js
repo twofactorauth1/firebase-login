43cc01fe4f9cce1e28423014da15dd04c54369c5
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-06-08 13:18:38 +0530

    Added model, dao and manager for promotions #9013

---
+++ promotions/model/promotion.js
@@ -0,0 +1,51 @@
+/**
+ * COPYRIGHT INDIGENOUS SOFTWARE, INC., LLC 2017
+ *
+ * All use or reproduction of any or all of this content must be approved.
+ * Please contact info@indigenous.io for approval or questions.
+ */
+
+require('../../models/base.model.js');
+
+var promotion = $$.m.ModelBase.extend({
+
+    defaults: function() {
+        return {
+            _id: null,
+            title: null,
+            description: null,
+            accountId: null,
+            userId : null,
+            attachment: null,
+            startDate: null,
+            expirationDate: null,
+            vendorId: null,
+            image: null,
+            created: {
+                date: new Date(),
+                by: null
+            },
+            modified: {
+                date: null,
+                by: null
+            },
+            _v:"0.1"
+        }
+    },
+
+
+    initialize: function(options) {
+
+    }
+
+}, {
+    db: {
+        storage: "mongo",
+        table: "promotions",
+        idStrategy: "uuid"
+    }
+});
+
+$$.m.Promotion = promotion;
+
+module.exports = promotion;
---
+++ promotions/promotion_manager.js
@@ -0,0 +1,18 @@
+/**
+ * COPYRIGHT INDIGENOUS SOFTWARE, INC., LLC 2017
+ *
+ * All use or reproduction of any or all of this content must be approved.
+ * Please contact info@indigenous.io for approval or questions.
+ */
+
+var promotionDao = require('./dao/promotion.dao.js');
+var log = $$.g.getLogger("promotion_manager");
+
+
+var accountDao = require('../dao/account.dao');
+
+
+module.exports = {
+	
+    
+};
