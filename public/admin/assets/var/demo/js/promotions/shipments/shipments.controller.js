b7d8beac72e423a524c0794de2e6fba01700a842
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-06-30 13:48:48 +0530

    #9230 Customer Details in shipment list

--- public/admin/assets/var/demo/js/promotions/shipments/shipments.controller.js
+++ public/admin/assets/var/demo/js/promotions/shipments/shipments.controller.js
@@ -28,7 +28,7 @@ function shipmentsComponentController($scope, $attrs, $window, $filter, $statePa
     vm.preventClick = preventClick;
     vm.state.promotion = vm.promotion;
     vm.getShipmentStatus = getShipmentStatus;
-
+    vm.stringifyAddress = stringifyAddress;
     function showFilteredRecords(){
         return UtilService.showFilteredRecords(vm.uiState.globalSearch, vm.uiState.fieldSearch);
     }
@@ -115,6 +115,41 @@ function shipmentsComponentController($scope, $attrs, $window, $filter, $statePa
         
     }
 
+    function stringifyAddress(details) {
+        var _firstRow = "";
+        var _middleRow = "";
+        var _bottomRow = "";
+        if (details) {
+            if(details.customerName){
+                _firstRow += details.customerName + '<br>';
+            }
+            if(details.address1 || details.address2)
+            {
+                if(details.address1){
+                    _middleRow +=  details.address1 + " ";     
+                }
+                if(details.address2){
+                    _middleRow += details.address2;    
+                }
+                if(_middleRow.length){
+                    _middleRow += '<br>';  
+                }
+            }
+            if(details.city || details.state || details.zip)
+            {
+                if(details.city){
+                    _bottomRow +=  details.city + ", ";     
+                }
+                if(details.state){
+                    _bottomRow +=  details.state + " ";  
+                }
+                if(details.zip){
+                    _bottomRow +=  details.zip;  
+                }
+            }
+        }
+        return _firstRow + _middleRow + _bottomRow;
+    }
 }
 
 })();
