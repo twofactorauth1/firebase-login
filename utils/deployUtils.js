1b2a0e04ac7d686892278f9f5f2b9d6d2544973a
Kyle Miller <millkyl@gmail.com>
2017-07-05 15:44:43 -0500

    add logging

--- utils/deployUtils.js
+++ utils/deployUtils.js
@@ -79,6 +79,8 @@ var deployUtils = {
                                                 elasticbeanstalk.terminateEnvironment(params, function(err, data) {
                                                     if(err) {
                                                         console.error('Error terminating:', err);
+                                                    } else {
+                                                        console.log('Terminated');
                                                     }
                                                     cb(err);
                                                 });
