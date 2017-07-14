573d103ed18ab8f62a37e21bfc8e8e70ab2af9f9
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-07-14 11:50:23 +0530

    Added quotes to top bar

--- public/admin/assets/var/demo/js/quotes/modals/quote-details-modal.controller.js
+++ public/admin/assets/var/demo/js/quotes/modals/quote-details-modal.controller.js
@@ -1,14 +1,9 @@
 'use strict';
 /*global app*/
-app.controller('QuoteDetailsModalController', ['$scope', '$timeout', 'parentVm', 'toaster', 'SweetAlert', function ($scope, $timeout, parentVm, toaster, SweetAlert) {
+app.controller('QuoteDetailsModalController', ['$scope', '$timeout', 'toaster', 'SweetAlert', function ($scope, $timeout, toaster, SweetAlert) {
 
     var vm = this;
 
-    vm.parentVm = parentVm;
-    
-    vm.state = {
-        orgCardAndPermissions: vm.parentVm.state.orgCardAndPermissions
-    };
     vm.uiState = {
         
     };
