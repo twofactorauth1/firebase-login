a85d9d02ee0ff73842fdc8f799b1135200290ea1
Ashish <ashu8006kumar@gmail.com>
2017-07-19 14:48:54 +0530

    changes reverted #9308 buid failure issue

--- public/admin/assets/js/controllers/contactsCtrl.js
+++ public/admin/assets/js/controllers/contactsCtrl.js
@@ -45,16 +45,7 @@
             column: '',
             details: {}
         };
-        /*
-        filter issue
-        */
-        $scope.filterFn = function(item){
-            if(!($scope.contact.tags instanceof Array)){
-                return true;
-            }
-            var isSelected= $scope.contact.tags.filter(tag=>tag.label == item.label).length > 0;
-            return !isSelected;
-        };
+
 
         /*
          * @getContacts
