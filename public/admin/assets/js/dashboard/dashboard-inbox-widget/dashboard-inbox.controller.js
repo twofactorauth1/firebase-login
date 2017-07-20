cd6384f9ded929dda7ba250c64fbd9d4b1835ab4
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-07-20 14:06:48 +0530

    Securematics > Platform > Broadcast message > Error appear on console, if user navigate to dashboard module after saving the broadcast message #9276

--- public/admin/assets/js/dashboard/dashboard-inbox-widget/dashboard-inbox.controller.js
+++ public/admin/assets/js/dashboard/dashboard-inbox-widget/dashboard-inbox.controller.js
@@ -23,9 +23,12 @@ function dashboardInboxComponentController($scope, $attrs, $filter, $modal, $tim
         if(messages){
             vm.inboxMessages = $filter('orderBy')(messages, '-modified.date');
             vm.setSelectedMessage(0);
-           $timeout(function() {
-               $(".slick-swipe").slick({ adaptiveHeight: true, slidesToShow:1, dots:false, arrows:true, autoplay:false, touchMove:true, draggable:true });
-           }, 500);
+            if(angular.element(".slick-swipe").hasClass('slick-initialized')){
+                angular.element(".slick-swipe").slick("unslick");
+            }
+            $timeout(function() {
+                $(".slick-swipe").slick({ adaptiveHeight: true, slidesToShow:1, dots:false, arrows:true, autoplay:false, touchMove:true, draggable:true });
+            }, 500);
         }
     }, true);
 
