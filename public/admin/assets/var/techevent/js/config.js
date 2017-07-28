d69b53dfe47db6b7426727a9e7f48a580d534e48
Chris Ricci <chris@indigenous.io>
2017-07-28 18:54:48 -0500

    More commits for techevent org

---
+++ public/admin/assets/var/techevent/js/config.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'development',stripeKey:'pk_test_EuZhZHVourE3RaRxELJaYEya',segmentKey:'vVXdSwotRr',googleAnalyticsId:'ga:82461709',googleAnalyticsScope:'ga:pageviews,ga:timeOnPage,ga:exits,ga:avgTimeOnPage,ga:entranceRate,ga:entrances,ga:exitRate,ga:uniquePageviews',googleClientId:'277102651227-q4rq36pdioba0vomtmssv07ojqie046b.apps.googleusercontent.com',googleClientSecret:'sKl01ZtMha3K4BnBygiSQ82g',googleServerKey:'AIzaSyCAkloYlXlZx_---WXevaNHv03ReYpnvLs',leaddyno:'b2a1f6ba361b15f4ce8ad5c36758de951af61a50',facebookClientID:'622171824473460',paypalCheckoutURL:'https://www.sandbox.paypal.com/webapps/adaptivepayment/flow/pay'})
+
+;
