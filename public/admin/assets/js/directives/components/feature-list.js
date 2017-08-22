82e617bdaebed177fdeb6cb3ee1e9144d6a03b48
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-08-22 15:31:10 +0530

    #9658 syntax issue

--- public/admin/assets/js/directives/components/feature-list.js
+++ public/admin/assets/js/directives/components/feature-list.js
@@ -100,7 +100,7 @@ app.directive('featureListComponent',["$window", "$timeout", function ($window, 
             
             var styleString = ' ';
             if (isActive) {
-                var color = var color = $(".list-features-"+ component._id +" li.active .fr-view:last span").css("color");
+                var color = $(".list-features-"+ component._id +" li.active .fr-view:last span").css("color");
                 styleString += 'border-bottom: 1px solid ' + color + ';';
             }
             return styleString;
