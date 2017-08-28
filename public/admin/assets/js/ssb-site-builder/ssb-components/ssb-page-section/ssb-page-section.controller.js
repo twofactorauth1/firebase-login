34dfa597377f93c3f47717bf942ff3cf33c27822
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-08-28 16:40:36 +0530

    #9679 check for image to load

--- public/admin/assets/js/ssb-site-builder/ssb-components/ssb-page-section/ssb-page-section.controller.js
+++ public/admin/assets/js/ssb-site-builder/ssb-components/ssb-page-section/ssb-page-section.controller.js
@@ -860,23 +860,28 @@ function ssbPageSectionController($scope, $attrs, $filter, $transclude, $sce, $t
             }, 0);
         }
 
-        if(vm.uiState && vm.section && vm.section.bg && vm.section.bg.img && vm.section.bg.img.parallax && vm.section.bg.img.url){
-            var isLoaded = false;
+        if(vm.uiState && vm.section){
+            
             var unbindWatcher = $scope.$watch(function() {
-                return angular.element("#px-ele-"+ vm.section._id).length
+                return angular.element(".parallax").length
             }, function(newValue, oldValue) {
-                if (newValue && newValue > 0 && !isLoaded) {
-                    isLoaded = true;
-                    var src = vm.section.bg.img.url;
-                    if(src){
-                        var image = new Image();
-                        image.onload = function() {
-                            console.log("image loaded")
-                            $timeout(function() {
-                                $scope.$broadcast('parallaxCall', {});    
-                            }, 0);
-                        };
-                        image.src = src; 
+                if (newValue && newValue > 0 && !vm.uiState.backgroundImagesLoaded) {
+                    var elem = angular.element(".parallax").first();
+                    vm.uiState.backgroundImagesLoaded = true;
+                    var bg = elem.css('background-image');
+                    if (bg) {
+                        var src = bg.replace(/(^url\()|(\)$|[\"\'])/g, '');
+                        if(src){
+                            var image = new Image();
+                            image.onload = function() {
+                                console.log("image loaded")
+                                $scope.$broadcast('parallaxCall', {});
+                                $timeout(function() {
+                                    $scope.$broadcast('parallaxCall', {});    
+                                }, 1000);
+                            };
+                            image.src = src; 
+                        }
                     }
 
                     unbindWatcher();
