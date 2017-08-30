d1f1ea07cdb1bec3a450838ed2fc6635a6f903d6
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-08-30 16:17:11 +0530

    #9720 added grunt task to update slider images

--- utils/dbcopyutil.js
+++ utils/dbcopyutil.js
@@ -826,6 +826,36 @@ var copyutil = {
             });
         });
     },
+
+    updateThumbnailSliderImages :function(fn) {
+        var srcURL = mongoConfig.TEST_MONGODB_CONNECT;
+        //var srcURL = mongoConfig.PROD_MONGODB_CONNECT
+        var srcMongo = mongoskin.db(srcURL, {safe: true});
+
+        var sections = srcMongo.collection('sections');
+
+        sections.find({'components.type':'thumbnail-slider', accountId: {$ne:0},  latest: {$ne:false}}).toArray(function(err, sectionArry){
+            async.each(sectionArry, function(section, cb){
+                if(section.components && section.components.length && section.components[0].type == 'thumbnail-slider' && section.components[0].thumbnailCollection){                    
+                    _.each(section.components[0].thumbnailCollection, function(image){
+                        if(!image.img){
+                            image.img = "<img src=\"" + image.url + "\">";
+                        }
+                    })
+                    sections.save(section, function(){
+                        cb();
+                    });
+                }
+                else{
+                    cb();
+                }
+            }, function done(){
+                console.log('done');
+                fn();
+            });
+        });
+    },
+
     
     _updateBlogPages :function(fn) {
 
