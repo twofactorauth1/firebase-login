6b2a420e7c38cc1ec0c4b605b9609c86dd237a3d
Ashish <ashu8006kumar@gmail.com>
2017-09-04 15:13:26 +0530

    #9741 all directives expect components

--- public/scripts/directives/externalScripts.js
+++ public/scripts/directives/externalScripts.js
@@ -1,33 +1,32 @@
+/*global angular */
 angular.module('mainApp')
-    .directive('externalScripts', ['$sce', 'externalScriptLookup',
+	.directive('externalScripts', ['$sce', 'externalScriptLookup',
         function ($sce, externalScriptLookup) {
-            return {
-                restrict: 'E',
-                replace: true,
-                template: '<span ng-bind-html="externalScripts"></span>',
-                link: function (scope, elem, attr) {
-                    scope.externalScripts = '';
-                    var scriptList = [];
-                    var scriptLookup = externalScriptLookup;
+			'use strict';
+			return {
+				restrict: 'E',
+				replace: true,
+				template: '<span ng-bind-html="externalScripts"></span>',
+				link: function (scope) {
+					scope.externalScripts = '';
+					var scriptList = [],
+						scriptLookup = externalScriptLookup;
 
-                    scope.$on('external.scripts.page.data', function (event, args) {
-                        var page = args.page;
-                        if(page){
-                            var componentTypes = _.uniq(_.pluck(_.flatten(_.pluck(page.sections, 'components')), 'type'));
-                            componentTypes.forEach(function (c, i) {
-                                for (var k in scriptLookup) {
-                                    if ((scriptLookup[k].indexOf(c) > -1) && (scriptList.indexOf(k) === -1)) {
-                                        scriptList.push(k);
-                                        scope.externalScripts += '\n\n' + k;
-                                        scope.externalScripts = $sce.trustAsHtml(scope.externalScripts);
-                                    }
-                                }
-                            });
-                        }
-                        
-
-                    });
-                }
-            };
+					scope.$on('external.scripts.page.data', function (event, args) {
+						var page = args.page;
+						if (page) {
+							_.uniq(_.pluck(_.flatten(_.pluck(page.sections, 'components')), 'type')).forEach(function (c) {
+								for (var k in scriptLookup) {
+									if ((scriptLookup[k].indexOf(c) > -1) && (scriptList.indexOf(k) === -1)) {
+										scriptList.push(k);
+										scope.externalScripts += '\n\n' + k;
+										scope.externalScripts = $sce.trustAsHtml(scope.externalScripts);
+									}
+								}
+							});
+						}
+					});
+				}
+			};
         }
     ]);
