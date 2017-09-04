a70131b333a467b3f4c0136de86211572fe019b1
Ashish <ashu8006kumar@gmail.com>
2017-09-04 15:58:16 +0530

    missing files

--- public/scripts/directives/components/navigation.js
+++ public/scripts/directives/components/navigation.js
@@ -1,75 +1,82 @@
+/*global app, angular, window, $ */
+/*jslint unparam:true*/
 app.directive('navigationComponent', ['websiteService', 'accountService', '$timeout', function (websiteService, accountService, $timeout) {
-    return {
-        scope: {
-            component: '='
-        },
-        templateUrl: '/components/component-wrap.html',
-        controller: function ($scope, websiteService, accountService, SsbPageSectionService, $compile) {
-            $scope.navbarId = _.random(0, 1000);
-            $scope.website = {};
-            if(window.indigenous && window.indigenous.precache && window.indigenous.precache.siteData && window.indigenous.precache.siteData.linkList){
-                $scope.website.linkLists = window.indigenous.precache.siteData.linkList;               
-            }
-            
-            if (!angular.isDefined($scope.component.shownavbox)) {
-                $scope.component.shownavbox = true;
-            }
-            if(!$scope.website.linkLists){
-                websiteService(function (err, website) {
-                    $scope.website = website;
-                    $timeout(function () {
-                        $(window).trigger('resize');
-                    }, 0);
-                });
-            }
-            
-            accountService(function (err, account) {
-                $scope.account = account;
-            });
-            var args = {};
-            $scope.$emit('getCurrentPage', args);
-            $scope.currentpage = args.currentpage;
-            // Special case for blogs
-            if($scope.currentpage && ($scope.currentpage.handle=== 'blog-list' || $scope.currentpage.handle=== 'blog-post')){
-                $scope.currentpage.handle = 'blog';
-            }
-            $scope.toggleNavClass=function(ele){
-                var li=$(ele.target).parents("li");
-                if(li){
-                    if(!li.hasClass("nav-active")){
-                        li.parents("section").addClass("overflow_visible");
-                        li.siblings().removeClass("nav-active");
-                        li.addClass("nav-active");
-                    }else{
-                        li.removeClass("nav-active");
-                        li.parents("section").removeClass("overflow_visible");
-                    }
-                }
-            }
-            $scope.$watchCollection(function() { return SsbPageSectionService.offset }, function(offset) {
-                $scope.scrollOffsetTop = offset;
-            });
+	'use strict';
+	return {
+		scope: {
+			component: '='
+		},
+		templateUrl: '/components/component-wrap.html',
+		controller: function ($scope, websiteService, accountService, SsbPageSectionService) {
+			$scope.navbarId = _.random(0, 1000);
+			$scope.website = {};
+			if (window.indigenous && window.indigenous.precache && window.indigenous.precache.siteData && window.indigenous.precache.siteData.linkList) {
+				$scope.website.linkLists = window.indigenous.precache.siteData.linkList;
+				$timeout(function () {
+					$(window).trigger('resize');
+				}, 0);
+			}
 
-            var pageList = {};
-            if(window.indigenous && window.indigenous.precache && window.indigenous.precache.siteData && window.indigenous.precache.siteData.pages){
-                pageList = window.indigenous.precache.siteData.pages;
-            }
+			if (!angular.isDefined($scope.component.shownavbox)) {
+				$scope.component.shownavbox = true;
+			}
+			if (!$scope.website.linkLists) {
+				websiteService(function (err, website) {
+					$scope.website = website;
+					$timeout(function () {
+						$(window).trigger('resize');
+					}, 0);
+				});
+			}
 
-            $scope.checkIfReloadPage = function(link, type){
-                if(link){
-                    if(link === 'blog'){
-                        return true;
-                    }
-                    else{
-                        if(pageList && pageList[link]){
-                            if(pageList[link].isBlogCopy){
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
+			accountService(function (err, account) {
+				$scope.account = account;
+			});
+			var args = {},
+				pageList = {};
+			$scope.$emit('getCurrentPage', args);
+			$scope.currentpage = args.currentpage;
+			// Special case for blogs
+			if ($scope.currentpage && ($scope.currentpage.handle === 'blog-list' || $scope.currentpage.handle === 'blog-post')) {
+				$scope.currentpage.handle = 'blog';
+			}
+			$scope.toggleNavClass = function (ele) {
+				var li = $(ele.target).parents("li");
+				if (li) {
+					if (!li.hasClass("nav-active")) {
+						li.parents("section").addClass("overflow_visible");
+						li.siblings().removeClass("nav-active");
+						li.addClass("nav-active");
+					} else {
+						li.removeClass("nav-active");
+						li.parents("section").removeClass("overflow_visible");
+					}
+				}
+			};
+			$scope.$watchCollection(function () {
+				return SsbPageSectionService.offset;
+			}, function (offset) {
+				$scope.scrollOffsetTop = offset;
+			});
+			if (window.indigenous && window.indigenous.precache && window.indigenous.precache.siteData && window.indigenous.precache.siteData.pages) {
+				pageList = window.indigenous.precache.siteData.pages;
+			}
 
-        }
-    }
+			$scope.checkIfReloadPage = function (link) {
+				if (link && link.data) {
+					if (link.data === 'blog') {
+						return true;
+					} else {
+						if (pageList && pageList[link.data]) {
+							if (pageList[link.data].isBlogCopy) {
+								return true;
+							}
+						}
+					}
+				}
+				return false;
+			};
+
+		}
+	};
 }]);
