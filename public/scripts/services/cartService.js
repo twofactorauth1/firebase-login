9950f3a3bcf73afa20c474a863514d2ad51c2e9e
Ashish <ashu8006kumar@gmail.com>
2017-09-05 11:38:34 +0530

    #9741 frontend services

--- public/scripts/services/cartService.js
+++ public/scripts/services/cartService.js
@@ -1,186 +1,194 @@
+/*global mainApp  , angular */
+/* eslint-disable no-console */
 (function () {
-    mainApp.factory('CartDetailsService', CartDetailsService);
-    CartDetailsService.$inject = ['nonShippingChargeStates'];
-    function CartDetailsService(nonShippingChargeStates){
-        var cartService = {};
+
+	'use strict';
 
 
-        cartService.getCartItems = getCartItems;
-        
-        cartService.addItemToCart = addItemToCart;
-
-        cartService.calculateTotalCharges = calculateTotalCharges;
-
-        cartService.checkOnSale = checkOnSale;
-
-        cartService.removeItemFromCart = removeItemFromCart;
-
-        cartService.checkIfStateTaxable = checkIfStateTaxable;
-
-        cartService.productViewLoaded = false;
-
-        cartService.total = 0;
-        cartService.cartTax = 0;
-        cartService.subTotal = 0;
-        cartService.showTax = false;
-        cartService.taxPercent = 0;
-        cartService.showNotTaxed = false;
-        cartService.hasSubscriptionProduct = false;
-        cartService.shippingTax = 0;
-        cartService.totalTax = 0;
-        cartService.totalDiscount = 0;
-        cartService.commerceSettings = {};
-        cartService.isStateTaxable = false;
+	function CartDetailsService(nonShippingChargeStates) {
+		var cartService = {};
 
 
-        function getCartItems() {
-            return cartService.items;
-        }       
+		cartService.getCartItems = getCartItems;
 
-        function addItemToCart(item, _discount, percent_off) {
-            cartService.items.push(item);
+		cartService.addItemToCart = addItemToCart;
 
-            cartService.items.forEach(function (item, index) {
-                if (item.type == "SUBSCRIPTION") {
-                    cartService.hasSubscriptionProduct = true;
-                }
-            });
-            calculateTotalCharges(_discount, percent_off);
-        }
+		cartService.calculateTotalCharges = calculateTotalCharges;
 
-        function removeItemFromCart(product, _discount, percent_off) {
-            var filteredItems = _.filter(cartService.items, function(item) {
-                return item._id !== product._id;
-            });
-            cartService.items = filteredItems;
-            cartService.hasSubscriptionProduct = false;
-            cartService.items.forEach(function (item, index) {
-                if (item.type == "SUBSCRIPTION") {
-                    cartService.hasSubscriptionProduct = true;
-                }
-            });
-            calculateTotalCharges(_discount, percent_off);
-        }
+		cartService.checkOnSale = checkOnSale;
 
-        function calculateTotalCharges(_discount, percent_off) {
-            if(!angular.isDefined(_discount)){
-                _discount = 0;
-            }
-            if(!angular.isDefined(percent_off)){
-                percent_off = false;
-            }
+		cartService.removeItemFromCart = removeItemFromCart;
 
-            var _subTotal = 0;
-            var _cartTax = 0;
-            var _shippingTax = 0;
-            var _totalTax = 0;
-            var _subcartTaxable = 0;
-            var _overrides  = 0;
-            var _nonOverrides = 0;
-            var _totalShippingCharges = 0;
-            // Shipping Charges condtions and calculations
-            var shipping = {
-                chargeType: null,
-                charge: null
-            };
+		cartService.checkIfStateTaxable = checkIfStateTaxable;
 
-            if(cartService.commerceSettings){
-                if(cartService.commerceSettings.shipping && cartService.commerceSettings.shipping.enabled){                    
-                    shipping.chargeType = cartService.commerceSettings.shipping.chargeType;
-                    shipping.charge = cartService.commerceSettings.shipping.charge || 0;                  
-                }
-            }
+		cartService.productViewLoaded = false;
 
-            _.each(cartService.items, function(item){
-                var _price = item.regular_price;
-                if (checkOnSale(item)) {
-                    _price = item.sale_price
-                }
-                _subTotal = parseFloat(_subTotal) + (parseFloat(_price) * item.quantity);
-                if (item.taxable && cartService.showTax) {
-                    _subcartTaxable += _price * item.quantity;
-                }
+		cartService.total = 0;
+		cartService.cartTax = 0;
+		cartService.subTotal = 0;
+		cartService.showTax = false;
+		cartService.taxPercent = 0;
+		cartService.showNotTaxed = false;
+		cartService.hasSubscriptionProduct = false;
+		cartService.shippingTax = 0;
+		cartService.totalTax = 0;
+		cartService.totalDiscount = 0;
+		cartService.commerceSettings = {};
+		cartService.isStateTaxable = false;
 
-                if (!item.taxable) {
-                    cartService.showNotTaxed = true;
-                }
 
-                if(shipping.chargeType){    
-                    if(shipping.chargeType === 'item'){
-                        if(item.shipping_charges && item.shipping_charges.item_override_charge){
-                            _overrides += item.quantity * item.shipping_charges.item_override_charge;
-                        } else {
-                            _nonOverrides = _nonOverrides += item.quantity * parseFloat(shipping.charge);
-                        }
-                    } else if(shipping.chargeType === 'order' && item.shipping_charges && item.shipping_charges.item_order_additive_charge){
-                        _overrides += item.quantity * item.shipping_charges.item_order_additive_charge;
-                    }
-                }
-            });
+		function getCartItems() {
+			return cartService.items;
+		}
 
-            if(shipping.chargeType === 'order'){
-                _totalShippingCharges = _overrides + parseFloat(shipping.charge);
-            } else if(shipping.chargeType === 'item'){
-                _totalShippingCharges = _overrides + _nonOverrides;
-            }
+		function addItemToCart(item, _discount, percent_off) {
+			cartService.items.push(item);
 
-            if(_discount && !percent_off){
-                _discount = _discount / 100;
-            } else if(_discount && percent_off){
-                _discount = (_subTotal) * _discount / 100;
-            }
+			cartService.items.forEach(function (item) {
+				if (item.type === "SUBSCRIPTION") {
+					cartService.hasSubscriptionProduct = true;
+				}
+			});
+			calculateTotalCharges(_discount, percent_off);
+		}
 
-            if (_subcartTaxable > 0) {
-                _cartTax = (_subcartTaxable - _discount) * parseFloat(cartService.taxPercent) / 100;
-            }
-            if(_cartTax < 0)
-                _cartTax = 0;
-            
-            if(cartService.isStateTaxable){
-                _shippingTax = _totalShippingCharges * parseFloat(cartService.taxPercent) / 100;
-            }
+		function removeItemFromCart(product, _discount, percent_off) {
+			var filteredItems = _.filter(cartService.items, function (item) {
+				return item._id !== product._id;
+			});
+			cartService.items = filteredItems;
+			cartService.hasSubscriptionProduct = false;
+			cartService.items.forEach(function (item) {
+				if (item.type === "SUBSCRIPTION") {
+					cartService.hasSubscriptionProduct = true;
+				}
+			});
+			calculateTotalCharges(_discount, percent_off);
+		}
 
-            _totalTax = _cartTax + _shippingTax;
-            cartService.subTotal = _subTotal;
-            cartService.cartTax = _cartTax;
-            cartService.shippingTax = _shippingTax;
-            cartService.totalDiscount = _discount;
-            cartService.totalShipping = _totalShippingCharges;
-            cartService.totalTax = _totalTax;
-            cartService.total = (_subTotal + _totalTax + _totalShippingCharges - _discount > 0 ? _subTotal + _totalTax + _totalShippingCharges - _discount : 0);            
-        }
+		function calculateTotalCharges(_discount, percent_off) {
+			if (!angular.isDefined(_discount)) {
+				_discount = 0;
+			}
+			if (!angular.isDefined(percent_off)) {
+				percent_off = false;
+			}
 
-        function checkOnSale(_product) {
-            if (_product.on_sale) {
-                if (_product.sale_date_from && _product.sale_date_to) {
-                    var date = new Date();
-                    var startDate = new Date(_product.sale_date_from);
-                    var endDate = new Date(_product.sale_date_to);
-                    if (startDate <= date && date <= endDate) {
-                        return true; //false in this case
-                    }
-                    return false;
-                }
-                return true;
-            }
-        }
+			var _subTotal = 0,
+				_cartTax = 0,
+				_shippingTax = 0,
+				_totalTax = 0,
+				_subcartTaxable = 0,
+				_overrides = 0,
+				_nonOverrides = 0,
+				_totalShippingCharges = 0,
+				// Shipping Charges condtions and calculations
+				shipping = {
+					chargeType: null,
+					charge: null
+				};
 
-        function checkIfStateTaxable(_state, _discount, percent_off){
-            if(_state && cartService.commerceSettings && cartService.commerceSettings.shipping && cartService.commerceSettings.shipping.enabled){
-                var stateExists = _.filter(nonShippingChargeStates, function(stateObj){ return stateObj.abbr === _state; });
-                if(stateExists && stateExists.length){
-                    cartService.isStateTaxable = false;
-                } else{
-                    cartService.isStateTaxable = true;
-                }
-                calculateTotalCharges(_discount, percent_off);
-            }
-            
-        }
-        
-        (function init() {
-            cartService.items = [];
-        })();
-    return cartService;       
-    }
-})();
+			if (cartService.commerceSettings) {
+				if (cartService.commerceSettings.shipping && cartService.commerceSettings.shipping.enabled) {
+					shipping.chargeType = cartService.commerceSettings.shipping.chargeType;
+					shipping.charge = cartService.commerceSettings.shipping.charge || 0;
+				}
+			}
+
+			_.each(cartService.items, function (item) {
+				var _price = item.regular_price;
+				if (checkOnSale(item)) {
+					_price = item.sale_price;
+				}
+				_subTotal = parseFloat(_subTotal) + (parseFloat(_price) * item.quantity);
+				if (item.taxable && cartService.showTax) {
+					_subcartTaxable += _price * item.quantity;
+				}
+
+				if (!item.taxable) {
+					cartService.showNotTaxed = true;
+				}
+
+				if (shipping.chargeType) {
+					if (shipping.chargeType === 'item') {
+						if (item.shipping_charges && item.shipping_charges.item_override_charge) {
+							_overrides += item.quantity * item.shipping_charges.item_override_charge;
+						} else {
+							_nonOverrides = _nonOverrides += item.quantity * parseFloat(shipping.charge);
+						}
+					} else if (shipping.chargeType === 'order' && item.shipping_charges && item.shipping_charges.item_order_additive_charge) {
+						_overrides += item.quantity * item.shipping_charges.item_order_additive_charge;
+					}
+				}
+			});
+
+			if (shipping.chargeType === 'order') {
+				_totalShippingCharges = _overrides + parseFloat(shipping.charge);
+			} else if (shipping.chargeType === 'item') {
+				_totalShippingCharges = _overrides + _nonOverrides;
+			}
+
+			if (_discount && !percent_off) {
+				_discount = _discount / 100;
+			} else if (_discount && percent_off) {
+				_discount = (_subTotal) * _discount / 100;
+			}
+
+			if (_subcartTaxable > 0) {
+				_cartTax = (_subcartTaxable - _discount) * parseFloat(cartService.taxPercent) / 100;
+			}
+			if (_cartTax < 0)
+				_cartTax = 0;
+
+			if (cartService.isStateTaxable) {
+				_shippingTax = _totalShippingCharges * parseFloat(cartService.taxPercent) / 100;
+			}
+
+			_totalTax = _cartTax + _shippingTax;
+			cartService.subTotal = _subTotal;
+			cartService.cartTax = _cartTax;
+			cartService.shippingTax = _shippingTax;
+			cartService.totalDiscount = _discount;
+			cartService.totalShipping = _totalShippingCharges;
+			cartService.totalTax = _totalTax;
+			cartService.total = (_subTotal + _totalTax + _totalShippingCharges - _discount > 0 ? _subTotal + _totalTax + _totalShippingCharges - _discount : 0);
+		}
+
+		function checkOnSale(_product) {
+			if (_product.on_sale) {
+				if (_product.sale_date_from && _product.sale_date_to) {
+					var date = new Date();
+					var startDate = new Date(_product.sale_date_from);
+					var endDate = new Date(_product.sale_date_to);
+					if (startDate <= date && date <= endDate) {
+						return true; //false in this case
+					}
+					return false;
+				}
+				return true;
+			}
+		}
+
+		function checkIfStateTaxable(_state, _discount, percent_off) {
+			if (_state && cartService.commerceSettings && cartService.commerceSettings.shipping && cartService.commerceSettings.shipping.enabled) {
+				var stateExists = _.filter(nonShippingChargeStates, function (stateObj) {
+					return stateObj.abbr === _state;
+				});
+				if (stateExists && stateExists.length) {
+					cartService.isStateTaxable = false;
+				} else {
+					cartService.isStateTaxable = true;
+				}
+				calculateTotalCharges(_discount, percent_off);
+			}
+
+		}
+
+		(function init() {
+			cartService.items = [];
+		})();
+		return cartService;
+	}
+	mainApp.factory('CartDetailsService', CartDetailsService);
+	CartDetailsService.$inject = ['nonShippingChargeStates'];
+}());
--- public/scripts/services/orderService.js
+++ public/scripts/services/orderService.js
@@ -1,89 +1,91 @@
-/*global mainApp, moment, angular*/
+/*global mainApp, angular, console*/
 /*jslint unparam:true*/
-'use strict';
+/* eslint-disable no-console */
 mainApp.service('orderService', function ($http) {
-    var baseUrl = '/api/1.0/';
-    this.createOrder = function (order, fn) {
-        var apiUrl = baseUrl + ['orders'].join('/');
-        $http({
-            url: apiUrl,
-            method: "POST",
-            data: angular.toJson(order)
-        }).success(function (data, status, headers, config) {
-            fn(data);
-        }).error(function (err) {
-            console.log('END:Create Order with ERROR');
-            fn(err);
-        });
-    };
 
-    this.createPaypalOrder = function (order, fn) {
-        var apiUrl = baseUrl + ['orders', 'payment', 'paypal'].join('/');
-        $http({
-            url: apiUrl,
-            method: "POST",
-            data: angular.toJson(order)
-        }).success(function (data, status, headers, config) {
-            fn(data);
-        }).error(function (err) {
-            console.log('END:Create Paypal Order with ERROR');
-            fn(err);
-        });
-    };
+	'use strict';
+	var baseUrl = '/api/1.0/';
+	this.createOrder = function (order, fn) {
+		var apiUrl = baseUrl + ['orders'].join('/');
+		$http({
+			url: apiUrl,
+			method: "POST",
+			data: angular.toJson(order)
+		}).success(function (data) {
+			fn(data);
+		}).error(function (err) {
+			console.log('END:Create Order with ERROR');
+			fn(err);
+		});
+	};
 
-    this.getOrder = function (orderId, fn) {
-        var apiUrl = baseUrl + ['orders', orderId].join('/');
-        $http({
-            url: apiUrl,
-            method: "GET"
-        }).success(function (data, status, headers, config) {
-            fn(data);
-        }).error(function (err) {
-            console.log('END:Got Order with ERROR');
-            fn(err);
-        });
-    };
+	this.createPaypalOrder = function (order, fn) {
+		var apiUrl = baseUrl + ['orders', 'payment', 'paypal'].join('/');
+		$http({
+			url: apiUrl,
+			method: "POST",
+			data: angular.toJson(order)
+		}).success(function (data) {
+			fn(data);
+		}).error(function (err) {
+			console.log('END:Create Paypal Order with ERROR');
+			fn(err);
+		});
+	};
 
-    this.setOrderPaid = function (order, fn) {
-        var apiUrl = baseUrl + ['orders', order._id, 'paid'].join('/');
-        $http({
-            url: apiUrl,
-            method: "POST",
-            data: angular.toJson(order)
-        }).success(function (data, status, headers, config) {
-            fn(data);
-        }).error(function (err) {
-            console.log('END:Create Paypal Order with ERROR');
-            fn(err);
-        });
-    };
+	this.getOrder = function (orderId, fn) {
+		var apiUrl = baseUrl + ['orders', orderId].join('/');
+		$http({
+			url: apiUrl,
+			method: "GET"
+		}).success(function (data) {
+			fn(data);
+		}).error(function (err) {
+			console.log('END:Got Order with ERROR');
+			fn(err);
+		});
+	};
 
-    this.deletePaypalOrder = function (order, fn) {
-        var apiUrl = baseUrl + ['orders', order._id, 'paypal'].join('/');
-        apiUrl = apiUrl + '?payKey=' + order.payment_details.payKey;
-        $http({
-            url: apiUrl,
-            method: "DELETE"
-        }).success(function (data, status, headers, config) {
-            fn(data);
-        }).error(function (err) {
-            console.log('END:Got Order with ERROR');
-            fn(err);
-        });
-    };
+	this.setOrderPaid = function (order, fn) {
+		var apiUrl = baseUrl + ['orders', order._id, 'paid'].join('/');
+		$http({
+			url: apiUrl,
+			method: "POST",
+			data: angular.toJson(order)
+		}).success(function (data) {
+			fn(data);
+		}).error(function (err) {
+			console.log('END:Create Paypal Order with ERROR');
+			fn(err);
+		});
+	};
 
-    this.getEstimatedTax = function(order, fn) {
-        var apiUrl = baseUrl + ['orders', 'estimatedTax'].join('/');
-        $http({
-            url: apiUrl,
-            method: "POST",
-            data: angular.toJson(order)
-        }).success(function (data, status, headers, config) {
-            fn(data);
-        }).error(function (err) {
-            console.log('END:get estimated tax with ERROR');
-            fn(err);
-        });
-    };
+	this.deletePaypalOrder = function (order, fn) {
+		var apiUrl = baseUrl + ['orders', order._id, 'paypal'].join('/');
+		apiUrl = apiUrl + '?payKey=' + order.payment_details.payKey;
+		$http({
+			url: apiUrl,
+			method: "DELETE"
+		}).success(function (data) {
+			fn(data);
+		}).error(function (err) {
+			console.log('END:Got Order with ERROR');
+			fn(err);
+		});
+	};
+
+	this.getEstimatedTax = function (order, fn) {
+		var apiUrl = baseUrl + ['orders', 'estimatedTax'].join('/');
+		$http({
+			url: apiUrl,
+			method: "POST",
+			data: angular.toJson(order)
+		}).success(function (data) {
+			fn(data);
+		}).error(function (err) {
+			console.log('END:get estimated tax with ERROR');
+			fn(err);
+		});
+	};
 
 });
