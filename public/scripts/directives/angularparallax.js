515c9c854d541af7b6b0c4a8b9d9e9124ee22ead
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-09-05 18:44:05 +0530

    remove linting from angular parallax

--- public/scripts/directives/angularparallax.js
+++ public/scripts/directives/angularparallax.js
@@ -1,106 +1,108 @@
-/*global angular ,$ ,navigator ,document , window */
+'use strict';
+
 angular.module('angular-parallax', [
-]).directive('parallax', ['$window', function ($window) {
-	'use strict';
-	return {
-		restrict: 'A',
-		scope: {
-			parallaxRatio: '@',
-			parallaxVerticalOffset: '@',
-			parallaxHorizontalOffset: '@'
-		},
-		link: function ($scope, elem) {
-			var setPosition = function () {
-				// horizontal positioning
-				elem.css('left', $scope.parallaxHorizontalOffset + "px");
-				var calcValY = $window.pageYOffset * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1);
-				if (calcValY <= $window.innerHeight) {
-					var topVal = (calcValY < $scope.parallaxVerticalOffset ? $scope.parallaxVerticalOffset : calcValY);
-					elem.css('top', topVal + "px");
-				}
-			};
+]).directive('parallax', ['$window', function($window) {
+    return {
+        restrict: 'A',
+        scope: {
+            parallaxRatio: '@',
+            parallaxVerticalOffset: '@',
+            parallaxHorizontalOffset: '@',
+        },
+        link: function($scope, elem, $attrs) {
+            var setPosition = function () {
+                // horizontal positioning
+                elem.css('left', $scope.parallaxHorizontalOffset + "px");
+                var calcValY = $window.pageYOffset * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1 );
+                if (calcValY <= $window.innerHeight) {
+                    var topVal = (calcValY < $scope.parallaxVerticalOffset ? $scope.parallaxVerticalOffset : calcValY);
+                    elem.css('top', topVal + "px");
+                }
+            };
 
-			setPosition();
+            setPosition();
 
-			angular.element($window).bind("scroll", setPosition);
-			angular.element($window).bind("touchmove", setPosition);
-		} // link function
-	};
-}]).directive('parallaxBackground', ['$window', '$timeout', function ($window, $timeout) {
-	return {
-		restrict: 'A',
-		transclude: true,
-		template: '<div ng-transclude></div>',
-		scope: {
-			parallaxRatio: '@',
-			parallaxVerticalOffset: '@',
-			parallaxXPosition: '@'
-		},
-		link: function ($scope, elem) {
+            angular.element($window).bind("scroll", setPosition);
+            angular.element($window).bind("touchmove", setPosition);
+        }  // link function
+    };
+}]).directive('parallaxBackground', ['$window', '$timeout', function($window, $timeout) {
+    return {
+        restrict: 'A',
+        transclude: true,
+        template: '<div ng-transclude></div>',
+        scope: {
+            parallaxRatio: '@',
+            parallaxVerticalOffset: '@',
+            parallaxXPosition: '@'
+        },
+        link: function($scope, elem, attrs) {
+            var initialRun = true;
+            elem.addClass('opacity-0');
+            var setPosition = function () {
+                // Fix for smaller resolutions
+                var calcValY = 0;
+                var calcValX = "50%";
+                if($scope.parallaxXPosition) {
+                    if($scope.parallaxXPosition == 'left') {
+                        calcValX = "0%";
+                    }
+                    if($scope.parallaxXPosition == 'center') {
+                        calcValX = "50%";
+                    }
+                    if($scope.parallaxXPosition == 'right') {
+                        calcValX = "100%";
+                    }
+                }
+                if(elem.hasClass("parallax")){
+                    var win_width = $(window).width();
+                    var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
+                    if(win_width < 750 || iOS) {
+                        $scope.parallaxRatio = 0.02;
+                    }
+                    var yoffset = 0;
+                    if (initialRun && $window.pageYOffset == 0){
+                        // yoffset = 10;
+                    }
+                    calcValY = (pos(elem[0]) - $window.pageYOffset) * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1 );
+                    calcValY = calcValY - yoffset;
+                    elem.css('background-position', calcValX + " " + calcValY + "px");
+                    elem.css('background-attachment', "fixed");
+                } else {
+                    elem.css('background-position', calcValX + " " + calcValY + "px");
+                    elem.css('background-attachment', "inherit");
+                }
 
-			elem.addClass('opacity-0');
-			var initialRun = true,
-				pos = function (obj) {
-					//curLeft = 0, not used
-					var curTop = 0;
-					if (obj.offsetParent) {
-						do {
-							//curLeft += obj.offsetLeft;
-							curTop += obj.offsetTop;
-						} while (obj == obj.offsetParent);
-					}
-					return curTop;
-				},
-				setPosition = function () {
-					// Fix for smaller resolutions
-					var calcValY = 0,
-						calcValX = "50%";
-					if ($scope.parallaxXPosition) {
-						if ($scope.parallaxXPosition === 'left') {
-							calcValX = "0%";
-						}
-						if ($scope.parallaxXPosition === 'center') {
-							calcValX = "50%";
-						}
-						if ($scope.parallaxXPosition === 'right') {
-							calcValX = "100%";
-						}
-					}
-					if (elem.hasClass("parallax")) {
-						var win_width = $(window).width(),
-							yoffset = 0,
-							iOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false);
-						if (win_width < 750 || iOS) {
-							$scope.parallaxRatio = 0.02;
-						}
-						/*if (initialRun && $window.pageYOffset === 0) {
-							// yoffset = 10;
-						}*/
-						calcValY = (pos(elem[0]) - $window.pageYOffset) * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1);
-						calcValY = calcValY - yoffset;
-						elem.css('background-position', calcValX + " " + calcValY + "px");
-						elem.css('background-attachment', "fixed");
-					} else {
-						elem.css('background-position', calcValX + " " + calcValY + "px");
-						elem.css('background-attachment', "inherit");
-					}
+                if (initialRun) {
+                    $timeout(function() {
+                        elem.removeClass('opacity-0');
+                    }, 0);
+                    initialRun = false;
+                }
 
-					if (initialRun) {
-						$timeout(function () {
-							elem.removeClass('opacity-0');
-						}, 0);
-						initialRun = false;
-					}
+            };
 
-				};
-			// set our initial position - fixes webkit background render bug
-			$(document).ready(function () {
-				$timeout(function () {
-					setPosition();
-				}, 0);
-			});
-			angular.element($window).bind("scroll", setPosition);
-			angular.element($window).bind("touchmove", setPosition);
-		} // link function
-	};
+            var pos = function(obj){
+                var curLeft  = 0;
+                var curTop = 0;
+                if (obj.offsetParent){
+                    do {
+                        curLeft += obj.offsetLeft;
+                        curTop += obj.offsetTop;
+                    } while(obj = obj.offsetParent);
+                }
+                return curTop;
+            };
+
+            // set our initial position - fixes webkit background render bug
+            $(document).ready(function() {
+                $timeout(function() {
+                    setPosition();
+                }, 0);
+            })
+
+            angular.element($window).bind("scroll", setPosition);
+            angular.element($window).bind("touchmove", setPosition);
+        }  // link function
+    };
 }]);
