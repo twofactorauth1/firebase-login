0b6f73560842fcb31db931633243d03a1b996226
Ashish <ashu8006kumar@gmail.com>
2017-09-06 13:21:30 +0530

    utils and configs #9741

--- public/js/scripts/utils.js
+++ public/js/scripts/utils.js
@@ -1,21 +1,28 @@
+/*global indigenous, window ,analytics, console ,$ */
 indigenous = window.indigenous || {};
 indigenous.utils = window.indigenous.utils || {};
 
-indigenous.utils.trackEvent = function(eventName, metadata) {
-    analytics.track(eventName, metadata);
-    //console.log('track subscribe.');
-    return true;
+indigenous.utils.trackEvent = function (eventName, metadata) {
+	"use strict";
+	analytics.track(eventName, metadata);
+	//console.log('track subscribe.');
+	return true;
 };
 
-indigenous.utils.trackSubscriptionEvent = function() {
-    console.log('trackSubscriptionEvent');
-    var eventName = 'subscribed';
-    var emailAddress = $('input[name="EMAIL"]').val();
-    var metadata = {'email': emailAddress};
-    indigenous.utils.trackEvent(eventName, metadata);
-    if(window.analytics) {
-        analytics.identify({email:emailAddress});
-    }
+indigenous.utils.trackSubscriptionEvent = function () {
+	"use strict";
+	console.log('trackSubscriptionEvent');
+	var eventName = 'subscribed',
+		emailAddress = $('input[name="EMAIL"]').val(),
+		metadata = {
+			'email': emailAddress
+		};
+	indigenous.utils.trackEvent(eventName, metadata);
+	if (window.analytics) {
+		analytics.identify({
+			email: emailAddress
+		});
+	}
 
-    return true;
+	return true;
 };
