e2593ea5d46f87316b1f5e18a960f1931e941771
Ashish <ashu8006kumar@gmail.com>
2017-09-12 15:44:39 +0530

    #9817 code cleaned

--- dao/contact.dao.js
+++ dao/contact.dao.js
@@ -1212,11 +1212,13 @@ var dao = {
     },
 	deepExtend : function(destination, source) {
 		for (var property in source) {
-			if (typeof source[property] === "object" && source[property] !== null ) {
-				destination[property] = destination[property] || {};
-				arguments.callee(destination[property], source[property]);
-			} else if (source[property] !== null && destination[property] === null) {
-				destination[property] = source[property];
+			if(source[property] !== null){
+				if (typeof source[property] === "object") {
+					destination[property] = destination[property] || {};
+					arguments.callee(destination[property], source[property]);
+				} else  {
+					destination[property] = source[property];
+				}
 			}
 		}
 	}
