c2be79ae96bba610894ca2eb44ff620c16c45b8b
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-09-13 10:52:38 +0530

    Add date pull down filter and daily active user should be constrained to the period in the date pull-down #9802

--- public/admin/assets/var/demo/js/users/users.controller.js
+++ public/admin/assets/var/demo/js/users/users.controller.js
@@ -6,12 +6,48 @@
 
         var vm = this;
         vm.init = init;
+        vm.displayDatePicker = displayDatePicker;
         vm.state = {
             adminUserName: userConstant.admin_user.userName,
             adminUserEmailFilter: userConstant.admin_user.emailDomain,
             cardCodes: null,
             isAdmin: false,
             orgCardAndPermissions: UserPermissionsConfig.orgConfigAndPermissions
+        };
+
+        function displayDatePicker(){
+            $('.deshboard-date-picker').click();
+        }
+
+        var dateSwitch = false;
+        $scope.$watch('vm.selectedDate', function () {
+            vm.date.startDate = moment(vm.selectedDate.startDate).format();
+            vm.date.endDate = moment(vm.selectedDate.endDate).format();
+            
+            if (dateSwitch) {
+                loadActiveUsers();
+            }
+            dateSwitch = true;
+        });
+
+        vm.selectedDate = {
+            startDate: moment().subtract(29, 'days').startOf('day'),
+            endDate: moment()
+        };
+
+        vm.pickerOptions = {
+            startDate: moment().subtract(29, 'days').toDate(),
+            endDate: moment().toDate(),
+            format: 'YYYY-MM-DD',
+            opens: 'left',
+            ranges: {
+                'Today': [moment().startOf('day'), moment()],
+                'Yesterday': [moment().subtract(1, 'days').startOf('day'), moment().subtract(1, 'days').endOf('day')],
+                'Last 7 Days': [moment().subtract(6, 'days').startOf('day'), moment().endOf('day')],
+                'Last 30 Days': [moment().subtract(29, 'days').startOf('day'), moment().endOf('day')],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
         };
 
         var localTimezoneOffset=0;
@@ -48,27 +84,36 @@
                 AccountService.getUpdatedAccount(function (account) {
                     vm.state.account = account;
                     loadAccountUsers();
-                    ChartAnalyticsService.getDailyActiveUsers(vm.state.account._id, function(dau){
-                        //DAU
-                        var dauData = [];
-                        _.each(dau, function(dau){
-                            var subArr = [];
-                            var value = dau.total || 0;
-                            subArr.push(new Date(dau.timeframe.start.replace(" ", "T")).getTime()+localTimezoneOffset);
-                            subArr.push(value);
-                            dauData.push(subArr);
-                        });
-                        vm.dauData = dauData;
-                        ChartAnalyticsService.getActiveUserConfig(vm.dauData, function (data) {
-                            vm.activeUserConfig = data;
-                            vm.activeUserConfig.loading = false;
-                            reflowCharts();
-                        });
-                    })
+                    loadActiveUsers();
                 });
               }
             }
         });
+
+        function loadActiveUsers(){
+            ChartAnalyticsService.getDailyActiveUsers(vm.state.account._id, vm.date.startDate, vm.date.endDate, function(dau){
+                //DAU
+                var dauData = [];
+                _.each(dau, function(dau){
+                    var subArr = [];
+                    var value = dau.total || 0;
+                    subArr.push(new Date(dau.timeframe.start.replace(" ", "T")).getTime()+localTimezoneOffset);
+                    subArr.push(value);
+                    dauData.push(subArr);
+                });
+                vm.dauData = dauData;
+                if (moment(vm.date.endDate).diff(moment(vm.date.startDate), 'days') <= 7) {
+                    ChartAnalyticsService.setGraunularity('hours');
+                } else {
+                    ChartAnalyticsService.setGraunularity('days');
+                }
+                ChartAnalyticsService.getActiveUserConfig(vm.dauData, function (data) {
+                    vm.activeUserConfig = data;
+                    vm.activeUserConfig.loading = false;
+                    reflowCharts();
+                });
+            })
+        }
 
         function loadAccountUsers(){
             UserService.getAccountUsers(function(users){
@@ -517,6 +562,13 @@
                 reflowCharts();
             }
         });
+
+        vm.date = {
+            startDate: moment().subtract(29, 'days').format(),
+            endDate: moment().format()
+        };
+
+        
 
         function init() {
             UserService.getVendors(function(data){
