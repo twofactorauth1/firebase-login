292b8c0b20b46663d00463916ecb89a2524617cc
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-09-19 22:29:43 +0530

    #8927 ssb front end implemented international phone number to simple form

---
+++ public/scripts/directives/ind-international-tel-number.js
@@ -0,0 +1,28 @@
+/*global app,$ ,angular  */
+app.directive('indInternationalTelNumber', function ($timeout, $window) {
+	'use strict';
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		scope: {
+			form: '@form'
+		},
+		link: function (scope, element, attrs, ngModel) {
+			$timeout(function () {
+				$(element).intlTelInput({utilsScript: "/js/libs/intl-tel-input/lib/libphonenumber/build/utils.js"});				
+			}, 0);
+
+			scope.$watch(function () {
+              return ngModel.$modelValue;
+            }, function(newValue) {
+            	if(newValue){
+            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, $(element).intlTelInput("isValidNumber"));
+            	}
+            	else{
+            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, true);
+            	}
+            	
+           	});
+		}
+	};
+});
