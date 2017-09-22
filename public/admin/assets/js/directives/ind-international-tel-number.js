d45e807d0a1f236c9209e15531255b1f41235752
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-09-22 17:54:52 +0530

    #8927 feedback on tel number

--- public/admin/assets/js/directives/ind-international-tel-number.js
+++ public/admin/assets/js/directives/ind-international-tel-number.js
@@ -8,44 +8,46 @@ app.directive('indInternationalTelNumber', function ($timeout, $window) {
 			form: '@form'
 		},
 		link: function (scope, element, attrs, ngModel) {
-			$timeout(function () {
-				
-					$(element).removeAttr("placeholder");
-					$(element).intlTelInput({
-						nationalMode: false,
-						utilsScript: "/js/libs/intl-tel-input/build/js/utils.js"
-					});
-
-					scope.$watch(function () {
-	              		return ngModel.$modelValue;
-	            	}, function(newValue, oldValue) {
-		            	if(newValue && !oldValue){
-		            		$(element).trigger("keyup");
-		            	}
-	           		});
-
-					if(scope.form){
-
-						$(element).on("open:countrydropdown", function(e) {
-  							$(element).parents("section").addClass("overflow_visible-without-index");
-						});
-
-						$(element).on("close:countrydropdown", function(e) {
-	  						$(element).parents("section").removeClass("overflow_visible-without-index");
+			if(attrs.name === 'phone' || attrs.phoneField){
+				$timeout(function () {
+					
+						$(element).removeAttr("placeholder");
+						$(element).intlTelInput({
+							nationalMode: false,
+							utilsScript: "/js/libs/intl-tel-input/build/js/utils.js"
 						});
 
 						scope.$watch(function () {
 		              		return ngModel.$modelValue;
-		            	}, function(newValue) {
-			            	if(newValue){
-			            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, $(element).intlTelInput("isValidNumber"));
-			            	}
-			            	else{
-			            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, true);
+		            	}, function(newValue, oldValue) {
+			            	if(newValue && !oldValue){
+			            		$(element).trigger("keyup");
 			            	}
 		           		});
-					}		
-			}, 0);
-		}
+
+						if(scope.form){
+
+							$(element).on("open:countrydropdown", function(e) {
+	  							$(element).parents("section").addClass("overflow_visible-without-index");
+							});
+
+							$(element).on("close:countrydropdown", function(e) {
+		  						$(element).parents("section").removeClass("overflow_visible-without-index");
+							});
+
+							scope.$watch(function () {
+			              		return ngModel.$modelValue;
+			            	}, function(newValue) {
+				            	if(newValue){
+				            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, $(element).intlTelInput("isValidNumber"));
+				            	}
+				            	else{
+				            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, true);
+				            	}
+			           		});
+						}		
+				}, 0);
+			}
+		}			
 	};
 });
