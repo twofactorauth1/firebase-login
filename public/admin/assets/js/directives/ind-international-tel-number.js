dfbbcfabc9d2af8dde0bd63d7b7748be45d6ad3e
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-09-22 13:55:17 +0530

    #8927 contact details and order details

---
+++ public/admin/assets/js/directives/ind-international-tel-number.js
@@ -0,0 +1,51 @@
+/*global app,$ ,angular  */
+app.directive('indInternationalTelNumber', function ($timeout, $window) {
+	'use strict';
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		scope: {
+			form: '@form'
+		},
+		link: function (scope, element, attrs, ngModel) {
+			$timeout(function () {
+				
+					$(element).removeAttr("placeholder");
+					$(element).intlTelInput({
+						nationalMode: false,
+						utilsScript: "/js/libs/intl-tel-input/build/js/utils.js"
+					});
+
+					scope.$watch(function () {
+	              		return ngModel.$modelValue;
+	            	}, function(newValue, oldValue) {
+		            	if(newValue && !oldValue){
+		            		$(element).trigger("keyup");
+		            	}
+	           		});
+
+					if(scope.form){
+
+						$(element).on("open:countrydropdown", function(e) {
+  							$(element).parents("section").addClass("overflow_visible-without-index");
+						});
+
+						$(element).on("close:countrydropdown", function(e) {
+	  						$(element).parents("section").removeClass("overflow_visible-without-index");
+						});
+
+						scope.$watch(function () {
+		              		return ngModel.$modelValue;
+		            	}, function(newValue) {
+			            	if(newValue){
+			            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, $(element).intlTelInput("isValidNumber"));
+			            	}
+			            	else{
+			            		scope.$parent[scope.form][attrs.name].$setValidity(attrs.name, true);
+			            	}
+		           		});
+					}		
+			}, 0);
+		}
+	};
+});
