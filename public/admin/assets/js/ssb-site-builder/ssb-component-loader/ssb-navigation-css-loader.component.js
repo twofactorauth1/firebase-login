d6ba007e4b4adc1f93e05dd6e226d92a4111a41a
Ashish <ashu8006kumar@gmail.com>
2017-09-22 08:27:23 +0530

    css component added , moving common code to one place.

---
+++ public/admin/assets/js/ssb-site-builder/ssb-component-loader/ssb-navigation-css-loader.component.js
@@ -0,0 +1,14 @@
+/*global app*/
+(function () {
+	"use strict";
+	app.directive('ssbNavigationComponentCssLoader', function () {
+		return {
+			restrict: 'E',
+			scope: {
+				component: "="
+			},
+			templateUrl: '/admin/assets/js/ssb-site-builder/ssb-components/shared/navigation_style.html',
+			replace: true
+		};
+	});
+}());
