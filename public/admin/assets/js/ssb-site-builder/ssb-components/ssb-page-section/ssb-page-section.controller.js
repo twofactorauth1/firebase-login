990a8fd9db461a91e74769a98e54b5e0db3d4fe5
Ashish <ashu8006kumar@gmail.com>
2017-10-09 18:00:10 +0530

    #9941

--- public/admin/assets/js/ssb-site-builder/ssb-components/ssb-page-section/ssb-page-section.controller.js
+++ public/admin/assets/js/ssb-site-builder/ssb-components/ssb-page-section/ssb-page-section.controller.js
@@ -118,6 +118,15 @@
 					if (section.bg && section.bg.img && section.bg.img.show === false) {
 						classString += " hide-hero-bg-image";
 					}
+					angular.forEach(section.components, function (cmp, index) { 
+						if(cmp.type=="navigation" && cmp.navigation){
+							if(cmp.navigation.wideMobileMode){
+								classString += " ssb-section-wmm";
+							}else if(cmp.navigation.alwaysmobileMode){
+								classString += " ssb-section-amm";
+							}
+						}
+					});
 				}
 
 				if (section.filter) {
