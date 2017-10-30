f20bb8d79790c6297a8b74fc02c8813b8eae4921
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-10-30 14:34:48 +0530

    #10210 front end implementation

---
+++ public/scripts/directives/indi-accordion.js
@@ -0,0 +1,36 @@
+/*global angular  */
+
+app.directive('indiAccordion', function ($timeout) {
+
+	'use strict';
+	return {
+		restrict: 'E',		
+		link: function (scope, element, attr) {
+			scope.$watch(
+				function () {
+					return element.find(".accordion").length;
+				},
+				function (value) {
+					var acc = document.getElementsByClassName("accordion");
+					var i;
+
+					for (i = 0; i < acc.length; i++) {
+					    acc[i].onclick = function(){
+					        /* Toggle between adding and removing the "active" class,
+					        to highlight the button that controls the panel */
+					        this.classList.toggle("active");
+
+					        /* Toggle between hiding and showing the active panel */
+					        var panel = this.nextElementSibling;
+					        if (panel.style.display === "block") {
+					            panel.style.display = "none";
+					        } else {
+					            panel.style.display = "block";
+					        }
+					    }
+					}
+				}
+			);
+		}
+	};
+});
