4c231b070aff76e77f861493f2656aa6600cc7a8
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-10-30 15:59:44 +0530

    #10210 Opening one tab closes any open tabs

--- public/admin/assets/js/directives/indi-accordion.js
+++ public/admin/assets/js/directives/indi-accordion.js
@@ -10,23 +10,16 @@ app.directive('indiAccordion', function ($timeout) {
 				function () {
 					return element.find(".accordion").length;
 				},
-				function (value) {					
+				function (value) {
 					var acc = element.find(".accordion");
+
 					var i;
 
 					for (i = 0; i < acc.length; i++) {
-					    acc[i].onclick = function(){
-					        /* Toggle between adding and removing the "active" class,
-					        to highlight the button that controls the panel */
-					        this.classList.toggle("active");
 
-					        /* Toggle between hiding and showing the active panel */
-					        var panel = this.nextElementSibling;
-					        if (panel.style.display === "block") {
-					            panel.style.display = "none";
-					        } else {
-					            panel.style.display = "block";
-					        }
+					    acc[i].onclick = function(){
+					    	element.find(".accordion").removeClass("active");
+					        this.classList.toggle("active");
 					    }
 					}
 				}
