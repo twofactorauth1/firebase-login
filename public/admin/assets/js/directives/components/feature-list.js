5b9e1595cbafcc0cb8ad3cd835ced906e5b7b31a
Sanjeev Kumar <sanjeev.gul83@gmail.com>
2017-11-16 20:11:57 +0530

    #9701 horizontal line color issue

--- public/admin/assets/js/directives/components/feature-list.js
+++ public/admin/assets/js/directives/components/feature-list.js
@@ -113,7 +113,11 @@ app.directive('featureListComponent', ["$window", "$timeout", function ($window,
 				var styleString = ' ',
 					color;
 				if (isActive && !component.hideActiveFeautureUnderline) {
-					color = $(".list-features-" + component._id + " li.active .fr-view span:not('.fr-marker'):not('.fr-placeholder'):not(:empty):last").css("color");
+					if($(".list-features-" + component._id + " li.active .features-tabs-heading :not('.fr-marker'):not('.fr-placeholder'):not(:empty):last").text()){
+						color = $(".list-features-" + component._id + " li.active .features-tabs-heading :not('.fr-marker'):not('.fr-placeholder'):not(:empty):last").css("color")
+					}
+					else
+						color = $(".list-features-" + component._id + " li.active .fr-view span:not('.fr-marker'):not('.fr-placeholder'):not(:empty):last").css("color");
 					
 					if (!color) {
 						color = $(".list-features-" + component._id + " li.active").css("color");
