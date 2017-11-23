fff7ebef1b544446acefb9dcad89cd374b3d7929
Ashish <ashu8006kumar@gmail.com>
2017-11-23 15:37:50 +0530

    #10091 spectrum updated for blog settings

--- public/admin/assets/js/controllers/settingsCtrl.js
+++ public/admin/assets/js/controllers/settingsCtrl.js
@@ -489,35 +489,8 @@
         label: "100%",
         value: "100%"
       }
-    ]
-
-    $scope.spectrum = {
-      options: {
-        showPalette: true,
-        clickoutFiresChange: true,
-        showInput: true,
-        showButtons: false,
-        allowEmpty: true,
-        hideAfterPaletteSelect: false,
-        showPaletteOnly: true,
-        togglePaletteOnly: true,
-        togglePaletteMoreText: 'more',
-        togglePaletteLessText: 'less',
-        preferredFormat: 'hex',
-        appendTo: 'body',
-        palette: [
-          ["#C91F37", "#DC3023", "#9D2933", "#CF000F", "#E68364", "#F22613", "#CF3A24", "#C3272B", "#8F1D21", "#D24D57"],
-          ["#f47998", "#F47983", "#DB5A6B", "#C93756", "#FCC9B9", "#FFB3A7", "#F62459", "#F58F84", "#875F9A", "#5D3F6A"],
-          ["#89729E", "#763568", "#8D608C", "#A87CA0", "#5B3256", "#BF55EC", "#8E44AD", "#9B59B6", "#BE90D4", "#4D8FAC"],
-          ["#5D8CAE", "#22A7F0", "#19B5FE", "#59ABE3", "#48929B", "#317589", "#89C4F4", "#4B77BE", "#1F4788", "#003171"],
-          ["#044F67", "#264348", "#7A942E", "#8DB255", "#5B8930", "#6B9362", "#407A52", "#006442", "#87D37C", "#26A65B"],
-          ["#26C281", "#049372", "#2ABB9B", "#16A085", "#36D7B7", "#03A678", "#4DAF7C", "#D9B611", "#F3C13A", "#F7CA18"],
-          ["#E2B13C", "#A17917", "#F5D76E", "#F4D03F", "#FFA400", "#E08A1E", "#FFB61E", "#FAA945", "#FFA631", "#FFB94E"],
-          ["#E29C45", "#F9690E", "#CA6924", "#F5AB35", "#BFBFBF", "#F2F1EF", "#BDC3C7", "#ECF0F1", "#D2D7D3", "#757D75"],
-          ["#EEEEEE", "#ABB7B7", "#6C7A89", "#95A5A6"]
-        ]
-      }
-    };
+    ] 
+    $scope.spectrum = {options:SimpleSiteBuilderService.getSpectrumColorOptions()} ;
 
   }]);
 }(angular));
