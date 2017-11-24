d3977f9761b56847d41773e6c609fc860361b082
Ashish <ashu8006kumar@gmail.com>
2017-11-24 14:05:04 +0530

    #10039 debugger removed

--- public/js/libs_misc/froala-wysiwyg-editor/js/plugins/font-awesome-icons.js
+++ public/js/libs_misc/froala-wysiwyg-editor/js/plugins/font-awesome-icons.js
@@ -126,7 +126,6 @@
             }), b.events.on("keyup", function() {
                 for (var c = b.$el.get(0).querySelectorAll(".fr-fontAwesomeIcon"), d = 0; d < c.length; d++) "undefined" != typeof c[d].textContent && 0 === c[d].textContent.replace(/\u200B/gi, "").length && a(c[d]).remove()
             }), b.events.on('commands.before', function(cmd, param1, param2) { 
-                debugger
                 if (cmd == 'fontSize') {
                     selectedFontSize = param1;
                 }
