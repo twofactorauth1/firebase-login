414ceea4cdfed728e0dcd829169a927ed8e3a9f9
ryan-avnish <ryan.avnish@gmail.com>
2017-12-12 16:39:16 +0530

    removed temp variable

--- views/website.server.view.js
+++ views/website.server.view.js
@@ -204,7 +204,6 @@ _.extend(view.prototype, BaseView.prototype, {
                 value.website.resources = value.website.resources || {};
                 value.website.resources.userScripts = value.website.resources.userScripts || {};
                 value.website.resources.userScripts.global = value.website.resources.userScripts.global || {};
-                data.cusomData = 'jello';
                 data.userScripts = "";
                 if(value.showhide && value.showhide.userScripts && value.website.resources.toggles && value.website.resources.toggles.userScripts){
                     value.website.resources.userScripts.global = value.website.resources.userScripts.global || {};
