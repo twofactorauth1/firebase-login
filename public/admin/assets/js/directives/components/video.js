c7d51ccdebbfcd23ff13b0893c6c5985ca18dde6
ryan-avnish <ryan.avnish@gmail.com>
2018-01-16 19:02:34 +0530

    #8423 issue fix

--- public/admin/assets/js/directives/components/video.js
+++ public/admin/assets/js/directives/components/video.js
@@ -15,7 +15,7 @@ app.directive('videoComponent', ['$sce', function ($sce) {
 				var parsedUrl = urlParser.parse(url),
 					retUrl = "";
 				if (parsedUrl) {
-					retUrl = iframeUrl + parsedUrl.id + '?showinfo=0&rel=0&hd=1';
+					retUrl = iframeUrl + parsedUrl.id + '?rel=0&hd=1';
 					if (autoPlay) {
 						retUrl = retUrl + '&autoplay=1';
 					}
